80abea74cc12ba98cacdd73711762ac453337145
Robert Nelson <robertcnelson@gmail.com>
2010-04-15 15:20:42 -0500

    update /etc/e2fsck.conf tweak to option recommended in bug 563618

--- tools/fixup-gui.sh
+++ tools/fixup-gui.sh
@@ -2,19 +2,9 @@
 set -e
 
 cat > /etc/e2fsck.conf <<EOF
-[problems]
+[options]
 
-# Superblock last mount time is in the future (PR_0_FUTURE_SB_LAST_MOUNT).
-0x000031 = {
-    preen_ok = true
-    preen_nomessage = true
-} 
-
-# Superblock last write time is in the future (PR_0_FUTURE_SB_LAST_WRITE).
-0x000032 = {
-    preen_ok = true
-    preen_nomessage = true
-}
+broken_system_clock = true
 
 EOF
 
