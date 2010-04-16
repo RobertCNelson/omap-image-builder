de58f3cf6756d03b9c22b587c6e3bcf2693424e7
Robert Nelson <robertcnelson@gmail.com>
2010-04-16 12:12:30 -0500

    add /etc/fw_env.config file for beagles

--- tools/fixup-gui.sh
+++ tools/fixup-gui.sh
@@ -13,6 +13,15 @@ mkdir -p /boot/mmc
 echo "/dev/mmcblk0p2   /           auto   errors=remount-ro   0   1" >> /etc/fstab
 echo "/dev/mmcblk0p1   /boot/mmc   auto   defaults            0   0" >> /etc/fstab
 
+cat > /etc/fw_env.config <<FW
+# Configuration file for fw_(printenv/saveenv) utility.
+# Up to two entries are valid, in this case the redundand
+# environment sector is assumed present.
+# MTD device name       Device offset   Env. size       Flash sector size
+       /dev/mtd2               0x0000          0x20000         0x20000
+
+FW
+
 cat > /etc/X11/xorg.conf <<XORG
 
 Section "Monitor"
--- tools/fixup.sh
+++ tools/fixup.sh
@@ -6,7 +6,6 @@ cat > /etc/e2fsck.conf <<EOF
 
 broken_system_clock = true
 
-
 EOF
 
 mkdir -p /boot/mmc
@@ -14,4 +13,12 @@ mkdir -p /boot/mmc
 echo "/dev/mmcblk0p2   /           auto   errors=remount-ro   0   1" >> /etc/fstab
 echo "/dev/mmcblk0p1   /boot/mmc   auto   defaults            0   0" >> /etc/fstab
 
+cat > /etc/fw_env.config <<FW
+# Configuration file for fw_(printenv/saveenv) utility.
+# Up to two entries are valid, in this case the redundand
+# environment sector is assumed present.
+# MTD device name       Device offset   Env. size       Flash sector size
+       /dev/mtd2               0x0000          0x20000         0x20000
+
+FW
 
