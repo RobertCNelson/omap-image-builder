f2351a4caaf3eb0855ca818a12206d3160ca0410
Robert Nelson <robertcnelson@gmail.com>
2010-04-19 12:28:41 -0500

    remove mtd partion and implement a kill flash-kernel

--- tools/fixup.sh
+++ tools/fixup.sh
@@ -13,12 +13,13 @@ mkdir -p /boot/uboot
 echo "/dev/mmcblk0p2   /           auto   errors=remount-ro   0   1" >> /etc/fstab
 echo "/dev/mmcblk0p1   /boot/uboot auto   defaults            0   0" >> /etc/fstab
 
-cat > /etc/fw_env.config <<FW
-# Configuration file for fw_(printenv/saveenv) utility.
-# Up to two entries are valid, in this case the redundand
-# environment sector is assumed present.
-# MTD device name       Device offset   Env. size       Flash sector size
-       /dev/mtd2               0x0000          0x20000         0x20000
+cat > /etc/flash-kernel.conf <<FK
+#!/bin/sh
 
-FW
+echo "flash-kernel stopped:"
+echo "You are currently running the Community Kernel edition"
+echo "remove /etc/flash-kernel.conf to run Ubuntu's Kernel"
+exit
+
+FK
 
