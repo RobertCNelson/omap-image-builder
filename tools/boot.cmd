b37c7d41bf9d84d83c4217b874482ab106391b03
Robert Nelson <robertcnelson@gmail.com>
2010-05-05 11:37:31 -0500

    pass uboot buddy variable for zippy1/2 detection

--- tools/boot.cmd
+++ tools/boot.cmd
@@ -1,4 +1,4 @@
 setenv bootcmd 'mmc init; fatload mmc 0:1 0x80000000 uImage; fatload mmc 0:1 0x81600000 uInitrd; bootm 0x80000000 0x81600000'
-setenv bootargs 'console=tty0 console=ttyS2,115200n8 root=/dev/mmcblk0p2 rootwait ro vram=12M omapfb.mode=dvi:1280x720MR-16@60 fixrtc'
+setenv bootargs console=tty0 console=ttyS2,115200n8 root=/dev/mmcblk0p2 rootwait ro vram=12M omapfb.mode=dvi:1280x720MR-16@60 fixrtc buddy=${buddy}
 boot
 
