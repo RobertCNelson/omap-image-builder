0df7ddbb07542b23d5782ec099824cbef9b9e7bb
Robert Nelson <robertcnelson@gmail.com>
2010-08-11 06:50:43 -0500

    bump kernel

--- build_image.sh
+++ build_image.sh
@@ -240,7 +240,7 @@ function lucid_release {
 reset_vars
 
 DIST=lucid
-KERNEL="http://rcn-ee.net/deb/lucid/v2.6.34.2-l2/linux-image-2.6.34.2-l2_1.0lucid_armel.deb"
+KERNEL="http://rcn-ee.net/deb/lucid/v2.6.34.3-l2/linux-image-2.6.34.3-l2_1.0lucid_armel.deb"
 EXTRA="linux-firmware,"
 #USER_PASS="--login ubuntu --password temppwd"
 COMPONENTS=$UBUNTU_COMPONENTS
@@ -256,7 +256,7 @@ function lucid_xfce4 {
 reset_vars
 
 DIST=lucid
-KERNEL="http://rcn-ee.net/deb/lucid/v2.6.34.2-l2/linux-image-2.6.34.2-l2_1.0lucid_armel.deb"
+KERNEL="http://rcn-ee.net/deb/lucid/v2.6.34.3-l2/linux-image-2.6.34.3-l2_1.0lucid_armel.deb"
 EXTRA="linux-firmware,"
 #USER_PASS="--login ubuntu --password temppwd"
 COMPONENTS=$UBUNTU_COMPONENTS
@@ -273,7 +273,7 @@ reset_vars
 
 DIST=maverick
 #KERNEL="http://rcn-ee.net/deb/maverick/v2.6.35-dl13/linux-image-2.6.35-dl13_1.0maverick_armel.deb"
-KERNEL="http://rcn-ee.net/deb/maverick/v2.6.34.2-l2/linux-image-2.6.34.2-l2_1.0maverick_armel.deb"
+KERNEL="http://rcn-ee.net/deb/maverick/v2.6.34.3-l2/linux-image-2.6.34.3-l2_1.0maverick_armel.deb"
 EXTRA="linux-firmware,"
 #USER_PASS="--login ubuntu --password temppwd"
 COMPONENTS=$UBUNTU_COMPONENTS
@@ -290,7 +290,7 @@ reset_vars
 
 DIST=maverick
 #KERNEL="http://rcn-ee.net/deb/maverick/v2.6.35-dl13/linux-image-2.6.35-dl13_1.0maverick_armel.deb"
-KERNEL="http://rcn-ee.net/deb/maverick/v2.6.34.2-l2/linux-image-2.6.34.2-l2_1.0maverick_armel.deb"
+KERNEL="http://rcn-ee.net/deb/maverick/v2.6.34.3-l2/linux-image-2.6.34.3-l2_1.0maverick_armel.deb"
 EXTRA="linux-firmware,"
 #USER_PASS="--login ubuntu --password temppwd"
 COMPONENTS=$UBUNTU_COMPONENTS
@@ -324,9 +324,8 @@ mkdir -p ${DIR}/deploy
 set_mirror
 dl_rootstock
 
-#lucid_release
-#lucid_xfce4
-maverick_release
+lucid_release
+lucid_xfce4
+#maverick_release
 #maverick_xfce4
-
 
