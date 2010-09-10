044307678d7eb4c0d266e64acdbadf8cd15eb3d0
Robert Nelson <robertcnelson@gmail.com>
2010-09-10 13:22:46 -0500

    touchbook can now use mmc init, use debug console for now

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -138,8 +138,8 @@ function touchbook_boot_scripts {
 cat > /tmp/boot.cmd <<touchbook_boot_cmd
 setenv dvimode 1024x600MR-16@60
 setenv vram 12MB
-setenv bootcmd 'mmcinit; fatload mmc 0:1 0x80300000 uImage; fatload mmc 0:1 0x81600000 uInitrd; bootm 0x80300000 0x81600000'
-setenv bootargs console=tty1 root=/dev/mmcblk0p2 rootwait ro vram=\${vram} omapfb.mode=dvi:\${dvimode} fixrtc mpurate=\${mpurate}
+setenv bootcmd 'mmc init; fatload mmc 0:1 0x80300000 uImage; fatload mmc 0:1 0x81600000 uInitrd; bootm 0x80300000 0x81600000'
+setenv bootargs console=ttyS2,115200n8 console=tty1 root=/dev/mmcblk0p2 rootwait ro vram=\${vram} omapfb.mode=dvi:\${dvimode} fixrtc mpurate=\${mpurate}
 boot
 
 touchbook_boot_cmd
