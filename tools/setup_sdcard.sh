47e33b2431b3e24d8daade09ea188864e5d4208d
Robert Nelson <robertcnelson@gmail.com>
2010-10-19 13:48:16 -0500

    drop mpurate when doing debug
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -78,14 +78,10 @@ function beagle_debug_scripts {
 
 cat > /tmp/boot.cmd <<beagle_debug_cmd
 echo "Full Debug"
-if test "\${beaglerev}" = "xMA"; then
-echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
-setenv mpurate 800
-fi
 setenv dvimode 1280x720MR-16@60
 setenv vram 12MB
 setenv bootcmd 'mmc init; fatload mmc 0:1 0x80300000 uImage; fatload mmc 0:1 0x81600000 uInitrd; bootm 0x80300000 0x81600000'
-setenv bootargs earlyprintk debug-oem-config console=ttyS2,115200n8 console=tty0 root=/dev/mmcblk0p2 rootwait ro vram=\${vram} omapfb.mode=dvi:\${dvimode} fixrtc buddy=\${buddy} mpurate=\${mpurate}
+setenv bootargs earlyprintk debug-oem-config console=ttyS2,115200n8 console=tty0 root=/dev/mmcblk0p2 rootwait ro vram=\${vram} omapfb.mode=dvi:\${dvimode} fixrtc buddy=\${buddy}
 boot
 
 beagle_debug_cmd
