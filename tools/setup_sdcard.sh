b54089de480460290ebf36b59dd60cd68498e976
Robert Nelson <robertcnelson@gmail.com>
2010-12-03 12:33:43 -0600

    drop using sudo, will help other distros
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -30,6 +30,11 @@
 #oem-config username/password
 #add: "debug-oem-config" to bootargs
 
+if [[ $UID -ne 0 ]]; then
+ echo "$0 must be run as sudo user or root"
+ exit
+fi
+
 unset MMC
 unset SWAP_BOOT_USER
 unset DEFAULT_USER
@@ -317,15 +322,15 @@ function cleanup_sd {
  for (( c=1; c<=$NUM_MOUNTS; c++ ))
  do
   DRIVE=$(mount | grep -v none | grep "$MMC" | tail -1 | awk '{print $1}')
-  sudo umount ${DRIVE} &> /dev/null || true
+  umount ${DRIVE} &> /dev/null || true
  done
 
- sudo parted -s ${MMC} mklabel msdos
+parted -s ${MMC} mklabel msdos
 }
 
 function create_partitions {
 
-sudo fdisk -H 255 -S 63 ${MMC} << END
+fdisk -H 255 -S 63 ${MMC} << END
 n
 p
 1
@@ -340,33 +345,33 @@ w
 END
 
 sync
-sudo partprobe ${MMC}
+partprobe ${MMC}
 
 echo ""
 echo "3 / 7: Formatting Boot Partition"
 echo ""
 
-sudo mkfs.vfat -F 16 ${MMC}${PARTITION_PREFIX}1 -n ${BOOT_LABEL} &> ${DIR}/sd.log
+mkfs.vfat -F 16 ${MMC}${PARTITION_PREFIX}1 -n ${BOOT_LABEL} &> ${DIR}/sd.log
 
 mkdir -p ${TEMPDIR}/disk
-sudo partprobe ${MMC}
+partprobe ${MMC}
 
-if sudo mount -t vfat ${MMC}${PARTITION_PREFIX}1 ${TEMPDIR}/disk; then
+if mount -t vfat ${MMC}${PARTITION_PREFIX}1 ${TEMPDIR}/disk; then
 
 	if [ "$DO_UBOOT" ];then
 	 if ls ${TEMPDIR}/dl/${MLO} >/dev/null 2>&1;then
-	 sudo cp -v ${TEMPDIR}/dl/${MLO} ${TEMPDIR}/disk/MLO
+	  cp -v ${TEMPDIR}/dl/${MLO} ${TEMPDIR}/disk/MLO
 	 fi
 
 	 if ls ${TEMPDIR}/dl/${UBOOT} >/dev/null 2>&1;then
-	 sudo cp -v ${TEMPDIR}/dl/${UBOOT} ${TEMPDIR}/disk/u-boot.bin
+	  cp -v ${TEMPDIR}/dl/${UBOOT} ${TEMPDIR}/disk/u-boot.bin
 	 fi
 	fi
 
 	cd ${TEMPDIR}/disk
 	sync
 	cd ${DIR}/
-	sudo umount ${TEMPDIR}/disk || true
+	umount ${TEMPDIR}/disk || true
 
 	echo ""
 	echo "Initial Boot Partition Created"
@@ -379,7 +384,7 @@ else
 	exit
 fi
 
-sudo fdisk ${MMC} << ROOTFS
+fdisk ${MMC} << ROOTFS
 n
 p
 2
@@ -390,12 +395,12 @@ w
 ROOTFS
 
 sync
-sudo partprobe ${MMC}
+partprobe ${MMC}
 
 echo ""
 echo "4 / 7: Formating ${RFS} Partition"
 echo ""
-sudo mkfs.${RFS} ${MMC}${PARTITION_PREFIX}2 -L ${RFS_LABEL} &>> ${DIR}/sd.log
+mkfs.${RFS} ${MMC}${PARTITION_PREFIX}2 -L ${RFS_LABEL} &>> ${DIR}/sd.log
 
 }
 
@@ -403,19 +408,19 @@ function populate_boot {
  echo ""
  echo "5 / 7: Populating Boot Partition"
  echo ""
- sudo partprobe ${MMC}
+ partprobe ${MMC}
 
- if sudo mount -t vfat ${MMC}${PARTITION_PREFIX}1 ${TEMPDIR}/disk; then
+ if mount -t vfat ${MMC}${PARTITION_PREFIX}1 ${TEMPDIR}/disk; then
 
  if ls ${DIR}/vmlinuz-* >/dev/null 2>&1;then
   LINUX_VER=$(ls ${DIR}/vmlinuz-* | awk -F'vmlinuz-' '{print $2}')
   echo "uImage"
-  sudo mkimage -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n ${LINUX_VER} -d ${DIR}/vmlinuz-* ${TEMPDIR}/disk/uImage
+  mkimage -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n ${LINUX_VER} -d ${DIR}/vmlinuz-* ${TEMPDIR}/disk/uImage
  fi
 
  if ls ${DIR}/initrd.img-* >/dev/null 2>&1;then
   echo "uInitrd"
-  sudo mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n initramfs -d ${DIR}/initrd.img-* ${TEMPDIR}/disk/uInitrd
+  mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n initramfs -d ${DIR}/initrd.img-* ${TEMPDIR}/disk/uInitrd
  fi
 
 if [ "$DO_UBOOT" ];then
@@ -424,27 +429,27 @@ if [ "$DO_UBOOT" ];then
 #in that case user.scr gets loaded over boot.scr
 if [ "$SWAP_BOOT_USER" ] ; then
  if ls /tmp/boot.cmd >/dev/null 2>&1;then
-  sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /tmp/boot.cmd ${TEMPDIR}/disk/user.scr
-  sudo cp /tmp/boot.cmd ${TEMPDIR}/disk/user.cmd
+  mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /tmp/boot.cmd ${TEMPDIR}/disk/user.scr
+  cp /tmp/boot.cmd ${TEMPDIR}/disk/user.cmd
   rm -f /tmp/user.cmd || true
  fi
 fi
 
  if ls /tmp/boot.cmd >/dev/null 2>&1;then
- sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /tmp/boot.cmd ${TEMPDIR}/disk/boot.scr
- sudo cp /tmp/boot.cmd ${TEMPDIR}/disk/boot.cmd
+ mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /tmp/boot.cmd ${TEMPDIR}/disk/boot.scr
+ cp /tmp/boot.cmd ${TEMPDIR}/disk/boot.cmd
  rm -f /tmp/boot.cmd || true
  fi
 
  if ls /tmp/user.cmd >/dev/null 2>&1;then
- sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Reset Nand" -d /tmp/user.cmd ${TEMPDIR}/disk/user.scr
- sudo cp /tmp/user.cmd ${TEMPDIR}/disk/user.cmd
+ mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Reset Nand" -d /tmp/user.cmd ${TEMPDIR}/disk/user.scr
+ cp /tmp/user.cmd ${TEMPDIR}/disk/user.cmd
  rm -f /tmp/user.cmd || true
  fi
 
  #for igepv2 users
  if ls ${TEMPDIR}/disk/boot.scr >/dev/null 2>&1;then
- sudo cp -v ${TEMPDIR}/disk/boot.scr ${TEMPDIR}/disk/boot.ini
+ cp -v ${TEMPDIR}/disk/boot.scr ${TEMPDIR}/disk/boot.ini
  fi
 
 fi
@@ -663,36 +668,36 @@ cd ..
 
 gst_omapfb
 
- sudo mkdir -p ${TEMPDIR}/disk/tools/dsp
- sudo cp -v /tmp/readme.txt ${TEMPDIR}/disk/tools/readme.txt
- sudo cp -v /tmp/rebuild_uinitrd.sh ${TEMPDIR}/disk/tools/rebuild_uinitrd.sh
- sudo chmod +x ${TEMPDIR}/disk/tools/rebuild_uinitrd.sh
+ mkdir -p ${TEMPDIR}/disk/tools/dsp
+ cp -v /tmp/readme.txt ${TEMPDIR}/disk/tools/readme.txt
+ cp -v /tmp/rebuild_uinitrd.sh ${TEMPDIR}/disk/tools/rebuild_uinitrd.sh
+ chmod +x ${TEMPDIR}/disk/tools/rebuild_uinitrd.sh
 
- sudo cp -v /tmp/boot_scripts.sh ${TEMPDIR}/disk/tools/boot_scripts.sh
- sudo chmod +x ${TEMPDIR}/disk/tools/boot_scripts.sh
+ cp -v /tmp/boot_scripts.sh ${TEMPDIR}/disk/tools/boot_scripts.sh
+ chmod +x ${TEMPDIR}/disk/tools/boot_scripts.sh
 
- sudo cp -v /tmp/fix_zippy2.sh ${TEMPDIR}/disk/tools/fix_zippy2.sh
- sudo chmod +x ${TEMPDIR}/disk/tools/fix_zippy2.sh
+ cp -v /tmp/fix_zippy2.sh ${TEMPDIR}/disk/tools/fix_zippy2.sh
+ chmod +x ${TEMPDIR}/disk/tools/fix_zippy2.sh
 
- sudo cp -v /tmp/latest_kernel.sh ${TEMPDIR}/disk/tools/latest_kernel.sh
- sudo chmod +x ${TEMPDIR}/disk/tools/latest_kernel.sh
+ cp -v /tmp/latest_kernel.sh ${TEMPDIR}/disk/tools/latest_kernel.sh
+ chmod +x ${TEMPDIR}/disk/tools/latest_kernel.sh
 
- sudo cp -v /tmp/minimal_xfce.sh ${TEMPDIR}/disk/tools/minimal_xfce.sh
- sudo chmod +x ${TEMPDIR}/disk/tools/minimal_xfce.sh
+ cp -v /tmp/minimal_xfce.sh ${TEMPDIR}/disk/tools/minimal_xfce.sh
+ chmod +x ${TEMPDIR}/disk/tools/minimal_xfce.sh
 
- sudo cp -v /tmp/get_chrome.sh ${TEMPDIR}/disk/tools/get_chrome.sh
- sudo chmod +x ${TEMPDIR}/disk/tools/get_chrome.sh
+ cp -v /tmp/get_chrome.sh ${TEMPDIR}/disk/tools/get_chrome.sh
+ chmod +x ${TEMPDIR}/disk/tools/get_chrome.sh
 
- sudo cp -v /tmp/gst-dsp.sh  ${TEMPDIR}/disk/tools/dsp/gst-dsp.sh
- sudo chmod +x ${TEMPDIR}/disk/tools/dsp/gst-dsp.sh
+ cp -v /tmp/gst-dsp.sh  ${TEMPDIR}/disk/tools/dsp/gst-dsp.sh
+ chmod +x ${TEMPDIR}/disk/tools/dsp/gst-dsp.sh
 
- sudo cp -v /tmp/gst-omapfb.sh ${TEMPDIR}/disk/tools/dsp/gst-omapfb.sh
- sudo chmod +x ${TEMPDIR}/disk/tools/dsp/gst-omapfb.sh
+ cp -v /tmp/gst-omapfb.sh ${TEMPDIR}/disk/tools/dsp/gst-omapfb.sh
+ chmod +x ${TEMPDIR}/disk/tools/dsp/gst-omapfb.sh
 
 cd ${TEMPDIR}/disk
 sync
 cd ${DIR}/
-sudo umount ${TEMPDIR}/disk || true
+umount ${TEMPDIR}/disk || true
 
 	echo ""
 	echo "Finished populating Boot Partition"
@@ -711,20 +716,20 @@ function populate_rootfs {
  echo "6 / 7: Populating rootfs Partition"
  echo "Be patient, this may take a few minutes"
  echo ""
- sudo partprobe ${MMC}
+ partprobe ${MMC}
 
- if sudo mount -t ${RFS} ${MMC}${PARTITION_PREFIX}2 ${TEMPDIR}/disk; then
+ if mount -t ${RFS} ${MMC}${PARTITION_PREFIX}2 ${TEMPDIR}/disk; then
 
  if ls ${DIR}/armel-rootfs-*.tgz >/dev/null 2>&1;then
-   pv ${DIR}/armel-rootfs-*.tgz | sudo tar --numeric-owner --preserve-permissions -xzf - -C ${TEMPDIR}/disk/
+   pv ${DIR}/armel-rootfs-*.tgz | tar --numeric-owner --preserve-permissions -xzf - -C ${TEMPDIR}/disk/
  fi
 
  if ls ${DIR}/armel-rootfs-*.tar >/dev/null 2>&1;then
-   pv ${DIR}/armel-rootfs-*.tar | sudo tar --numeric-owner --preserve-permissions -xf - -C ${TEMPDIR}/disk/
+   pv ${DIR}/armel-rootfs-*.tar | tar --numeric-owner --preserve-permissions -xf - -C ${TEMPDIR}/disk/
  fi
 
 if [ "$DEFAULT_USER" ] ; then
- sudo rm -f ${TEMPDIR}/disk/var/lib/oem-config/run || true
+ rm -f ${TEMPDIR}/disk/var/lib/oem-config/run || true
 fi
 
  if [ "$CREATE_SWAP" ] ; then
@@ -732,19 +737,19 @@ fi
   echo ""
   echo "Extra: Creating SWAP File"
   echo ""
-
   echo "SWAP BUG creation note:"
-  echo "IF this takes a long time, open another command terminal and run dmesg"
+  echo "IF this takes a long time, (more then 5mins) open another command terminal and run dmesg"
   echo "if theres a nasty error, ctrl-c/reboot and try again... its an annoying bug.."
+  echo ""
 
   SPACE_LEFT=$(df ${TEMPDIR}/disk/ | grep ${MMC}${PARTITION_PREFIX}2 | awk '{print $4}')
 
   let SIZE=$SWAP_SIZE*1024
 
   if [ $SPACE_LEFT -ge $SIZE ] ; then
-   sudo dd if=/dev/zero of=${TEMPDIR}/disk/mnt/SWAP.swap bs=1M count=$SWAP_SIZE
-   sudo mkswap ${TEMPDIR}/disk/mnt/SWAP.swap
-   echo "/mnt/SWAP.swap  none  swap  sw  0 0" | sudo tee -a ${TEMPDIR}/disk/etc/fstab
+   dd if=/dev/zero of=${TEMPDIR}/disk/mnt/SWAP.swap bs=1M count=$SWAP_SIZE
+   mkswap ${TEMPDIR}/disk/mnt/SWAP.swap
+   echo "/mnt/SWAP.swap  none  swap  sw  0 0" >> ${TEMPDIR}/disk/etc/fstab
    else
    echo "FIXME Recovery after user selects SWAP file bigger then whats left not implemented"
   fi
@@ -755,7 +760,7 @@ fi
  sync
  cd ${DIR}/
 
- sudo umount ${TEMPDIR}/disk || true
+ umount ${TEMPDIR}/disk || true
 
 	echo ""
 	echo "Finished populating rootfs Partition"
@@ -773,14 +778,14 @@ fi
 }
 
 function check_mmc {
- FDISK=$(sudo LC_ALL=C sfdisk -l 2>/dev/null | grep "[Disk] ${MMC}" | awk '{print $2}')
+ FDISK=$(LC_ALL=C sfdisk -l 2>/dev/null | grep "[Disk] ${MMC}" | awk '{print $2}')
 
  if test "-$FDISK-" = "-$MMC:-"
  then
   echo ""
   echo "I see..."
-  echo "sudo sfdisk -l:"
-  sudo LC_ALL=C sfdisk -l 2>/dev/null | grep "[Disk] /dev/" --color=never
+  echo "sfdisk -l:"
+  LC_ALL=C sfdisk -l 2>/dev/null | grep "[Disk] /dev/" --color=never
   echo ""
   echo "mount:"
   mount | grep -v none | grep "/dev/" --color=never
@@ -792,8 +797,8 @@ function check_mmc {
   echo ""
   echo "Are you sure? I Don't see [${MMC}], here is what I do see..."
   echo ""
-  echo "sudo sfdisk -l:"
-  sudo LC_ALL=C sfdisk -l 2>/dev/null | grep "[Disk] /dev/" --color=never
+  echo "sfdisk -l:"
+  LC_ALL=C sfdisk -l 2>/dev/null | grep "[Disk] /dev/" --color=never
   echo ""
   echo "mount:"
   mount | grep -v none | grep "/dev/" --color=never
@@ -902,7 +907,7 @@ esac
 }
 
 function usage {
-    echo "usage: $(basename $0) --mmc /dev/sdX --uboot <dev board> --swap_file <50Mb mininum>"
+    echo "usage: sudo $(basename $0) --mmc /dev/sdX --uboot <dev board> --swap_file <50Mb mininum>"
 cat <<EOF
 
 required options:
