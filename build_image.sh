fd6bbcc08d86be2a9ac606828956b1ea6254deec
Robert Nelson <robertcnelson@gmail.com>
2010-12-21 11:33:23 -0600

    need panda image for mark
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_image.sh
+++ build_image.sh
@@ -65,7 +65,7 @@ MAVERICK_RELEASE="ubuntu-10.10-r3"
 #Natty Schedule:
 #https://wiki.ubuntu.com/NattyReleaseSchedule
 #alpha-1 : December 2nd
-NATTY_ALPHA="ubuntu-natty-alpha1"
+NATTY_ALPHA="ubuntu-natty-alpha1-r1"
 #alpha-2 : February 3rd
 NATTY_ALPHA2="ubuntu-natty-alpha2"
 #alpha-3 : March 3rd
@@ -413,10 +413,10 @@ function natty_release {
 reset_vars
 
 DIST=natty
-SERIAL=ttyS2
-#SERIAL=ttyO2
-latest_stable
-#latest_testing
+#SERIAL=ttyS2
+SERIAL=ttyO2
+#latest_stable
+latest_testing
 EXTRA="linux-firmware,devmem2,"
 COMPONENTS=$UBUNTU_COMPONENTS
 MIRROR=$MIRROR_UBU
@@ -456,7 +456,7 @@ set_mirror
 unset USE_OEM
 dl_rootstock
 maverick_release
-#natty_release
+natty_release
 
 #squeeze_release
 
