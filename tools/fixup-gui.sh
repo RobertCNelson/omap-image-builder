d78139fb325679241faaf7841ace75874516b2a0
Robert Nelson <robertcnelson@gmail.com>
2011-02-17 08:58:42 -0600

    add workaround for smsc95xx kevent2 issues
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/fixup-gui.sh
+++ tools/fixup-gui.sh
@@ -6,6 +6,9 @@ mkdir -p /boot/uboot
 echo "/dev/mmcblk0p2   /           auto   errors=remount-ro   0   1" >> /etc/fstab
 echo "/dev/mmcblk0p1   /boot/uboot auto   defaults            0   0" >> /etc/fstab
 
+#smsc95xx kevent workaround/hack
+echo "vm.min_free_kbytes = 8192" >> /etc/sysctl.conf
+
 cat > /etc/flash-kernel.conf <<FK
 #!/bin/sh
 UBOOT_PART=/dev/mmcblk0p1
