8c5a64fa7602490a691a91f0d4dc4f5ae133ba53
Robert Nelson <robertcnelson@gmail.com>
2011-03-30 10:59:21 -0500

    so close to not needing the 800Mhz limit anymore...
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -152,6 +152,10 @@ if test "\${beaglerev}" = "xMB"; then
 echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
 setenv mpurate 800
 fi
+if test "\${beaglerev}" = "xMC"; then
+echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
+setenv mpurate 800
+fi
 setenv dvimode 1280x720MR-16@60
 setenv vram 12MB
 setenv bootcmd 'mmc init; fatload mmc 0:1 0x80300000 uImage; fatload mmc 0:1 0x81600000 uInitrd; bootm 0x80300000 0x81600000'
@@ -170,6 +174,10 @@ echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
 setenv mpurate 800
 fi
 if test "\${beaglerev}" = "xMB"; then
+echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
+setenv mpurate 800
+fi
+if test "\${beaglerev}" = "xMC"; then
 echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
 setenv mpurate 800
 fi
