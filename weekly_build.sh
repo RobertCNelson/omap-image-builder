2ae07e8e8bcf97b6cbf3e29b3878673a8a033a51
Robert Nelson <robertcnelson@gmail.com>
2011-07-08 15:02:38 -0500

    add simpiler weekly build
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ weekly_build.sh
@@ -0,0 +1,199 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2009-2011 Robert Nelson <robertcnelson@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+SYST=$(cat /etc/hostname)
+ARCH=$(uname -m)
+TIME=$(date +%y%m%d)
+
+MINIMAL="-minimal"
+
+MINIMAL_APT="btrfs-tools,i2c-tools,nano,pastebinit,uboot-envtools,uboot-mkimage,usbutils,wget,wireless-tools,wpasupplicant"
+#cpufrequtils
+
+DEB_MIRROR="http://rcn-ee.net/deb"
+
+DEB_COMPONENTS="main,contrib,non-free"
+UBU_COMPONENTS="main,universe,multiverse"
+
+MIRROR_UBU="--mirror http://ports.ubuntu.com/ubuntu-ports/"
+MIRROR_DEB="--mirror http://ftp.us.debian.org/debian/"
+MIRROR_DEB_ARMHF="--mirror http://ftp.debian-ports.org/debian/"
+
+DIR=$PWD
+
+function reset_vars {
+
+unset DIST
+unset KERNEL
+unset EXTRA
+unset USER_PASS
+
+}
+
+function set_mirror {
+
+if [ $SYST == "work-p4" ] || [ $SYST == "work-celeron" ] || [ $SYST == "voodoo-e6400" ]; then
+	MIRROR_UBU="--mirror http://192.168.0.10:3142/ports.ubuntu.com/ubuntu-ports"
+	MIRROR_DEB="--mirror http://192.168.0.10:3142/ftp.us.debian.org/debian/"
+	MIRROR_DEB_ARMHF="--mirror http://192.168.0.10:3142/ftp.debian-ports.org/debian/"
+fi
+
+if [ $SYST == "lvrm" ] || [ $SYST == "x4-955" ] || [ "$ARCH" = "armv5tel" ] || [ "$ARCH" = "armv7l" ]; then
+	MIRROR_UBU="--mirror http://192.168.1.95:3142/ports.ubuntu.com/ubuntu-ports"
+	MIRROR_DEB="--mirror http://192.168.1.95:3142/ftp.us.debian.org/debian/"
+	MIRROR_DEB_ARMHF="--mirror http://192.168.1.95:3142/ftp.debian-ports.org/debian/"
+	DEB_MIRROR="http://192.168.1.95:81/dl/mirrors/deb"
+fi
+
+}
+
+function dl_rootstock {
+	rm -rfd ${DIR}/../project-rootstock
+	cd ${DIR}/../
+	git clone git://github.com/RobertCNelson/project-rootstock.git
+	cd ${DIR}/../project-rootstock
+        git checkout origin/rcn-ee-images -b rcn-ee-images
+
+if [ "$ARCH" = "armv7l" ]; then
+	patch -p0 < ${DIR}/patches/add-debian-ports-keyring.diff
+fi
+
+if [ "${USE_OEM}" ] ; then
+#disable with debian
+	patch -p0 < ${DIR}/patches/oemconfig-and-user.diff
+fi
+
+	cd ${DIR}/deploy/
+}
+
+function minimal_armel {
+
+	rm -f ${DIR}/deploy/armel-rootfs-*.tar
+	rm -f ${DIR}/deploy/vmlinuz-*
+	rm -f ${DIR}/deploy/initrd.img-*
+	rm -f ${DIR}/deploy/rootstock-*.log
+
+	sudo ${DIR}/../project-rootstock/rootstock --fqdn omap ${USER_PASS} --fullname "Demo User" --imagesize 2G \
+	--seed ${MINIMAL_APT},${EXTRA} ${MIRROR} --components "${COMPONENTS}" \
+	--dist ${DIST} --serial ${SERIAL} --script ${DIR}/tools/fixup.sh \
+	--kernel-image ${KERNEL} --apt-upgrade --arch=${ARCH}
+}
+
+function compression {
+	rm -rfd ${DIR}/deploy/${TIME}-${KERNEL_SEL}/$BUILD || true
+	mkdir -p ${DIR}/deploy/${TIME}-${KERNEL_SEL}/$BUILD
+
+	if ls ${DIR}/deploy/armel-rootfs-*.tar >/dev/null 2>&1;then
+		mv -v ${DIR}/deploy/armel-rootfs-*.tar ${DIR}/deploy/${TIME}-${KERNEL_SEL}/$BUILD
+	fi
+
+	if ls ${DIR}/deploy/vmlinuz-* >/dev/null 2>&1;then
+		mv -v ${DIR}/deploy/vmlinuz-* ${DIR}/deploy/${TIME}-${KERNEL_SEL}/$BUILD
+	fi
+
+	if ls ${DIR}/deploy/initrd.img-* >/dev/null 2>&1;then
+		mv -v ${DIR}/deploy/initrd.img-* ${DIR}/deploy/${TIME}-${KERNEL_SEL}/$BUILD
+	fi
+
+	if ls ${DIR}/deploy/rootstock-*.log >/dev/null 2>&1;then
+		rm -f ${DIR}/deploy/rootstock-*.log || true
+	fi
+
+	cp -v ${DIR}/tools/setup_sdcard.sh ${DIR}/deploy/${TIME}-${KERNEL_SEL}/$BUILD
+
+#	echo "Calculating MD5SUMS" 
+#	cd ${DIR}/deploy/$BUILD
+#	md5sum ./* > ${DIR}/deploy/$BUILD.md5sums 2> /dev/null
+
+	echo "Starting Compression"
+	cd ${DIR}/deploy/${TIME}-${KERNEL_SEL}/
+	#tar cvfz $BUILD.tar.gz ./$BUILD
+	#tar cvfj $BUILD.tar.bz2 ./$BUILD
+	#tar cvfJ $BUILD.tar.xz ./$BUILD
+
+#if [ "$ARCH" = "armv5tel" ] || [ "$ARCH" = "armv7l" ];then
+#	tar cvf $BUILD.tar ./$BUILD
+#else
+	tar cvf $BUILD.tar ./$BUILD
+	xz -z -7 -v $BUILD.tar
+#fi
+
+	cd ${DIR}/deploy/
+}
+
+function kernel_select {
+
+if [ -f /tmp/LATEST-${SUBARCH} ] ; then
+	rm -f /tmp/LATEST-${SUBARCH}
+fi
+
+wget --no-verbose --directory-prefix=/tmp/ http://rcn-ee.net/deb/${DIST}/LATEST-${SUBARCH}
+FTP_DIR=$(cat /tmp/LATEST-${SUBARCH} | grep "ABI:1 ${KERNEL_SEL}" | awk '{print $3}')
+FTP_DIR=$(echo ${FTP_DIR} | awk -F'/' '{print $6}')
+
+if [ -f /tmp/index.html ] ; then
+	rm -f /tmp/index.html
+fi
+
+wget --no-verbose --directory-prefix=/tmp/ http://rcn-ee.net/deb/${DIST}/${FTP_DIR}/
+ACTUAL_DEB_FILE=$(cat /tmp/index.html | grep linux-image | awk -F "\"" '{print $2}')
+
+KERNEL="${DEB_MIRROR}/${DIST}/${FTP_DIR}/${ACTUAL_DEB_FILE}"
+
+echo "Using: ${KERNEL}"
+
+}
+
+function armhf_release {
+
+sudo apt-get install debian-ports-archive-keyring
+reset_vars
+
+DIST=unstable
+SERIAL=ttyO2
+kernel_select
+EXTRA=''
+#EXTRA="initramfs-tools,"
+MIRROR=$MIRROR_DEB_ARMHF
+COMPONENTS="main"
+BUILD=unstable$MINIMAL-armhf-$SUBARCH
+USER_PASS="--login debian --password temppwd"
+ARCH=armhf
+minimal_armel
+compression
+
+}
+
+mkdir -p ${DIR}/deploy/
+
+#Disable set_mirror for release
+set_mirror
+dl_rootstock
+
+KERNEL_SEL="STABLE"
+
+SUBARCH="omap"
+armhf_release
+
+SUBARCH="imx"
+armhf_release
+
