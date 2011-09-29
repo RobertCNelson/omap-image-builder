276163eb9bc4488b25a2b030171739234c88a1fe
Robert Nelson <robertcnelson@gmail.com>
2011-09-29 10:09:15 -0500

    oneiric doesnt have xubuntu-gdm-theme
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -445,7 +445,11 @@ cat > ${TEMPDIR}/minimal_xfce.sh <<basic_xfce
 #!/bin/sh
 
 sudo apt-get update
+if lsb_release -c | grep oneiric ; then
+sudo apt-get -y install xfce4 xubuntu-artwork xserver-xorg-video-omap3 network-manager
+else
 sudo apt-get -y install xfce4 gdm xubuntu-gdm-theme xubuntu-artwork xserver-xorg-video-omap3 network-manager
+fi
 
 echo "Disabling eth0 in /etc/network/interfaces so xfce's network-manager works"
 sudo sed -i 's/auto eth0/#auto eth0/g' /etc/network/interfaces
