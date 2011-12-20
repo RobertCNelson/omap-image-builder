aef5db79a4ee24dd826a2741f1633c09396dcf5f
Robert Nelson <robertcnelson@gmail.com>
2011-12-20 16:30:58 -0600

    update the boot updater script
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -605,24 +605,26 @@ cat > ${TEMPDIR}/update_boot_files.sh <<update_boot_files
 cd /boot/uboot
 sudo mount -o remount,rw /boot/uboot
 
-if ! ls /boot/initrd.img-\$(uname -r) >/dev/null 2>&1;then
+if [ ! -f /boot/initrd.img-\$(uname -r) ] ; then
 sudo update-initramfs -c -k \$(uname -r)
 else
 sudo update-initramfs -u -k \$(uname -r)
 fi
 
-if ls /boot/initrd.img-\$(uname -r) >/dev/null 2>&1;then
+if [ -f /boot/initrd.img-\$(uname -r) ] ; then
 sudo mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n initramfs -d /boot/initrd.img-\$(uname -r) /boot/uboot/uInitrd
 fi
 
-if ls /boot/uboot/boot.cmd >/dev/null 2>&1;then
+if [ -f /boot/uboot/boot.cmd ] ; then
 sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /boot/uboot/boot.cmd /boot/uboot/boot.scr
+sudo cp /boot/uboot/boot.scr /boot/uboot/boot.ini
 fi
-if ls /boot/uboot/serial.cmd >/dev/null 2>&1;then
+
+if [ -f /boot/uboot/serial.cmd ] ; then
 sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /boot/uboot/serial.cmd /boot/uboot/boot.scr
 fi
-sudo cp /boot/uboot/boot.scr /boot/uboot/boot.ini
-if ls /boot/uboot/user.cmd >/dev/null 2>&1;then
+
+if [ -f /boot/uboot/user.cmd ] ; then
 sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Reset Nand" -d /boot/uboot/user.cmd /boot/uboot/user.scr
 fi
 
