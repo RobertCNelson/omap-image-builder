6e52af94fc27177ccd1d7596ae9097315c62b736
Robert Nelson <robertcnelson@gmail.com>
2011-12-22 11:41:32 -0600

    use local mirror for deb at work too
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_image.sh
+++ build_image.sh
@@ -103,6 +103,7 @@ function set_mirror {
 if [ $SYST == "work-p4" ] || [ $SYST == "work-celeron" ] || [ $SYST == "voodoo-e6400" ]; then
 	MIRROR_UBU="--mirror http://192.168.0.10:3142/ports.ubuntu.com/ubuntu-ports"
 	MIRROR_DEB="--mirror http://192.168.0.10:3142/ftp.us.debian.org/debian/"
+	DEB_MIRROR="http://192.168.0.10/dl/mirrors/deb"
 fi
 
 if [ $SYST == "hera" ] || [ $SYST == "lvrm" ] || [ $SYST == "x4-955" ] || [ "$HOST_ARCH" = "armv7l" ]; then
