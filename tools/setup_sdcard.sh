349e2df238cdbf55e668ffafe08b0762f16c9301
Robert Nelson <robertcnelson@gmail.com>
2012-02-21 11:01:15 -0600

    sdcard script: before using LC_ALL=C we checked for multiple language translations of Disk, remove old []s
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>
    Reported-by: Robert P. J. Day <rpjday@crashcourse.ca>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -945,14 +945,14 @@ fi
 
 function check_mmc {
 
- FDISK=$(LC_ALL=C fdisk -l 2>/dev/null | grep "[Disk] ${MMC}" | awk '{print $2}')
+ FDISK=$(LC_ALL=C fdisk -l 2>/dev/null | grep "Disk ${MMC}" | awk '{print $2}')
 
  if test "-$FDISK-" = "-$MMC:-"
  then
   echo ""
   echo "I see..."
   echo "fdisk -l:"
-  LC_ALL=C fdisk -l 2>/dev/null | grep "[Disk] /dev/" --color=never
+  LC_ALL=C fdisk -l 2>/dev/null | grep "Disk /dev/" --color=never
   echo ""
   echo "mount:"
   mount | grep -v none | grep "/dev/" --color=never
@@ -965,7 +965,7 @@ function check_mmc {
   echo "Are you sure? I Don't see [${MMC}], here is what I do see..."
   echo ""
   echo "fdisk -l:"
-  LC_ALL=C fdisk -l 2>/dev/null | grep "[Disk] /dev/" --color=never
+  LC_ALL=C fdisk -l 2>/dev/null | grep "Disk /dev/" --color=never
   echo ""
   echo "mount:"
   mount | grep -v none | grep "/dev/" --color=never
