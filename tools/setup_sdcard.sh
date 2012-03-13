d46e93e23f7d72ade89b35a72a5d1bfdb33469ab
Robert Nelson <robertcnelson@gmail.com>
2012-03-13 15:01:06 -0500

    make sure the device specifi args get set
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -312,65 +312,62 @@ case "$SYSTEM" in
 
 cat >> ${TEMPDIR}/bootscripts/normal.cmd <<uenv_normalboot_cmd
 optargs=VIDEO_CONSOLE
+deviceargs=setenv device_args mpurate=\${mpurate} buddy=\${buddy} buddy2=\${buddy2} musb_hdrc.fifo_mode=5
 
-device_args=mpurate=\${mpurate} buddy=\${buddy} buddy2=\${buddy2} musb_hdrc.fifo_mode=5
-
-loaduimage=run xyz_mmcboot; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
+loaduimage=run xyz_mmcboot; run deviceargs; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
 uenv_normalboot_cmd
         ;;
     beagle_cx)
 
 cat >> ${TEMPDIR}/bootscripts/normal.cmd <<uenv_normalboot_cmd
 optargs=VIDEO_CONSOLE
+deviceargs=setenv device_args mpurate=\${mpurate} buddy=\${buddy} buddy2=\${buddy2} musb_hdrc.fifo_mode=5
 
-device_args=mpurate=\${mpurate} buddy=\${buddy} buddy2=\${buddy2} musb_hdrc.fifo_mode=5
-
-loaduimage=run xyz_mmcboot; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
+loaduimage=run xyz_mmcboot; run deviceargs; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
 uenv_normalboot_cmd
         ;;
     beagle_xm)
 
 cat >> ${TEMPDIR}/bootscripts/normal.cmd <<uenv_normalboot_cmd
 optargs=VIDEO_CONSOLE
+deviceargs=setenv device_args mpurate=\${mpurate} buddy=\${buddy} buddy2=\${buddy2}
 
-device_args=mpurate=\${mpurate} buddy=\${buddy} buddy2=\${buddy2}
-
-loaduimage=run xyz_mmcboot; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
+loaduimage=run xyz_mmcboot; run deviceargs; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
 uenv_normalboot_cmd
         ;;
     crane)
 
 cat >> ${TEMPDIR}/bootscripts/normal.cmd <<uenv_normalboot_cmd
 optargs=VIDEO_CONSOLE
-device_args=
+deviceargs=setenv device_args
 
-loaduimage=run xyz_mmcboot; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
+loaduimage=run xyz_mmcboot; run deviceargs; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
 uenv_normalboot_cmd
         ;;
     panda)
 
 cat >> ${TEMPDIR}/bootscripts/normal.cmd <<uenv_normalboot_cmd
 optargs=VIDEO_CONSOLE
-device_args=
+deviceargs=setenv device_args
 
-loaduimage=run xyz_mmcboot; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
+loaduimage=run xyz_mmcboot; run deviceargs; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
 uenv_normalboot_cmd
         ;;
     panda_es)
 
 cat >> ${TEMPDIR}/bootscripts/normal.cmd <<uenv_normalboot_cmd
 optargs=VIDEO_CONSOLE
-device_args=
+deviceargs=setenv device_args
 
-loaduimage=run xyz_mmcboot; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
+loaduimage=run xyz_mmcboot; run deviceargs; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
 uenv_normalboot_cmd
         ;;
     bone)
 
 cat >> ${TEMPDIR}/bootscripts/normal.cmd <<uenv_normalboot_cmd
-device_args=ip=\${ip_method}
+deviceargs=setenv device_args ip=\${ip_method}
 
-mmc_load_uimage=run xyz_mmcboot; run bootargs_defaults; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
+mmc_load_uimage=run xyz_mmcboot; run bootargs_defaults; run deviceargs; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
 uenv_normalboot_cmd
         ;;
 esac
