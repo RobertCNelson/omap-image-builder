d1ebd3c07cb8a16c3742b4dea8fffae62fad7840
Robert Nelson <robertcnelson@gmail.com>
2012-07-31 10:58:58 -0500

    seperate out rcn-ee demo stuff
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ host/rcn-ee-demo-image.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#Oneiric Schedule:
+#https://wiki.ubuntu.com/OneiricReleaseSchedule
+#alpha-1 : June 2nd
+ONEIRIC_ALPHA="ubuntu-oneiric-alpha1"
+#alpha-2 : July  7th
+ONEIRIC_ALPHA2="ubuntu-oneiric-alpha2"
+#alpha-3 : August 4th
+ONEIRIC_ALPHA3="ubuntu-oneiric-alpha3"
+#beta1 : September 1st
+ONEIRIC_BETA1="ubuntu-oneiric-beta1"
+#beta2 : September 22nd
+ONEIRIC_BETA2="ubuntu-oneiric-beta2"
+#10.10 : October 13th
+ONEIRIC_RELEASE="ubuntu-11.10-r13"
+
+ONEIRIC_CURRENT=${ONEIRIC_RELEASE}
+
+#Precise Schedule:
+#https://wiki.ubuntu.com/PrecisePangolin/ReleaseSchedule
+#alpha-1 : Dec 1st
+PRECISE_ALPHA="ubuntu-precise-alpha1"
+#alpha-2 : Feb 2nd
+PRECISE_ALPHA2="ubuntu-precise-alpha2-1"
+#beta-1 : March 1st
+PRECISE_BETA1="ubuntu-precise-beta1"
+#beta-2 : March 29th
+PRECISE_BETA2="ubuntu-precise-beta2"
+#12.04 : April 26th
+PRECISE_RELEASE="ubuntu-12.04-r6"
+
+PRECISE_CURRENT=${PRECISE_RELEASE}
+
+#Quantal Schedule:
+#https://wiki.ubuntu.com/QuantalQuetzal/ReleaseSchedule
+#alpha-1 : June 7th
+QUANTAL_ALPHA="ubuntu-quantal-alpha1"
+#alpha-2 : June 28th
+QUANTAL_ALPHA2="ubuntu-quantal-alpha2"
+#alpha-3 : July 26nd
+QUANTAL_ALPHA3="ubuntu-quantal-alpha3"
+#beta-1 : September 6th
+QUANTAL_BETA1="ubuntu-quantal-beta1"
+#beta-2 : September 27th
+QUANTAL_BETA2="ubuntu-quantal-beta2"
+#12.04 : October 18th
+QUANTAL_RELEASE="ubuntu-12.10-r1"
+
+QUANTAL_CURRENT=${QUANTAL_BETA1}
--- lxde_image.sh
+++ lxde_image.sh
@@ -26,55 +26,9 @@ TIME=$(date +%Y-%m-%d)
 
 unset USE_OEM
 
-#Oneiric Schedule:
-#https://wiki.ubuntu.com/OneiricReleaseSchedule
-#alpha-1 : June 2nd
-ONEIRIC_ALPHA="ubuntu-oneiric-alpha1"
-#alpha-2 : July  7th
-ONEIRIC_ALPHA2="ubuntu-oneiric-alpha2"
-#alpha-3 : August 4th
-ONEIRIC_ALPHA3="ubuntu-oneiric-alpha3"
-#beta1 : September 1st
-ONEIRIC_BETA1="ubuntu-oneiric-beta1"
-#beta2 : September 22nd
-ONEIRIC_BETA2="ubuntu-oneiric-beta2"
-#10.10 : October 13th
-ONEIRIC_RELEASE="ubuntu-11.10-r13"
-
-ONEIRIC_CURRENT=${ONEIRIC_RELEASE}
-
-#Precise Schedule:
-#https://wiki.ubuntu.com/PrecisePangolin/ReleaseSchedule
-#alpha-1 : Dec 1st
-PRECISE_ALPHA="ubuntu-precise-alpha1"
-#alpha-2 : Feb 2nd
-PRECISE_ALPHA2="ubuntu-precise-alpha2-1"
-#beta-1 : March 1st
-PRECISE_BETA1="ubuntu-precise-beta1"
-#beta-2 : March 29th
-PRECISE_BETA2="ubuntu-precise-beta2"
-#12.04 : April 26th
-PRECISE_RELEASE="ubuntu-12.04-r6"
-
-PRECISE_CURRENT=${PRECISE_RELEASE}
-
-#Quantal Schedule:
-#https://wiki.ubuntu.com/QuantalQuetzal/ReleaseSchedule
-#alpha-1 : June 7th
-QUANTAL_ALPHA="ubuntu-quantal-alpha1"
-#alpha-2 : June 28th
-QUANTAL_ALPHA2="ubuntu-quantal-alpha2"
-#alpha-3 : July 26nd
-QUANTAL_ALPHA3="ubuntu-quantal-alpha3"
-#beta-1 : September 6th
-QUANTAL_BETA1="ubuntu-quantal-beta1"
-#beta-2 : September 27th
-QUANTAL_BETA2="ubuntu-quantal-beta2"
-#12.04 : October 18th
-QUANTAL_RELEASE="ubuntu-12.10-r1"
-
-QUANTAL_CURRENT=${QUANTAL_BETA1}
-
+ONEIRIC_CURRENT="ubuntu-11.10"
+PRECISE_CURRENT="ubuntu-12.04"
+QUANTAL_RELEASE="ubuntu-12.10"
 SQUEEZE_CURRENT="debian-6.0.5"
 WHEEZY_CURRENT="debian-wheezy"
 
@@ -337,6 +291,7 @@ source ${DIR}/defaults.sh
 
 if [ -f ${DIR}/rcn-ee.host ] ; then
 	source ${DIR}/host/rcn-ee-host.sh
+	source ${DIR}/host/rcn-ee-demo-image.sh
 fi
 
 mkdir -p ${DIR}/deploy/
