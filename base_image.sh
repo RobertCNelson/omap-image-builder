0b66c7712da9811ac496e56f45549a14a58c68c2
Robert Nelson <robertcnelson@gmail.com>
2012-09-01 08:38:04 -0500

    RAMTMP varible is no default no, but in a new location
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- base_image.sh
+++ base_image.sh
@@ -238,21 +238,21 @@ if [ "${DEBOOT_TEST}" != "${DEBOOT_VER}" ] ; then
 	rm -rf debootstrap_${DEBOOT_VER}_all.deb || true
 fi
 
-RAMTMP_TEST=$(cat /etc/default/rcS | grep -v "#" | grep RAMTMP | awk -F"=" '{print $2}')
-if [ -f /etc/default/rcS ] ; then
+RAMTMP_TEST=$(cat /etc/default/tmpfs | grep -v "#" | grep RAMTMP | awk -F"=" '{print $2}')
+if [ -f /etc/default/tmpfs ] ; then
 	if [ "-${RAMTMP_TEST}-" == "-yes-" ] ; then
 		if [ "-${HOST_ARCH}-" == "-armv7l-" ] ; then
 			echo ""
 			echo "ERROR"
-			echo "With RAMTMP=yes in /etc/default/rcS on ARM, debootstrap will fail, as /tmp is mounted as nodev."
-			echo "Please modify /etc/default/rcS and set RAMTMP=no and reboot."
+			echo "With RAMTMP=yes in /etc/default/tmpfs on ARM, debootstrap will fail, as /tmp is mounted as nodev."
+			echo "Please modify /etc/default/tmpfs and set RAMTMP=no and reboot."
 			echo ""
 			exit
 		else
 			echo ""
 			echo "WARNING"
-			echo "With RAMTMP=yes in /etc/default/rcS, this script will probally fail due to running out of memory."
-			echo "Please modify /etc/default/rcS and set RAMTMP=no and reboot."
+			echo "With RAMTMP=yes in /etc/default/tmpfs, this script will probally fail due to running out of memory."
+			echo "Please modify /etc/default/tmpfs and set RAMTMP=no and reboot."
 			echo ""
 		fi
 	fi
