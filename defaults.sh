6336de7f8a8640d254e9437d3ebcfdc43a9109ca
Robert Nelson <robertcnelson@gmail.com>
2012-09-10 10:18:39 -0500

    prepare for new release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- defaults.sh
+++ defaults.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-RELEASE_HOST="panda-es-1gb-b0"
+RELEASE_HOST="panda-es-1gb-a3"
 
 #DEBOOT_VER="1.0.41ubuntu1"
-#DEBOOT_HTTP="http://ports.ubuntu.com/pool/main/d/debootstrap"
-
 DEBOOT_VER="1.0.42"
-DEBOOT_HTTP="http://ftp.us.debian.org/debian/pool/main/d/debootstrap"
+DEBOOT_HTTP="http://ports.ubuntu.com/pool/main/d/debootstrap"
+
+#DEBOOT_VER="1.0.42"
+#DEBOOT_HTTP="http://ftp.us.debian.org/debian/pool/main/d/debootstrap"
 
 DEB_MIRROR="http://rcn-ee.net/deb"
 
