9a8d12b4234814137ed30b705fa22d2c4c864a43
Robert Nelson <robertcnelson@gmail.com>
2012-10-05 06:45:54 -0500

    move host checking to a single file
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_image.sh
+++ build_image.sh
@@ -317,6 +317,7 @@ function sid_release {
 }
 
 source ${DIR}/var/defaults.sh
+source ${DIR}/var/check_host.sh
 
 if [ -f ${DIR}/rcn-ee.host ] ; then
 	source ${DIR}/host/rcn-ee-host.sh
@@ -324,40 +325,6 @@ if [ -f ${DIR}/rcn-ee.host ] ; then
 fi
 
 mkdir -p ${DIR}/deploy/
-
-if [ ! $(which fakeroot) ] ; then
-	echo "Missing fakeroot"
-	sudo apt-get -y install fakeroot
-fi
-
-DEBOOT_TEST=$(sudo debootstrap --version | awk '{print $2}')
-
-if [ "${DEBOOT_TEST}" != "${DEBOOT_VER}" ] ; then
-	echo "Installing minimal debootstrap version..."
-	wget ${DEBOOT_HTTP}/debootstrap_${DEBOOT_VER}_all.deb
-	sudo dpkg -i debootstrap_${DEBOOT_VER}_all.deb
-	rm -rf debootstrap_${DEBOOT_VER}_all.deb || true
-fi
-
-RAMTMP_TEST=$(cat /etc/default/tmpfs | grep -v "#" | grep RAMTMP | awk -F"=" '{print $2}')
-if [ -f /etc/default/tmpfs ] ; then
-	if [ "-${RAMTMP_TEST}-" == "-yes-" ] ; then
-		if [ "-${HOST_ARCH}-" == "-armv7l-" ] ; then
-			echo ""
-			echo "ERROR"
-			echo "With RAMTMP=yes in /etc/default/tmpfs on ARM, debootstrap will fail, as /tmp is mounted as nodev."
-			echo "Please modify /etc/default/tmpfs and set RAMTMP=no and reboot."
-			echo ""
-			exit
-		else
-			echo ""
-			echo "WARNING"
-			echo "With RAMTMP=yes in /etc/default/tmpfs, this script will probally fail due to running out of memory."
-			echo "Please modify /etc/default/tmpfs and set RAMTMP=no and reboot."
-			echo ""
-		fi
-	fi
-fi
 
 if [ -f ${DIR}/release ] ; then
 	echo "Building Release Package, with no mirrors"
