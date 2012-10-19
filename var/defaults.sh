5b40974f23b7914dda69a156b0c2b80233e0dd4e
Robert Nelson <robertcnelson@gmail.com>
2012-10-19 12:17:57 -0500

    distro: debian: squeeze is at 6.0.6
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- var/defaults.sh
+++ var/defaults.sh
@@ -20,6 +20,6 @@ MIRROR_DEB="--mirror http://ftp.us.debian.org/debian/"
 PRECISE_CURRENT="ubuntu-12.04"
 QUANTAL_CURRENT="ubuntu-12.10"
 RARING_CURRENT="ubuntu-13.04"
-SQUEEZE_CURRENT="debian-6.0.5"
+SQUEEZE_CURRENT="debian-6.0.6"
 WHEEZY_CURRENT="debian-wheezy"
 
