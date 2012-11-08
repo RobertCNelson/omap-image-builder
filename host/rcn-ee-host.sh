4e8fe84e76d3a8ec95fd71c2d5fc39ad772437e7
Robert Nelson <robertcnelson@gmail.com>
2012-11-08 13:33:28 -0600

    now the jenkins test should work
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- host/rcn-ee-host.sh
+++ host/rcn-ee-host.sh
@@ -7,7 +7,7 @@ if [ "x${SYST}" == "xhades" ] || [ "x${SYST}" == "xwork-e6400" ] ; then
 	MIRROR_DEB="--mirror http://192.168.0.10:3142/ftp.us.debian.org/debian/"
 fi
 
-if [ "x${SYST}" == "xhera" ] || [ "x${SYST}" == "xe350" ] || [ "x${SYST}" == "xx4-955" ] || [ "x${SYST}" == "x${RELEASE_HOST}" ] ; then
+if [ "x${SYST}" == "xposeidon" ] || [ "x${SYST}" == "x${RELEASE_HOST}" ] ; then
 	MIRROR_UBU="--mirror http://192.168.1.95:3142/ports.ubuntu.com/ubuntu-ports"
 	MIRROR_DEB="--mirror http://192.168.1.95:3142/ftp.us.debian.org/debian/"
 	DEB_MIRROR="http://192.168.1.95:81/dl/mirrors/deb"
