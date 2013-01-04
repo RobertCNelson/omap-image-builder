c5d1c8705bf46be33c1a599acc9dd7c61e2f63de
Robert Nelson <robertcnelson@gmail.com>
2013-01-04 11:12:50 -0600

    RootStock-NG: auto install minimal debootstrap
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 DIR=$(pwd)
 TEMPDIR=$(mktemp -d)
@@ -7,8 +7,36 @@ debarch="armhf"
 distro="wheezy"
 apt_proxy="192.168.0.10:3142/"
 deb_mirror="ftp.us.debian.org/debian"
-
 host_arch="$(uname -m)"
+
+#1.0.${minimal_debootstrap}
+minimal_debootstrap="46"
+
+debootstrap_is_installed () {
+	unset deb_pkgs
+	dpkg -l | grep debootstrap >/dev/null || deb_pkgs+="debootstrap "
+
+	if [ "${deb_pkgs}" ] ; then
+		echo "Installing: ${deb_pkgs}"
+		sudo apt-get update
+		sudo apt-get -y install ${deb_pkgs}
+	fi
+}
+
+debootstrap_what_version () {
+	test_debootstrap=$(/usr/sbin/debootstrap --version | awk '{print $2}' | awk -F"." '{print $3}')
+	echo "debootstrap version: 1.0."$test_debootstrap""
+}
+
+debootstrap_is_installed
+debootstrap_what_version
+
+if [[ "$test_debootstrap" < "$minimal_debootstrap" ]] ; then
+	echo "Installing minimal debootstrap version: 1.0."${minimal_debootstrap}"..."
+	wget http://rcn-ee.net/mirror/debootstrap/debootstrap_1.0.${minimal_debootstrap}_all.deb
+	sudo dpkg -i debootstrap_1.0.${minimal_debootstrap}_all.deb
+	rm -rf debootstrap_1.0.${minimal_debootstrap}_all.deb || true
+fi
 
 echo "Starting: debootstrap in ${TEMPDIR}"
 sudo debootstrap --foreign --arch ${debarch} ${distro} ${TEMPDIR} http://${apt_proxy}${deb_mirror}
