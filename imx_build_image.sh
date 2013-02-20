b4e8cd9517264a4d0f00498d0da3b1ed457e07ec
Robert Nelson <robertcnelson@gmail.com>
2013-02-20 09:49:06 -0600

    RootStock-NG: offically rip project-rootstock out of the scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- imx_build_image.sh
+++ imx_build_image.sh
@@ -26,7 +26,6 @@ TIME=$(date +%Y-%m-%d)
 time=$(date +%Y-%m-%d)
 
 unset USE_OEM
-unset ROOTSTOCKNG
 
 MINIMAL="-minimal"
 
@@ -61,171 +60,74 @@ function reset_vars {
 	IMAGESIZE="2G"
 }
 
-function dl_rootstock {
-	if [ ! -f ${DIR}/git/project-rootstock/.git/config ] ; then
-		mkdir -p ${DIR}/git/
-		cd ${DIR}/git/
-		git clone git://github.com/RobertCNelson/project-rootstock.git
-		cd ${DIR}/
-	fi
-
-	cd ${DIR}/git/project-rootstock
-	git checkout origin/master -b tmp
-	git branch -D run-script || true
-	git branch -D master || true
-
-	git checkout origin/master -b master
-	git branch -D tmp
-
-	git pull
-
-	cd ${DIR}/deploy/
-}
-
 function minimal_armel {
-	if [ "${ROOTSTOCKNG}" ] ; then
-		rm -f "${DIR}/.project" || true
+	rm -f "${DIR}/.project" || true
 
-		pkgs="${MINIMAL_APT}${EXTRA}"
+	pkgs="${MINIMAL_APT}${EXTRA}"
 
-		base_pkg_list=$(echo ${pkgs} | sed -e 's/,/ /g')
+	base_pkg_list=$(echo ${pkgs} | sed -e 's/,/ /g')
 
-		#Actual Releases will use version numbers..
-		case "${DIST}" in
-		squeeze)
-			#http://www.debian.org/releases/squeeze/
-			export_filename="${distro}-6.0.6-console-${ARCH}-${time}"
-			;;
-		quantal)
-			export_filename="${distro}-12.10-console-${ARCH}-${time}"
-			;;
-		*)
-			export_filename="${distro}-${DIST}-console-${ARCH}-${time}"
-			;;
-		esac
+	#Actual Releases will use version numbers..
+	case "${DIST}" in
+	squeeze)
+		#http://www.debian.org/releases/squeeze/
+		export_filename="${distro}-6.0.6-console-${ARCH}-${time}"
+		;;
+	quantal)
+		export_filename="${distro}-12.10-console-${ARCH}-${time}"
+		;;
+	*)
+		export_filename="${distro}-${DIST}-console-${ARCH}-${time}"
+		;;
+	esac
 
-		tempdir=$(mktemp -d)
+	tempdir=$(mktemp -d)
 
-		cat > ${DIR}/.project <<-__EOF__
-			tempdir="${tempdir}"
-			export_filename="${export_filename}"
+	cat > ${DIR}/.project <<-__EOF__
+		tempdir="${tempdir}"
+		export_filename="${export_filename}"
 
-			distro="${distro}"
+		distro="${distro}"
 
-			release="${DIST}"
-			dpkg_arch="${ARCH}"
+		release="${DIST}"
+		dpkg_arch="${ARCH}"
 
-			apt_proxy="${apt_proxy}"
-			base_pkg_list="${base_pkg_list}"
+		apt_proxy="${apt_proxy}"
+		base_pkg_list="${base_pkg_list}"
 
-			image_hostname="${FQDN}"
+		image_hostname="${FQDN}"
 
-			user_name="${USER_LOGIN}"
-			full_name="${USER_NAME}"
-			password="${USER_PASS}"
+		user_name="${USER_LOGIN}"
+		full_name="${USER_NAME}"
+		password="${USER_PASS}"
 
-			chroot_ENABLE_DEB_SRC="${chroot_ENABLE_DEB_SRC}"
+		chroot_ENABLE_DEB_SRC="${chroot_ENABLE_DEB_SRC}"
 
-			chroot_KERNEL_HTTP_DIR="${chroot_KERNEL_HTTP_DIR}"
+		chroot_KERNEL_HTTP_DIR="${chroot_KERNEL_HTTP_DIR}"
 
-		__EOF__
+	__EOF__
 
-		cat ${DIR}/.project
+	cat ${DIR}/.project
 
-		/bin/bash -e "${DIR}/RootStock-NG.sh" || { exit 1 ; }
-	else
-rm -f ${DIR}/deploy/arm*-rootfs-*.tar || true
-rm -f ${DIR}/deploy/vmlinuz-* || true
-rm -f ${DIR}/deploy/initrd.img-* || true
-rm -f ${DIR}/deploy/rootstock-*.log || true
-
-echo ""
-echo "Running as:"
-echo "-------------------------"
-echo "sudo ${DIR}/git/project-rootstock/rootstock  --imagesize ${IMAGESIZE} --fqdn ${FQDN} \
---login ${USER_LOGIN} --password ${USER_PASS} --fullname \"${USER_NAME}\" \
---seed ${MINIMAL_APT}${EXTRA} ${MIRROR} --components \"${COMPONENTS}\" \
---dist ${DIST} --serial ${SERIAL} --script ${DIR}/tools/${FIXUPSCRIPT} \
-${PRIMARY_KERNEL} ${SECONDARY_KERNEL} --apt-upgrade --arch=${ARCH} "
-echo "-------------------------"
-echo ""
-
-sudo ${DIR}/git/project-rootstock/rootstock  --imagesize ${IMAGESIZE} --fqdn ${FQDN} \
---login ${USER_LOGIN} --password ${USER_PASS} --fullname "${USER_NAME}" \
---seed ${MINIMAL_APT}${EXTRA} ${MIRROR} --components "${COMPONENTS}" \
---dist ${DIST} --serial ${SERIAL} --script ${DIR}/tools/${FIXUPSCRIPT} \
-${PRIMARY_KERNEL} ${SECONDARY_KERNEL} --apt-upgrade --arch=${ARCH}
-fi
+	/bin/bash -e "${DIR}/RootStock-NG.sh" || { exit 1 ; }
 }
 
 function compression {
-if [ ! "${ROOTSTOCKNG}" ] ; then
-	rm -rf ${DIR}/deploy/${TIME}/$BUILD || true
-	mkdir -p ${DIR}/deploy/${TIME}/$BUILD
-
-	if ls ${DIR}/deploy/arm*-rootfs-*.tar >/dev/null 2>&1;then
-		mv -v ${DIR}/deploy/arm*-rootfs-*.tar ${DIR}/deploy/${TIME}/$BUILD
-	fi
-
-	if ls ${DIR}/deploy/vmlinuz-* >/dev/null 2>&1;then
-		mv -v ${DIR}/deploy/vmlinuz-* ${DIR}/deploy/${TIME}/$BUILD
-	fi
-
-	if ls ${DIR}/deploy/initrd.img-* >/dev/null 2>&1;then
-		mv -v ${DIR}/deploy/initrd.img-* ${DIR}/deploy/${TIME}/$BUILD
-	fi
-
-	if [ "${PRIMARY_DTB_FILE}" ] ; then
-		wget --no-verbose --directory-prefix=${DIR}/deploy/${TIME}/$BUILD ${PRIMARY_DTB_FILE}
-	fi
-
-	if [ "${SECONDARY_DTB_FILE}" ] ; then
-		wget --no-verbose --directory-prefix=${DIR}/deploy/${TIME}/$BUILD ${SECONDARY_DTB_FILE}
-	fi
-
-	cp -v ${DIR}/tools/setup_sdcard.sh ${DIR}/deploy/${TIME}/$BUILD
-
-	cat > ${DIR}/deploy/${TIME}/$BUILD/user_password.list <<-__EOF__
-		${USER_LOGIN}:${USER_PASS}
-	__EOF__
-
-	echo "Starting Compression"
-	cd ${DIR}/deploy/${TIME}/
-
-	if [ -f ${DIR}/release ] ; then
-		tar cvf $BUILD.tar ./$BUILD
-		xz -z -7 -v $BUILD.tar
-
-		if [ "x${SYST}" == "x${RELEASE_HOST}" ] ; then
-			if [ -d /mnt/farm/testing/pending/ ] ; then
-				cp -v $BUILD.tar.xz /mnt/farm/testing/pending/$BUILD.tar.xz
-			fi
-		fi
-
-	else
-		tar cvf $BUILD.tar ./$BUILD
-	fi
-
-	cd ${DIR}/deploy/
-else
 	echo "Starting Compression"
 	cd ${DIR}/deploy/
 
+	tar cvf ${export_filename}.tar ./${export_filename}
+
 	if [ -f ${DIR}/release ] ; then
-		tar cvf ${export_filename}.tar ./${export_filename}
-		xz -z -7 -v ${export_filename}.tar
+		echo "xz -z -7 -v ${export_filename}.tar" >> /mnt/farm/testing/pending/compress.txt
 
 		if [ "x${SYST}" == "x${RELEASE_HOST}" ] ; then
 			if [ -d /mnt/farm/testing/pending/ ] ; then
-				cp -v ${export_filename}.tar.xz /mnt/farm/testing/pending/${export_filename}.tar.xz
+				cp -v ${export_filename}.tar /mnt/farm/testing/pending/${export_filename}.tar
 			fi
 		fi
-
-	else
-		tar cvf ${export_filename}.tar ./${export_filename}
 	fi
 	cd ${DIR}/
-fi
 }
 
 function kernel_chooser {
@@ -322,12 +224,6 @@ if [ -f ${DIR}/release ] ; then
 	fi
 
 	chroot_ENABLE_DEB_SRC="enable"
-fi
-
-ROOTSTOCKNG=1
-
-if [ ! "${ROOTSTOCKNG}" ] ; then
-	dl_rootstock
 fi
 
 ARCH=armhf
