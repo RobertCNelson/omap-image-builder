4adaecacc9e362aaeb6537c0f55d5571f2e2e61c
Robert Nelson <robertcnelson@gmail.com>
2013-02-20 11:12:57 -0600

    cleanup: old crude
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- var/defaults.sh
+++ var/defaults.sh
@@ -3,17 +3,3 @@
 #RELEASE_HOST="panda-es-1gb-a3"
 RELEASE_HOST="imx6q-sabrelite-1gb"
 
-DEB_MIRROR="http://rcn-ee.net/deb"
-
-DEB_COMPONENTS="main,contrib,non-free"
-UBU_COMPONENTS="main,universe,multiverse"
-
-MIRROR_UBU="--mirror http://ports.ubuntu.com/ubuntu-ports/"
-MIRROR_DEB="--mirror http://ftp.us.debian.org/debian/"
-
-PRECISE_CURRENT="ubuntu-12.04"
-QUANTAL_CURRENT="ubuntu-12.10"
-RARING_CURRENT="ubuntu-13.04"
-SQUEEZE_CURRENT="debian-6.0.6"
-WHEEZY_CURRENT="debian-wheezy"
-
