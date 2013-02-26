e9a15b2535b6ddfab7cbce0e98d64a1c23272931
Robert Nelson <robertcnelson@gmail.com>
2013-02-26 16:00:08 -0600

    RAMTMP: simplfy error check
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- var/check_host.sh
+++ var/check_host.sh
@@ -2,21 +2,13 @@
 
 RAMTMP_TEST=$(cat /etc/default/tmpfs | grep -v "#" | grep RAMTMP | awk -F"=" '{print $2}')
 if [ -f /etc/default/tmpfs ] ; then
-	if [ "-${RAMTMP_TEST}-" == "-yes-" ] ; then
-		if [ "-${HOST_ARCH}-" == "-armv7l-" ] ; then
-			echo ""
-			echo "ERROR"
-			echo "With RAMTMP=yes in /etc/default/tmpfs on ARM, debootstrap will fail, as /tmp is mounted as nodev."
-			echo "Please modify /etc/default/tmpfs and set RAMTMP=no and reboot."
-			echo ""
-			exit
-		else
-			echo ""
-			echo "WARNING"
-			echo "With RAMTMP=yes in /etc/default/tmpfs, this script will probally fail due to running out of memory."
-			echo "Please modify /etc/default/tmpfs and set RAMTMP=no and reboot."
-			echo ""
-		fi
+	if [ "x${RAMTMP_TEST}" == "xyes" ] ; then
+		echo ""
+		echo "ERROR"
+		echo "With RAMTMP=yes this script will fail..."
+		echo "Please modify /etc/default/tmpfs and set RAMTMP=no and reboot."
+		echo ""
+		exit
 	fi
 fi
 
