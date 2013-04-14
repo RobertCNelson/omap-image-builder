53698e707346e6c2325e2beae52413ff8264ef10
Robert Nelson <robertcnelson@gmail.com>
2013-04-14 09:36:00 -0500

    dtb: now supports leading slashes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -1095,6 +1095,15 @@ function kernel_detection {
 
 check_dtb_board () {
 	invalid_dtb=1
+
+	#/hwpack/${dtb_board}.conf
+	unset leading_slash
+	leading_slash=$(echo ${dtb_board} | grep "/" || unset leading_slash)
+	if [ "${leading_slash}" ] ; then
+		dtb_board=$(echo "${leading_slash##*/}")
+	fi
+
+	#${dtb_board}.conf
 	dtb_board=$(echo ${dtb_board} | awk -F ".conf" '{print $1}')
 	if [ -f "${DIR}"/hwpack/${dtb_board}.conf ] ; then
 		source "${DIR}"/hwpack/${dtb_board}.conf
