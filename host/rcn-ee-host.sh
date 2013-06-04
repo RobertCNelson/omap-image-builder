35fdc1345a1677e4e4d20ad31cfa90e69e65edf2
Robert Nelson <robertcnelson@gmail.com>
2013-06-04 18:45:19 -0500

    rcn: two of them now
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- host/rcn-ee-host.sh
+++ host/rcn-ee-host.sh
@@ -7,11 +7,11 @@ hades|work-e6400)
 	apt_proxy="192.168.0.10:3142/"
 	mirror="http://rcn-ee.net/deb"
 	;;
-a53t|zeus|hestia|poseidon|panda-es-1gb-a3|imx6q-sabrelite-1gb)
+a53t|zeus|hestia|poseidon|panda-es-1gb-a3|imx6q-sabrelite-1gb-0|imx6q-sabrelite-1gb-1)
 	apt_proxy="rcn-ee.homeip.net:3142/"
 	mirror="http://rcn-ee.homeip.net:81/dl/mirrors/deb"
 	;;
 esac
 
-RELEASE_HOST="imx6q-sabrelite-1gb"
+RELEASE_HOST="imx6q-sabrelite-1gb-0"
 
