6a389077f9ff6115721d030f953af44ba7fd677b
Charles Steinkuehler <cdsteinkuehler@mah.priv.at>
2013-06-11 17:28:19 +0200

    Add support to build and install BeagleBone specific dtc

--- machinekit/scripts/000.copy.sh
+++ machinekit/scripts/000.copy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # If we have a 'kickstart' image, copy the pre-compiled source tree as a tarball
-for SOURCE in linuxcnc xenomai-2.6 ; do
+for SOURCE in linuxcnc xenomai-2.6 dtc ; do
 
 	if [ -d ~/beaglebone.kickstart/home/linuxcnc/${SOURCE} ] ; then
 		sudo tar cf ${tempdir}/home/linuxcnc/kickstart.${SOURCE}.tar -C ~/beaglebone.kickstart/home/linuxcnc ${SOURCE}
