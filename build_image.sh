87f279c2eff5b9637ca6222380c768f0c384a3e8
Robert Nelson <robertcnelson@gmail.com>
2013-06-14 15:44:39 -0500

    open up
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_image.sh
+++ build_image.sh
@@ -48,12 +48,12 @@ minimal_armel () {
 		;;
 	esac
 
-	if [ -f ${DIR}/release ] ; then
-		chroot_KERNEL_HTTP_DIR="\
-http://rcn-ee.net/deb/${release}-${dpkg_arch}/v3.7.10-x12/ \
-http://rcn-ee.net/deb/${release}-${dpkg_arch}/v3.8.13-bone21/ \
-http://rcn-ee.net/deb/${release}-${dpkg_arch}/v3.2.42-psp27/"
-	fi
+#	if [ -f ${DIR}/release ] ; then
+#		chroot_KERNEL_HTTP_DIR="\
+#http://rcn-ee.net/deb/${release}-${dpkg_arch}/v3.7.10-x12/ \
+#http://rcn-ee.net/deb/${release}-${dpkg_arch}/v3.8.13-bone21/ \
+#http://rcn-ee.net/deb/${release}-${dpkg_arch}/v3.2.42-psp27/"
+#	fi
 
 	tempdir=$(mktemp -d)
 
