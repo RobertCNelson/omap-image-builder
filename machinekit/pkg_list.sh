84e1f87bce61f24a198d2b110db5ee05a2f52e74
Charles Steinkuehler <charles@steinkuehler.net>
2013-07-14 04:22:31 +0200

    Remove apache and udhcpd from MachineKit image

---
+++ machinekit/pkg_list.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -e
+
+#Note: These will be auto installed by chroot.sh script:
+#lsb-release initramfs-tools sudo wget
+
+#Base
+base_pkgs="nano pastebinit"
+
+#Tools
+base_pkgs="${base_pkgs} bsdmainutils i2c-tools fbset hexedit memtester read-edid"
+
+#OS
+base_pkgs="${base_pkgs} dosfstools btrfs-tools cpufrequtils ntpdate"
+
+#USB Dongles
+base_pkgs="${base_pkgs} ppp usb-modeswitch usbutils"
+
+#Server
+#base_pkgs="${base_pkgs} apache2 openssh-server udhcpd"
+base_pkgs="${base_pkgs} openssh-server"
+
+#Wireless
+base_pkgs="${base_pkgs} wireless-tools wpasupplicant lowpan-tools wvdial lshw"
+
+#shellinabox (breaks ssh over eth/usb)
+#base_pkgs="${base_pkgs} shellinabox"
+
+#Gateone:
+#base_pkgs="${base_pkgs} python-tornado python-pyopenssl"
+
