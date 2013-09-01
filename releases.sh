d1568b5be49e40b34fca5dd89ff84f8e2ae3c5fc
Charles Steinkuehler <charles@steinkuehler.net>
2013-09-01 15:57:01 +0200

    Update kernel selections & track changes in master
    Signed-off-by: Charles Steinkuehler <charles@steinkuehler.net>

--- releases.sh
+++ releases.sh
@@ -3,28 +3,29 @@
 
 # Override RCN 3.8 BeagleBone with Xenomai version
 select_custom_kernel () {
-	SUBARCH="omap"
-	KERNEL_ABI="STABLE"
+	SUBARCH="armv7"
+	KERNEL_ABI="TESTING"
 	kernel_chooser
 	chroot_KERNEL_HTTP_DIR="${mirror}/${release}-${dpkg_arch}/${FTP_DIR}/"
 
-	#SUBARCH="omap-psp"
-	#KERNEL_ABI="TESTING"
-	#kernel_chooser
-	# Remember to change file name in machinekit/scripts/001.download.shu !!
-	chroot_KERNEL_HTTP_DIR="${chroot_KERNEL_HTTP_DIR} http://www.machinekit.net/deb/wheezy-armhf/v3.8.13xenomai-bone26/"
+	SUBARCH="omap"
+	KERNEL_ABI="STABLE"
+	kernel_chooser
+	chroot_KERNEL_HTTP_DIR="${chroot_KERNEL_HTTP_DIR} ${mirror}/${release}-${dpkg_arch}/${FTP_DIR}/"
 
 	SUBARCH="omap-psp"
 	KERNEL_ABI="STABLE"
 	kernel_chooser
-	chroot_KERNEL_HTTP_DIR="${chroot_KERNEL_HTTP_DIR} ${mirror}/${release}-${dpkg_arch}/${FTP_DIR}/"
+	# Remember to change file name in machinekit/scripts/001.download.shu !!
+	chroot_KERNEL_HTTP_DIR="${chroot_KERNEL_HTTP_DIR} http://www.machinekit.net/deb/wheezy-armhf/v3.8.13xenomai-bone26.1/"
 }
 
 
 machinekit_release () {
 	image_type="machinekit"
-	extra_pkgs="atmel-firmware firmware-ralink libertas-firmware zd1211-firmware nfs-common git-core build-essential autoconf libgd2-xpm libpth-dev dvipng tcl8.5-dev tk8.5-dev bwidget blt libxaw7-dev libncurses5-dev libreadline-dev asciidoc source-highlight dblatex xsltproc groff python-dev python-support python-tk python-lxml libglu1-mesa-dev libgl1-mesa-dev libgtk2.0-dev libgnomeprintui2.2-dev gettext libboost-python-dev texlive-lang-cyrillic libmodbus-dev libboost-thread-dev libboost-serialization-dev python-gtk2 python-gtk2-dev python-gtk2-doc python-gi python-gtksourceview2 python-imaging-tk python-notify2 python-vte python-xlib flex bison python-gtkglext1 python-serial ssh-askpass xfce4 xdm netsurf"
+	extra_pkgs="nfs-common git-core build-essential autoconf libgd2-xpm libpth-dev dvipng tcl8.5-dev tk8.5-dev bwidget blt libxaw7-dev libncurses5-dev libreadline-dev asciidoc source-highlight dblatex xsltproc groff python-dev python-support python-tk python-lxml libglu1-mesa-dev libgl1-mesa-dev libgtk2.0-dev libgnomeprintui2.2-dev gettext libboost-python-dev texlive-lang-cyrillic libmodbus-dev libboost-thread-dev libboost-serialization-dev python-gtk2 python-gtk2-dev python-gtk2-doc python-gi python-gtksourceview2 python-imaging-tk python-notify2 python-vte python-xlib flex bison python-gtkglext1 python-serial ssh-askpass xfce4 xdm netsurf"
 	#extra_pkgs="gitk git-gui"
+        firmware_pkgs="atmel-firmware firmware-ralink libertas-firmware zd1211-firmware"
 
 	#is_debian - inlined below with some changes
 
