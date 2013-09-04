5e84f05e6a5005d64530180aa45b8616160d53c1
Robert Nelson <robertcnelson@gmail.com>
2013-09-04 10:30:26 -0500

    MachineKit: drop (other bone26 kernel) and include deb pkgs no longer installed in chroot by default
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- releases.sh
+++ releases.sh
@@ -13,16 +13,12 @@ select_custom_kernel () {
 	#kernel_chooser
 	# Remember to change file name in machinekit/scripts/001.download.shu !!
 	chroot_KERNEL_HTTP_DIR="${chroot_KERNEL_HTTP_DIR} http://www.machinekit.net/deb/wheezy-armhf/v3.8.13xenomai-bone26/"
-
-	SUBARCH="omap-psp"
-	KERNEL_ABI="STABLE"
-	kernel_chooser
-	chroot_KERNEL_HTTP_DIR="${chroot_KERNEL_HTTP_DIR} ${mirror}/${release}-${dpkg_arch}/${FTP_DIR}/"
 }
 
 
 machinekit_release () {
 	image_type="machinekit"
+	include_pkgs_list="git-core,initramfs-tools,locales,sudo,u-boot-tools,wget"
 	extra_pkgs="atmel-firmware firmware-ralink libertas-firmware zd1211-firmware nfs-common git-core build-essential autoconf libgd2-xpm libpth-dev dvipng tcl8.5-dev tk8.5-dev bwidget blt libxaw7-dev libncurses5-dev libreadline-dev asciidoc source-highlight dblatex xsltproc groff python-dev python-support python-tk python-lxml libglu1-mesa-dev libgl1-mesa-dev libgtk2.0-dev libgnomeprintui2.2-dev gettext libboost-python-dev texlive-lang-cyrillic libmodbus-dev libboost-thread-dev libboost-serialization-dev python-gtk2 python-gtk2-dev python-gtk2-doc python-gi python-gtksourceview2 python-imaging-tk python-notify2 python-vte python-xlib flex bison python-gtkglext1 python-serial ssh-askpass xfce4 xdm netsurf"
 	#extra_pkgs="gitk git-gui"
 
