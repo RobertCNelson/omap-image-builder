38da2047681248013c17514d281fe00e1b3def95
Robert Nelson <robertcnelson@gmail.com>
2013-12-17 13:12:12 -0600

    bbb: add network-manager
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- beagleboard.org_image.sh
+++ beagleboard.org_image.sh
@@ -35,9 +35,10 @@ image_type="console"
 #Cloud9 IDE
 bborg_pkg_list="build-essential g++ curl libssl-dev apache2-utils libxml2-dev device-tree-compiler"
 bborg_pkg_list="${bborg_pkg_list} autoconf automake1.9 libtool flex bison gdb pkg-config libc-ares-dev less"
+#utils:
+bborg_pkg_list="${bborg_pkg_list} alsa-utils evtest"
 #lxde
-bborg_pkg_list="${bborg_pkg_list} lxde-core slim xserver-xorg-video-modesetting xserver-xorg x11-xserver-utils alsa-utils evtest"
-
+bborg_pkg_list="${bborg_pkg_list} lxde-core slim xserver-xorg-video-modesetting xserver-xorg x11-xserver-utils network-manager network-manager-gnome"
 
 minimal_armel () {
 	rm -f "${DIR}/.project" || true
