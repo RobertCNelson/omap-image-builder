93e71c835eb5d174702a9dc66853e5753bd31008
Robert Nelson <robertcnelson@gmail.com>
2013-12-18 22:27:36 -0600

    rcn: more hosts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- host/rcn-ee-host.sh
+++ host/rcn-ee-host.sh
@@ -4,7 +4,7 @@ system=$(uname -n)
 
 mirror="http://rcn-ee.net/deb"
 case "${system}" in
-poseidon|imx6q-sabrelite-1gb-0|imx6q-sabrelite-1gb-1)
+poseidon|imx6q-sabrelite-1gb-0|imx6q-sabrelite-1gb-1|imx6q-wandboard-2gb-0|imx6q-wandboard-2gb-1)
 	mirror="http://rcn-ee.homeip.net:81/dl/mirrors/deb"
 	;;
 esac
