8591a2872267391bcb3013c8ebb06a6e4116918f
Robert Nelson <robertcnelson@gmail.com>
2014-01-02 12:11:48 -0600

    bb.org: move to chroot_hooks
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ chroot_hooks/beagleboard.org.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+#chroot_cloud9_git_tag="v2.0.93"
+#chroot_node_release="v0.8.26"
+#chroot_node_build_options="--without-snapshot --shared-openssl --shared-zlib --prefix=/usr/local/"
+chroot_node_release="v0.10.24"
+chroot_node_build_options="--without-snapshot --shared-cares --shared-openssl --shared-zlib --prefix=/usr/local/"
+
+user_name="debian"
+
+qemu_warning () {
+	if [ "${warn_qemu_will_fail}" ] ; then
+		echo "Log: (chroot) Warning, qemu can fail here... (run on real armv7l hardware for production images)"
+		echo "Log: (chroot): [${qemu_command}]"
+	fi
+}
+
+install_cloud9 () {
+	mount -t tmpfs shmfs -o size=256M /dev/shm
+	df -Th
+
+	cd /opt/source
+	wget http://nodejs.org/dist/${chroot_node_release}/node-${chroot_node_release}.tar.gz
+	tar xf node-${chroot_node_release}.tar.gz
+	cd node-${chroot_node_release}
+	./configure ${chroot_node_build_options} && make -j5 && make install
+	cd /
+	rm -rf /opt/source/node-${chroot_node_release}/ || true
+
+	echo "debug: node: [`node --version`]"
+	echo "debug: npm: [`npm --version`]"
+
+	mkdir -p /opt/cloud9/ || true
+	if [ "x${chroot_cloud9_git_tag}" = "x" ] ; then
+		qemu_command="git clone --depth 1 https://github.com/ajaxorg/cloud9.git /opt/cloud9/ || true"
+		qemu_warning
+		git clone --depth 1 https://github.com/ajaxorg/cloud9.git /opt/cloud9/ || true
+		echo "/opt/cloud9/ : https://github.com/ajaxorg/cloud9.git" >> /opt/source/list.txt
+	else
+		qemu_command="git clone --depth 1 -b ${chroot_cloud9_git_tag} https://github.com/ajaxorg/cloud9.git /opt/cloud9/ || true"
+		qemu_warning
+		git clone --depth 1 -b ${chroot_cloud9_git_tag} https://github.com/ajaxorg/cloud9.git /opt/cloud9/ || true
+		echo "/opt/cloud9/ : https://github.com/ajaxorg/cloud9.git" >> /opt/source/list.txt
+	fi
+	chown -R ${user_name}:${user_name} /opt/cloud9/
+
+	if [ -f /usr/local/bin/sm ] ; then
+		echo "debug: sm: [`sm --version`]"
+		cd /opt/cloud9
+		qemu_command="sm install"
+		qemu_warning
+		sm install
+	#else
+		#cd /opt/cloud9
+		#npm install --arch=armhf
+	fi
+
+	mkdir -p /var/lib/cloud9 || true
+	qemu_command="git clone https://github.com/beagleboard/bonescript /var/lib/cloud9 --depth 1 || true"
+	qemu_warning
+	git clone https://github.com/beagleboard/bonescript /var/lib/cloud9 --depth 1 || true
+	chown -R ${user_name}:${user_name} /var/lib/cloud9
+	echo "/var/lib/cloud9 : https://github.com/beagleboard/bonescript" >> /opt/source/list.txt
+
+	if [ -f /var/www/index.html ] ; then
+		rm -rf /var/www/index.html || true
+	fi
+	qemu_command="git clone https://github.com/beagleboard/bone101 /var/www/ --depth 1 || true"
+	qemu_warning
+	git clone https://github.com/beagleboard/bone101 /var/www/ --depth 1 || true
+	echo "/var/www/ : https://github.com/beagleboard/bone101" >> /opt/source/list.txt
+	if [ -f /var/www/index.html ] ; then
+		rm -rf /var/www/index.html || true
+	fi
+	if [ -f /var/www/Support/bone101/index.html ] ; then
+		cp -v /var/www/Support/bone101/index.html /var/www/index.html
+	fi
+
+	sync
+	umount -l /dev/shm
+}
+
+install_cloud9
