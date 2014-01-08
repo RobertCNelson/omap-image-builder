856c5128de0662eee734ebd834d14aa163fa219d
Robert Nelson <robertcnelson@gmail.com>
2014-01-07 19:35:00 -0600

    scripts_device/am335x_evm.sh: drop extra debug stuff
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts_device/am335x_evm.sh
+++ scripts_device/am335x_evm.sh
@@ -3,10 +3,6 @@
 #Based off:
 #https://github.com/beagleboard/meta-beagleboard/blob/master/meta-beagleboard-extras/recipes-support/usb-gadget/gadget-init/g-ether-load.sh
 
-if [ ! -d /boot/uboot/debug/ ] ; then
-	mkdir -p /boot/uboot/debug/ || true
-fi
-
 eeprom="/sys/bus/i2c/devices/0-0050/eeprom"
 SERIAL_NUMBER=$(hexdump -e '8/1 "%c"' ${eeprom} -s 14 -n 2)-$(hexdump -e '8/1 "%c"' ${eeprom} -s 16 -n 12)
 ISBLACK=$(hexdump -e '8/1 "%c"' ${eeprom} -s 8 -n 4)
@@ -49,21 +45,13 @@ fi
 /sbin/ifconfig usb0 192.168.7.2 netmask 255.255.255.252
 /usr/sbin/udhcpd -S /etc/udhcpd.conf
 
-if [ -e /sys/class/drm/card0/card0-HDMI-A-1/edid ] ; then
-	if which fbset > /dev/null ; then
-		echo "fbset:" > /boot/uboot/debug/edid.txt
-		fbset >> /boot/uboot/debug/edid.txt
-	fi
-	if which parse-edid > /dev/null ; then
-		echo "edid:" >> /boot/uboot/debug/edid.txt
-		parse-edid /sys/class/drm/card0/card0-HDMI-A-1/edid >> /boot/uboot/debug/edid.txt
-	fi
-fi
-
 if [ -f /boot/uboot/flash-eMMC.txt ] ; then
+	if [ ! -d /boot/uboot/debug/ ] ; then
+		mkdir -p /boot/uboot/debug/ || true
+	fi
+
 	if [ -f /opt/scripts/beaglebone-black-eMMC-flasher.sh ] ; then
 		/bin/bash /opt/scripts/beaglebone-black-eMMC-flasher.sh >/boot/uboot/debug/flash-eMMC.log 2>&1
 	fi
 fi
-
-dmesg | grep cape > /boot/uboot/debug/cape.txt
+#
