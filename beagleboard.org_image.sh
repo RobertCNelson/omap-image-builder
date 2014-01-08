0b45616489832f1dc991c30521b883cfb74b8db1
Robert Nelson <robertcnelson@gmail.com>
2014-01-08 14:17:02 -0600

    bb.org: enable systemd by default
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- beagleboard.org_image.sh
+++ beagleboard.org_image.sh
@@ -149,11 +149,8 @@ production () {
 	tar xf debian-7.3-${image_type}-armhf-${time}.tar.xz
 
 	cd debian-7.3-${image_type}-armhf-${time}/
-	sudo ./setup_sdcard.sh --img BBB-eMMC-flasher-debian-7.3-${time} --uboot bone --beagleboard.org-production --bbb-flasher
-	sudo ./setup_sdcard.sh --img-4gb bone-debian-7.3-${time} --uboot bone --beagleboard.org-production
-
-	sudo ./setup_sdcard.sh --img BBB-eMMC-flasher-systemd-debian-7.3-${time} --uboot bone --beagleboard.org-production --bbb-flasher --enable-systemd
-	sudo ./setup_sdcard.sh --img-4gb bone-systemd-debian-7.3-${time} --uboot bone --beagleboard.org-production --enable-systemd
+	sudo ./setup_sdcard.sh --img BBB-eMMC-flasher-debian-7.3-${time} --uboot bone --beagleboard.org-production --bbb-flasher --enable-systemd
+	sudo ./setup_sdcard.sh --img-4gb bone-debian-7.3-${time} --uboot bone --beagleboard.org-production --enable-systemd
 
 	mv *.img ../
 	cd ..
@@ -161,9 +158,6 @@ production () {
 
 	xz -z -7 -v BBB-eMMC-flasher-debian-7.3-${time}-2gb.img
 	xz -z -7 -v bone-debian-7.3-${time}-4gb.img
-
-	xz -z -7 -v BBB-eMMC-flasher-systemd-debian-7.3-${time}-2gb.img
-	xz -z -7 -v bone-systemd-debian-7.3-${time}-4gb.img
 
 	__EOF__
 
