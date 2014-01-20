3409482790590d52386624058d0e9e9810aefcef
Robert Nelson <robertcnelson@gmail.com>
2014-01-20 12:52:06 -0600

    rcn: use any of these
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- host/rcn-ee-host.sh
+++ host/rcn-ee-host.sh
@@ -4,11 +4,14 @@ system=$(uname -n)
 
 mirror="http://rcn-ee.net/deb"
 case "${system}" in
-poseidon|imx6q-sabrelite-1gb-0|imx6q-sabrelite-1gb-1|imx6q-wandboard-2gb-0|imx6q-wandboard-2gb-1)
+poseidon)
 	mirror="http://rcn-ee.homeip.net:81/dl/mirrors/deb"
+	;;
+imx6q-sabrelite-1gb-0|imx6q-sabrelite-1gb-1|imx6q-wandboard-2gb-0|imx6q-wandboard-2gb-1)
+	mirror="http://rcn-ee.homeip.net:81/dl/mirrors/deb"
+	RELEASE_HOST="${system}"
 	;;
 esac
 
 apt_proxy="apt-proxy:3142/"
-RELEASE_HOST="imx6q-sabrelite-1gb-0"
-
+#
