0313fab0362961034485fc7441e6dd8832016d75
Robert Nelson <robertcnelson@gmail.com>
2014-02-25 11:46:23 -0600

    bb.org: add python-serial package
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- beagleboard.org_image.sh
+++ beagleboard.org_image.sh
@@ -65,7 +65,7 @@ bborg_pkg_list="${bborg_pkg_list} wicd-gtk"
 bborg_pkg_list="${bborg_pkg_list} libxi-dev"
 
 #development libs:
-bborg_pkg_list="${bborg_pkg_list} python-opencv libsdl1.2-dev python-pip python-setuptools python2.7-dev"
+bborg_pkg_list="${bborg_pkg_list} python-opencv libsdl1.2-dev python-pip python-setuptools python2.7-dev python-serial"
 
 #Web Stuff:
 bborg_pkg_list="${bborg_pkg_list} xchat"
