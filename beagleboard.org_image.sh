086a8c8cba2ecc64ecd673758777377347db0bc9
Robert Nelson <robertcnelson@gmail.com>
2014-04-11 10:05:07 -0500

    bb.org: use bone47 kernel for release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- beagleboard.org_image.sh
+++ beagleboard.org_image.sh
@@ -96,7 +96,7 @@ minimal_armel () {
 	esac
 
 	#When doing offical releases, always hard lock the kernel version...
-	#chroot_KERNEL_HTTP_DIR="${mirror}/${deb_codename}-${deb_arch}/v3.8.13-bone43/"
+	chroot_KERNEL_HTTP_DIR="${mirror}/${deb_codename}-${deb_arch}/v3.8.13-bone47/"
 
 	tempdir=$(mktemp -d -p ${DIR}/ignore)
 
