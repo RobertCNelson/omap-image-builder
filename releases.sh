2b80ccc8ac12a643f18ce5a4da3fe52a88898620
Charles Steinkuehler <charles@steinkuehler.net>
2014-04-13 07:44:27 +0000

    Add libtk-img package
    Signed-off-by: Charles Steinkuehler <charles@steinkuehler.net>

--- releases.sh
+++ releases.sh
@@ -29,7 +29,7 @@ machinekit_pkg_list () {
 machinekit_release () {
 	image_type="machinekit"
 	#extra_pkgs="systemd"
-	extra_pkgs="systemd nfs-common git-core build-essential autoconf libgd2-xpm libpth-dev dvipng tcl8.5-dev tk8.5-dev bwidget blt libxaw7-dev libncurses5-dev libreadline-dev asciidoc source-highlight dblatex xsltproc groff python-dev python-support python-tk python-lxml libglu1-mesa-dev libgl1-mesa-dev libgtk2.0-dev libgnomeprintui2.2-dev gettext libboost-python-dev texlive-lang-cyrillic libmodbus-dev libboost-thread-dev libboost-serialization-dev python-gtk2 python-gtk2-dev python-gtk2-doc python-gi python-gtksourceview2 python-imaging-tk python-notify2 python-vte python-xlib flex bison python-gtkglext1 python-serial libusb-1.0-0-dev"
+	extra_pkgs="systemd nfs-common git-core build-essential autoconf libgd2-xpm libpth-dev dvipng tcl8.5-dev tk8.5-dev bwidget blt libxaw7-dev libncurses5-dev libreadline-dev asciidoc source-highlight dblatex xsltproc groff python-dev python-support python-tk python-lxml libglu1-mesa-dev libgl1-mesa-dev libgtk2.0-dev libgnomeprintui2.2-dev gettext libboost-python-dev texlive-lang-cyrillic libmodbus-dev libboost-thread-dev libboost-serialization-dev python-gtk2 python-gtk2-dev python-gtk2-doc python-gi python-gtksourceview2 python-imaging-tk python-notify2 python-vte python-xlib flex bison python-gtkglext1 python-serial libusb-1.0-0-dev libtk-img"
 
 	firmware_pkgs="atmel-firmware firmware-ralink firmware-realtek libertas-firmware zd1211-firmware"
 
