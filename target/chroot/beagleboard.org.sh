7664800bf5a49502ed855fac0a46234123133b11
Robert Nelson <robertcnelson@gmail.com>
2014-05-30 12:02:53 -0500

    bb.org: make might not be installed
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- target/chroot/beagleboard.org.sh
+++ target/chroot/beagleboard.org.sh
@@ -416,7 +416,9 @@ install_git_repos () {
 	git_clone
 	if [ -f ${git_target_dir}/.git/config ] ; then
 		cd ${git_target_dir}/
-		make
+		if [ -f /usr/bin/make ] ; then
+			make
+		fi
 	fi
 
 	git_repo="https://github.com/biocode3D/prufh.git"
@@ -424,7 +426,9 @@ install_git_repos () {
 	git_clone
 	if [ -f ${git_target_dir}/.git/config ] ; then
 		cd ${git_target_dir}/
-		make LIBDIR_APP_LOADER=/usr/lib/ INCDIR_APP_LOADER=/usr/include
+		if [ -f /usr/bin/make ] ; then
+			make LIBDIR_APP_LOADER=/usr/lib/ INCDIR_APP_LOADER=/usr/include
+		fi
 	fi
 }
 
