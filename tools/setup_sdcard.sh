3dabb41fc592c4403e0f434846b6fefd6159d53d
Robert Nelson <robertcnelson@gmail.com>
2014-06-03 10:25:55 -0500

    setup_sdcard.sh: i need the room for window$ drivers in the boot partition
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -721,7 +721,9 @@ populate_boot () {
 		else
 			echo "Copying Kernel image:"
 			cp -v "${DIR}/${VMLINUZ_FILE}" ${TEMPDIR}/disk/zImage
-			cp -v "${DIR}/${VMLINUZ_FILE}" ${TEMPDIR}/disk/zImage-${select_kernel}
+			if [ ! "${bborg_production}" ] ; then
+				cp -v "${DIR}/${VMLINUZ_FILE}" ${TEMPDIR}/disk/zImage-${select_kernel}
+			fi
 			echo "-----------------------------"
 		fi
 	fi
@@ -731,7 +733,9 @@ populate_boot () {
 		echo "Copying Kernel initrd/uInitrd:"
 		if [ "${conf_uboot_CONFIG_SUPPORT_RAW_INITRD}" ] ; then
 			cp -v "${DIR}/${INITRD_FILE}" ${TEMPDIR}/disk/initrd.img
-			cp -v "${DIR}/${INITRD_FILE}" ${TEMPDIR}/disk/initrd.img-${select_kernel}
+			if [ ! "${bborg_production}" ] ; then
+				cp -v "${DIR}/${INITRD_FILE}" ${TEMPDIR}/disk/initrd.img-${select_kernel}
+			fi
 		else
 			mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n initramfs -d "${DIR}/${INITRD_FILE}" ${TEMPDIR}/disk/uInitrd
 		fi
