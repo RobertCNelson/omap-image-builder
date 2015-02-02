f836b9ba5e350e5ec1e6f175cce885110ced8e68
Robert Nelson <robertcnelson@gmail.com>
2015-02-01 18:00:04 -0600

    publish: more automation
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- publish/rcn-ee_bb.org-stable.sh
+++ publish/rcn-ee_bb.org-stable.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -e
 
 time=$(date +%Y-%m-%d)
+stage_dir="/var/www/html/rcn-ee.net/rootfs/bb.org/testing"
 DIR="$PWD"
 
 git pull --no-edit https://github.com/beagleboard/image-builder master
 
 export apt_proxy=apt-proxy:3142/
+
+if [ -d ./deploy ] ; then
+	rm -rf ./deploy || true
+fi
 
 ./RootStock-NG.sh -c bb.org-debian-stable
 ./RootStock-NG.sh -c bb.org-debian-stable-4gb
@@ -43,6 +48,19 @@ generic_img () {
         cd ..
 }
 
+stage_generic_img () {
+        if [ -d ${stage_dir} ] ; then
+                if [ ! -d ${stage_dir}/${time}/\${stage_blend}/ ] ; then
+                        mkdir -p ${stage_dir}/${time}/\${stage_blend}/ || true
+                fi
+                if [ -d ${stage_dir}/${time}/\${stage_blend}/ ] ; then
+                        if [ -f \${base_rootfs}.tar.xz ] ; then
+                                cp -v \${base_rootfs}.tar.xz ${stage_dir}/${time}/\${stage_blend}/
+                        fi
+                fi
+        fi
+}
+
 post_generic_img () {
         if [ -d ./\${base_rootfs} ] ; then
                 rm -rf \${base_rootfs} || true
@@ -51,16 +69,32 @@ post_generic_img () {
         if [ ! -f \${base_rootfs}.tar.xz ] ; then
                 ${archive} \${base_rootfs}.tar
         fi
+        stage_generic_img
+}
+
+stage_img () {
+        if [ -d ${stage_dir} ] ; then
+                if [ ! -d ${stage_dir}/${time}/\${stage_blend}/ ] ; then
+                        mkdir -p ${stage_dir}/${time}/\${stage_blend}/ || true
+                fi
+                if [ -d ${stage_dir}/${time}/\${stage_blend}/ ] ; then
+                        if [ -f \${wfile}.xz ] ; then
+                                cp -v \${wfile}.xz ${stage_dir}/${time}/\${stage_blend}/
+                        fi
+                fi
+        fi
 }
 
 compress_img () {
         if [ -f \${wfile} ] ; then
                 ${archive} \${wfile}
+                stage_img
         fi
 }
 
 ###Production lxde images: (BBB: 4GB eMMC)
 base_rootfs="${debian_lxde_4gb_stable}"
+stage_blend="lxde-4gb"
 pre_generic_img
 
 options="--img-4gb BBB-blank-eMMC-flasher-\${base_rootfs} --dtb bbb-blank-eeprom --beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd --rootfs_label rootfs --bbb-flasher --hostname beaglebone"
@@ -72,6 +106,7 @@ generic_img
 
 ###lxde images: (BBB: 2GB eMMC)
 base_rootfs="${debian_lxde_stable}"
+stage_blend="lxde"
 pre_generic_img
 
 options="--img-2gb BBB-eMMC-flasher-\${base_rootfs} --dtb beaglebone --beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd --rootfs_label rootfs --bbb-flasher  --bbb-old-bootloader-in-emmc --hostname beaglebone"
@@ -79,6 +114,7 @@ generic_img
 
 ###console images: (also single partition)
 base_rootfs="${debian_console_stable}"
+stage_blend="console"
 pre_generic_img
 
 options="--img-2gb BBB-eMMC-flasher-\${base_rootfs} --dtb beaglebone --boot_label BEAGLEBONE --enable-systemd --bbb-flasher --bbb-old-bootloader-in-emmc --hostname beaglebone"
@@ -88,6 +124,7 @@ generic_img
 
 ###lxqt image
 base_rootfs="${debian_lxqt_4gb_next}"
+stage_blend="lxqt-4gb"
 pre_generic_img
 
 options="--img-4gb BBB-eMMC-flasher-\${base_rootfs} --dtb beaglebone        --beagleboard.org-production --boot_label BEAGLEBONE --rootfs_label rootfs --bbb-flasher  --bbb-old-bootloader-in-emmc --hostname beaglebone"
@@ -102,6 +139,7 @@ generic_img
 
 ###machinekit:
 base_rootfs="${debian_machinekit_wheezy}"
+stage_blend="machinekit"
 pre_generic_img
 
 options="--img-4gb bone-\${base_rootfs}                   --dtb beaglebone       --beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd --bbb-old-bootloader-in-emmc --hostname beaglebone"
@@ -109,18 +147,23 @@ generic_img
 
 ###archive *.tar
 base_rootfs="${debian_lxde_4gb_stable}"
+stage_blend="lxde-4gb"
 post_generic_img
 
 base_rootfs="${debian_lxde_stable}"
+stage_blend="lxde"
 post_generic_img
 
 base_rootfs="${debian_console_stable}"
+stage_blend="console"
 post_generic_img
 
 base_rootfs="${debian_lxqt_4gb_next}"
+stage_blend="lxqt-4gb"
 post_generic_img
 
 base_rootfs="${debian_machinekit_wheezy}"
+stage_blend="machinekit"
 post_generic_img
 
 ###archive *.img
@@ -158,9 +201,14 @@ __EOF__
 
 chmod +x ${DIR}/deploy/gift_wrap_final_images.sh
 
+if [ ! -d /mnt/farm/images/ ] ; then
+	#nfs mount...
+	sudo mount -a
+fi
+
 if [ -d /mnt/farm/images/ ] ; then
+	cp -v ${DIR}/deploy/*.tar /mnt/farm/images/
 	cp -v ${DIR}/deploy/gift_wrap_final_images.sh /mnt/farm/images/gift_wrap_final_images.sh
 	chmod +x /mnt/farm/images/gift_wrap_final_images.sh
-	cp -v ${DIR}/deploy/*.tar /mnt/farm/images/
 fi
 
