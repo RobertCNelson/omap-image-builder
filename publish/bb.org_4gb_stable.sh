49ba8992686210e35836ca3899db1f1a72708e40
Robert Nelson <robertcnelson@gmail.com>
2015-02-13 07:17:28 -0600

    builder: git seems to fail, so lets just test an image that doesnt have any git calls
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- publish/bb.org_4gb_stable.sh
+++ publish/bb.org_4gb_stable.sh
@@ -5,24 +5,26 @@ DIR="$PWD"
 
 export apt_proxy=localhost:3142/
 
-debian_lxde_4gb_stable="debian-7.8-lxde-4gb-armhf-${time}"
+#image_name="debian-7.8-lxde-4gb-armhf-${time}"
+image_name="debian-7.8-console-armhf-${time}"
 
-options="--img-4gb bone-${debian_lxde_4gb_stable} --dtb beaglebone \
+options="--img-4gb bone-${image_name} --dtb beaglebone \
 --beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd \
 --bbb-old-bootloader-in-emmc --hostname beaglebone"
 
-./RootStock-NG.sh -c bb.org-debian-stable-4gb
+#./RootStock-NG.sh -c bb.org-debian-stable-4gb
+./RootStock-NG.sh -c bb.org-console-debian-stable
 
-if [ -d ./deploy/${debian_lxde_4gb_stable} ] ; then
-	cd ./deploy/${debian_lxde_4gb_stable}/
+if [ -d ./deploy/${image_name} ] ; then
+	cd ./deploy/${image_name}/
 	sudo ./setup_sdcard.sh ${options}
 
-	if [ -f bone-${debian_lxde_4gb_stable}-4gb.img ] ; then
-		mv bone-${debian_lxde_4gb_stable}-4gb.img ../
+	if [ -f bone-${image_name}-4gb.img ] ; then
+		mv bone-${image_name}-4gb.img ../
 		cd ../
-		xz -z -3 -v bone-${debian_lxde_4gb_stable}-4gb.img
+		xz -z -3 -v bone-${image_name}-4gb.img
 
-		rm -rf ./deploy/${debian_lxde_4gb_stable}/ || true
+		rm -rf ./deploy/${image_name}/ || true
 	fi
 fi
 
