acabd7e33a68f3724cf6b82f8328b4a91286978e
Robert Nelson <robertcnelson@gmail.com>
2015-02-13 14:13:59 -0600

    build: env didnt give us a the build number....
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- publish/bb.org_4gb_stable.sh
+++ publish/bb.org_4gb_stable.sh
@@ -3,6 +3,9 @@
 time=$(date +%Y-%m-%d)
 DIR="$PWD"
 
+ssh_user="robertcnelson@rcn-ee.net"
+server_dir="/home/robertcnelson/rcn-ee.net"
+
 export apt_proxy=localhost:3142/
 
 #image_name="debian-7.8-lxde-4gb-armhf-${time}"
@@ -11,12 +14,6 @@ image_name="debian-7.8-console-armhf-${time}"
 options="--img-4gb bone-${image_name} --dtb beaglebone \
 --beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd \
 --bbb-old-bootloader-in-emmc --hostname beaglebone"
-
-#dump env, do we have a build number?
-env
-
-#just a one time...
-sudo apt-get install pv
 
 #./RootStock-NG.sh -c bb.org-debian-stable-4gb
 ./RootStock-NG.sh -c bb.org-console-debian-stable
@@ -30,10 +27,11 @@ if [ -d ./deploy/${image_name} ] ; then
 		xz -z -3 -v bone-${image_name}-4gb.img
 
 		#upload:
+		#rsync -e ssh -av ./bone-${image_name}-4gb.img ${ssh_user}:${server_dir}/
 
 		#cleanup:
 		cd ../../
-		rm -rf ./deploy/${image_name}/ || true
+		rm -rf ./deploy/ || true
 	fi
 fi
 
