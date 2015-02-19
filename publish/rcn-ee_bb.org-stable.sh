0052dbe74ca328077971e9fe9338c14b54484ed4
Robert Nelson <robertcnelson@gmail.com>
2015-02-19 13:11:09 -0600

    publish: rewrite rcn-ee_bb.org-stable.sh
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- publish/rcn-ee_bb.org-stable.sh
+++ publish/rcn-ee_bb.org-stable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 time=$(date +%Y-%m-%d)
-stage_dir="/var/www/html/rcn-ee.net/rootfs/bb.org/testing"
+mirror_dir="/var/www/html/rcn-ee.net/rootfs/bb.org/testing"
 DIR="$PWD"
 
 git pull --no-edit https://github.com/beagleboard/image-builder master
@@ -26,10 +26,49 @@ debian_lxqt_4gb_next="debian-jessie-lxqt-4gb-armhf-${time}"
 
 archive="xz -z -8 -v"
 
+beaglebone="--dtb beaglebone --beagleboard.org-production --boot_label BEAGLEBONE \
+--rootfs_label rootfs --bbb-old-bootloader-in-emmc --hostname beaglebone"
+
+bb_blank_flasher="--dtb bbb-blank-eeprom --boot_label BEAGLEBONE \
+--rootfs_label rootfs --bbb-old-bootloader-in-emmc --hostname beaglebone"
+
+beaglebone_console="--dtb beaglebone --boot_label BEAGLEBONE \
+--bbb-old-bootloader-in-emmc --hostname beaglebone"
+
+bb_blank_flasher_console="--dtb bbb-blank-eeprom --boot_label BEAGLEBONE \
+--bbb-old-bootloader-in-emmc --hostname beaglebone"
+
+omap5-uevm="--dtb omap5-uevm --hostname omap5-uevm"
+am57xx-beagle-x15="--dtb am57xx-beagle-x15 --hostname BeagleBoard-X15"
+
 cat > ${DIR}/deploy/gift_wrap_final_images.sh <<-__EOF__
 #!/bin/bash
 
-pre_generic_img () {
+copy_base_rootfs_to_mirror () {
+        if [ -d ${mirror_dir} ] ; then
+                if [ ! -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        mkdir -p ${mirror_dir}/${time}/\${blend}/ || true
+                fi
+                if [ -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        if [ -f \${base_rootfs}.tar.xz ] ; then
+                                cp -v \${base_rootfs}.tar.xz ${mirror_dir}/${time}/\${blend}/
+                        fi
+                fi
+        fi
+}
+
+archive_base_rootfs () {
+        if [ -d ./\${base_rootfs} ] ; then
+                rm -rf \${base_rootfs} || true
+        fi
+
+        if [ ! -f \${base_rootfs}.tar.xz ] ; then
+                ${archive} \${base_rootfs}.tar
+        fi
+        copy_base_rootfs_to_mirror
+}
+
+extract_base_rootfs () {
         if [ -d ./\${base_rootfs} ] ; then
                 rm -rf \${base_rootfs} || true
         fi
@@ -41,170 +80,146 @@ pre_generic_img () {
         fi
 }
 
-generic_img () {
+copy_img_to_mirror () {
+        if [ -d ${mirror_dir} ] ; then
+                if [ ! -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        mkdir -p ${mirror_dir}/${time}/\${blend}/ || true
+                fi
+                if [ -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        if [ -f \${wfile}.xz ] ; then
+                                cp -v \${wfile}.xz ${mirror_dir}/${time}/\${blend}/
+                        fi
+                fi
+        fi
+}
+
+archive_img () {
+        if [ -f \${wfile} ] ; then
+                ${archive} \${wfile}
+                copy_img_to_mirror
+        fi
+}
+
+generate_img () {
         cd \${base_rootfs}/
         sudo ./setup_sdcard.sh \${options}
         mv *.img ../
         cd ..
 }
 
-stage_generic_img () {
-        if [ -d ${stage_dir} ] ; then
-                if [ ! -d ${stage_dir}/${time}/\${stage_blend}/ ] ; then
-                        mkdir -p ${stage_dir}/${time}/\${stage_blend}/ || true
-                fi
-                if [ -d ${stage_dir}/${time}/\${stage_blend}/ ] ; then
-                        if [ -f \${base_rootfs}.tar.xz ] ; then
-                                cp -v \${base_rootfs}.tar.xz ${stage_dir}/${time}/\${stage_blend}/
-                        fi
-                fi
-        fi
-}
-
-post_generic_img () {
-        if [ -d ./\${base_rootfs} ] ; then
-                rm -rf \${base_rootfs} || true
-        fi
-
-        if [ ! -f \${base_rootfs}.tar.xz ] ; then
-                ${archive} \${base_rootfs}.tar
-        fi
-        stage_generic_img
-}
-
-stage_img () {
-        if [ -d ${stage_dir} ] ; then
-                if [ ! -d ${stage_dir}/${time}/\${stage_blend}/ ] ; then
-                        mkdir -p ${stage_dir}/${time}/\${stage_blend}/ || true
-                fi
-                if [ -d ${stage_dir}/${time}/\${stage_blend}/ ] ; then
-                        if [ -f \${wfile}.xz ] ; then
-                                cp -v \${wfile}.xz ${stage_dir}/${time}/\${stage_blend}/
-                        fi
-                fi
-        fi
-}
-
-compress_img () {
-        if [ -f \${wfile} ] ; then
-                ${archive} \${wfile}
-                stage_img
-        fi
-}
-
 ###Production lxde images: (BBB: 4GB eMMC)
 base_rootfs="${debian_lxde_4gb_stable}"
-stage_blend="lxde-4gb"
-pre_generic_img
+blend="lxde-4gb"
+extract_base_rootfs
 
-options="--img-4gb BBB-blank-eMMC-flasher-\${base_rootfs} --dtb bbb-blank-eeprom --beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd --rootfs_label rootfs --bbb-flasher --hostname beaglebone"
-generic_img
-options="--img-4gb BBB-eMMC-flasher-\${base_rootfs}       --dtb beaglebone       --beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd --rootfs_label rootfs --bbb-flasher  --bbb-old-bootloader-in-emmc --hostname beaglebone"
-generic_img
-options="--img-4gb bone-\${base_rootfs}                   --dtb beaglebone       --beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd --bbb-old-bootloader-in-emmc --hostname beaglebone"
-generic_img
+options="--img-4gb BBB-blank-eMMC-flasher-\${base_rootfs} ${bb_blank_flasher} --enable-systemd --bbb-flasher"
+generate_img
+options="--img-4gb BBB-eMMC-flasher-\${base_rootfs} --enable-systemd --bbb-flasher"
+generate_img
+options="--img-4gb bone-\${base_rootfs} ${beaglebone} --enable-systemd"
+generate_img
 
 ###lxde images: (BBB: 2GB eMMC)
 base_rootfs="${debian_lxde_stable}"
-stage_blend="lxde"
-pre_generic_img
+blend="lxde"
+extract_base_rootfs
 
-options="--img-2gb BBB-eMMC-flasher-\${base_rootfs} --dtb beaglebone --beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd --rootfs_label rootfs --bbb-flasher  --bbb-old-bootloader-in-emmc --hostname beaglebone"
-generic_img
+options="--img-2gb BBB-eMMC-flasher-\${base_rootfs} ${beaglebone} --enable-systemd --bbb-flasher"
+generate_img
 
 ###console images: (also single partition)
 base_rootfs="${debian_console_stable}"
-stage_blend="console"
-pre_generic_img
+blend="console"
+extract_base_rootfs
 
-options="--img-2gb BBG-blank-eMMC-flasher-\${base_rootfs} --dtb bbb-blank-eeprom --boot_label BEAGLEBONE --enable-systemd --bbg-flasher --bbb-old-bootloader-in-emmc --hostname beaglebone"
-options="--img-2gb BBB-blank-eMMC-flasher-\${base_rootfs} --dtb bbb-blank-eeprom --boot_label BEAGLEBONE --enable-systemd --bbb-flasher --bbb-old-bootloader-in-emmc --hostname beaglebone"
-options="--img-2gb BBB-eMMC-flasher-\${base_rootfs} --dtb beaglebone --boot_label BEAGLEBONE --enable-systemd --bbb-flasher --bbb-old-bootloader-in-emmc --hostname beaglebone"
-generic_img
-options="--img-2gb bone-\${base_rootfs}             --dtb beaglebone --boot_label BEAGLEBONE --enable-systemd --bbb-old-bootloader-in-emmc --hostname beaglebone"
-generic_img
+options="--img-2gb BBG-blank-eMMC-flasher-\${base_rootfs} ${bb_blank_flasher_console} --enable-systemd --bbg-flasher"
+options="--img-2gb BBB-blank-eMMC-flasher-\${base_rootfs} ${bb_blank_flasher_console} --enable-systemd --bbb-flasher"
+options="--img-2gb BBB-eMMC-flasher-\${base_rootfs} ${beaglebone_console} --enable-systemd --bbb-flasher"
+generate_img
+options="--img-2gb bone-\${base_rootfs} ${beaglebone_console} --enable-systemd"
+generate_img
 
 ###machinekit:
 base_rootfs="${debian_machinekit_wheezy}"
-stage_blend="machinekit"
-pre_generic_img
-options="--img-4gb bone-\${base_rootfs}                   --dtb beaglebone       --beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd --bbb-old-bootloader-in-emmc --hostname beaglebone"
-generic_img
+blend="machinekit"
+extract_base_rootfs
+options="--img-4gb bone-\${base_rootfs} ${beaglebone} --enable-systemd"
+generate_img
 
 ###lxqt image
 base_rootfs="${debian_lxqt_4gb_next}"
-stage_blend="lxqt-4gb"
-pre_generic_img
+blend="lxqt-4gb"
+extract_base_rootfs
 
-options="--img-4gb BBB-eMMC-flasher-\${base_rootfs} --dtb beaglebone        --beagleboard.org-production --boot_label BEAGLEBONE --rootfs_label rootfs --bbb-flasher  --bbb-old-bootloader-in-emmc --hostname beaglebone"
-generic_img
-options="--img-4gb bone-\${base_rootfs}             --dtb beaglebone        --beagleboard.org-production --boot_label BEAGLEBONE --rootfs_label rootfs --bbb-old-bootloader-in-emmc --hostname beaglebone"
-generic_img
-options="--img-4gb bbx15-\${base_rootfs}            --dtb am57xx-beagle-x15 --hostname BeagleBoard-X15"
-generic_img
-options="--img-4gb omap5-uevm-\${base_rootfs}       --dtb omap5-uevm        --hostname omap5-uevm"
-generic_img
+options="--img-4gb BBB-eMMC-flasher-\${base_rootfs} ${beaglebone} --bbb-flasher"
+generate_img
+options="--img-4gb bone-\${base_rootfs} ${beaglebone}"
+generate_img
+options="--img-4gb bbx15-\${base_rootfs} ${am57xx-beagle-x15}"
+generate_img
+options="--img-4gb omap5-uevm-\${base_rootfs} ${omap5-uevm}"
+generate_img
 
 ###archive *.tar
 base_rootfs="${debian_lxde_4gb_stable}"
-stage_blend="lxde-4gb"
-post_generic_img
+blend="lxde-4gb"
+archive_base_rootfs
 
 base_rootfs="${debian_lxde_stable}"
-stage_blend="lxde"
-post_generic_img
+blend="lxde"
+archive_base_rootfs
 
 base_rootfs="${debian_console_stable}"
-stage_blend="console"
-post_generic_img
+blend="console"
+archive_base_rootfs
 
 base_rootfs="${debian_machinekit_wheezy}"
-stage_blend="machinekit"
-post_generic_img
+blend="machinekit"
+archive_base_rootfs
 
 base_rootfs="${debian_lxqt_4gb_next}"
-stage_blend="lxqt-4gb"
-post_generic_img
+blend="lxqt-4gb"
+archive_base_rootfs
 
 ###archive *.img
-stage_blend="lxde-4gb"
+blend="lxde-4gb"
 wfile="BBB-blank-eMMC-flasher-${debian_lxde_4gb_stable}-4gb.img"
-compress_img
+archive_img
 wfile="BBB-eMMC-flasher-${debian_lxde_4gb_stable}-4gb.img"
-compress_img
+archive_img
 wfile="bone-${debian_lxde_4gb_stable}-4gb.img"
-compress_img
+archive_img
 
-stage_blend="lxde"
+blend="lxde"
 wfile="BBB-eMMC-flasher-${debian_lxde_stable}-2gb.img"
-compress_img
+archive_img
 
-stage_blend="console"
+blend="console"
 wfile="BBB-blank-eMMC-flasher-${debian_console_stable}-2gb.img"
-compress_img
+archive_img
 wfile="BBG-blank-eMMC-flasher-${debian_console_stable}-2gb.img"
-compress_img
+archive_img
 wfile="BBB-eMMC-flasher-${debian_console_stable}-2gb.img"
-compress_img
+archive_img
 wfile="bone-${debian_console_stable}-2gb.img"
-compress_img
+archive_img
 
-stage_blend="machinekit"
+blend="machinekit"
 wfile="bone-${debian_machinekit_wheezy}-4gb.img"
-compress_img
+archive_img
 
-stage_blend="lxqt-4gb"
+blend="lxqt-4gb"
 wfile="BBB-eMMC-flasher-${debian_lxqt_4gb_next}-4gb.img"
-compress_img
+archive_img
 
 wfile="bone-${debian_lxqt_4gb_next}-4gb.img"
-compress_img
+archive_img
 
 wfile="bbx15-${debian_lxqt_4gb_next}-4gb.img"
-compress_img
+archive_img
 
 wfile="omap5-uevm-${debian_lxqt_4gb_next}-4gb.img"
-compress_img
+archive_img
 
 __EOF__
 
