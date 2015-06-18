9e89a673861fbd27c30ac78fbdec816cb2f14a21
Robert Nelson <robertcnelson@gmail.com>
2015-06-18 12:18:16 -0500

    bb.org: build bot
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- publish/bb.org_4gb_stable.sh
+++ publish/bb.org_4gb_stable.sh
@@ -10,20 +10,30 @@ server_dir="/var/lib/buildbot/masters/kernel-buildbot/public_html/images/${branc
 
 export apt_proxy=localhost:3142/
 
-image_name="debian-7.8-lxde-4gb-armhf-${time}"
-#image_name="debian-7.8-console-armhf-${time}"
+##Debian 7:
+#image_name="debian-7.8-lxde-4gb-armhf-${time}"
+#size="4gb"
 
-options="--img-4gb bone-${image_name} --dtb beaglebone \
---beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd \
---bbb-old-bootloader-in-emmc --hostname beaglebone"
+#options="--img-4gb bone-${image_name} --dtb beaglebone \
+#--beagleboard.org-production --boot_label BEAGLEBONE --enable-systemd \
+#--bbb-old-bootloader-in-emmc --hostname beaglebone"
 
-./RootStock-NG.sh -c bb.org-debian-wheezy-lxde-4gb
-#./RootStock-NG.sh -c bb.org-debian-wheezy-console
+#./RootStock-NG.sh -c bb.org-debian-wheezy-lxde-4gb
+
+##Debian 8:
+image_name="debian-8.1-lxqt-2gb-armhf-${time}"
+size="2gb"
+
+options="--img-2gb bone-${image_name} --dtb beaglebone \
+--beagleboard.org-production --boot_label BEAGLEBONE \
+--rootfs_label rootfs --bbb-old-bootloader-in-emmc --hostname beaglebone"
+
+./RootStock-NG.sh -c bb.org-debian-jessie-lxqt-2gb-v4.1
 
 keep_net_alive () {
 	while : ; do
 		sleep 15
-		echo "size: [`ls -lh ./bone-${image_name}-4gb.img.xz`]"
+		echo "size: [`ls -lh ./bone-${image_name}-${size}.img.xz`]"
 	done
 }
 
@@ -31,16 +41,16 @@ if [ -d ./deploy/${image_name} ] ; then
 	cd ./deploy/${image_name}/
 	sudo ./setup_sdcard.sh ${options}
 
-	if [ -f bone-${image_name}-4gb.img ] ; then
-		sudo chown buildbot.buildbot bone-${image_name}-4gb.img
+	if [ -f bone-${image_name}-${size}.img ] ; then
+		sudo chown buildbot.buildbot bone-${image_name}-${size}.img
 
 		keep_net_alive & KEEP_NET_ALIVE_PID=$!
 
-		xz -z -3 -v -v --verbose bone-${image_name}-4gb.img
+		xz -z -3 -v -v --verbose bone-${image_name}-${size}.img
 
 		#upload:
 		ssh ${ssh_user} mkdir -p ${server_dir}
-		rsync -e ssh -av ./bone-${image_name}-4gb.img.xz ${ssh_user}:${server_dir}/
+		rsync -e ssh -av ./bone-${image_name}-${size}.img.xz ${ssh_user}:${server_dir}/
 
 		[ -e /proc/$KEEP_NET_ALIVE_PID ] && sudo kill $KEEP_NET_ALIVE_PID
 
