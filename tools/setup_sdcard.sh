0b8d9ce26c58824b1d29ddf7a105c9f99268c6c4
Robert Nelson <robertcnelson@gmail.com>
2015-07-06 10:20:17 -0500

    setup: add google dns by default...
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -1158,9 +1158,11 @@ populate_rootfs () {
 		if [ "${DISABLE_ETH}" ] ; then
 			echo "#auto eth0" >> ${wfile}
 			echo "#iface eth0 inet dhcp" >> ${wfile}
+			echo "#    dns-nameservers 8.8.8.8 8.8.4.4" >> ${wfile}
 		else
 			echo "auto eth0"  >> ${wfile}
 			echo "iface eth0 inet dhcp" >> ${wfile}
+			echo "    dns-nameservers 8.8.8.8 8.8.4.4" >> ${wfile}
 		fi
 
 		#if we have systemd & wicd-gtk, disable eth0 in /etc/network/interfaces
@@ -1169,6 +1171,7 @@ populate_rootfs () {
 				sed -i 's/auto eth0/#auto eth0/g' ${wfile}
 				sed -i 's/allow-hotplug eth0/#allow-hotplug eth0/g' ${wfile}
 				sed -i 's/iface eth0 inet dhcp/#iface eth0 inet dhcp/g' ${wfile}
+				sed -i 's/dns-nameservers 8.8.8.8 8.8.4.4/#dns-nameservers 8.8.8.8 8.8.4.4/g' ${wfile}
 			fi
 		fi
 
@@ -1177,6 +1180,7 @@ populate_rootfs () {
 			sed -i 's/auto eth0/#auto eth0/g' ${wfile}
 			sed -i 's/allow-hotplug eth0/#allow-hotplug eth0/g' ${wfile}
 			sed -i 's/iface eth0 inet dhcp/#iface eth0 inet dhcp/g' ${wfile}
+			sed -i 's/dns-nameservers 8.8.8.8 8.8.4.4/#dns-nameservers 8.8.8.8 8.8.4.4/g' ${wfile}
 		fi
 
 		echo "# Example to keep MAC address between reboots" >> ${wfile}
