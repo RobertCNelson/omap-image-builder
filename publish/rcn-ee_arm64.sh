e6d45c3650ed443d25485d953c7ed7fa6955af2d
Robert Nelson <robertcnelson@gmail.com>
2015-07-07 19:52:24 -0500

    arm64: test
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ publish/rcn-ee_arm64.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+time=$(date +%Y-%m-%d)
+DIR="$PWD"
+
+export apt_proxy=apt-proxy:3142/
+
+./RootStock-NG.sh -c rcn-ee_console_debian_stretch_arm64
+
+debian_stretch="debian-stretch"
+archive="xz -z -8 -v"
+
+cat > ${DIR}/deploy/gift_wrap_final_images.sh <<-__EOF__
+#!/bin/bash
+
+${archive} ${debian_jessie}-console-arm64-${time}.tar
+
+__EOF__
+
+chmod +x ${DIR}/deploy/gift_wrap_final_images.sh
+
+if [ ! -d /mnt/farm/images/ ] ; then
+	#nfs mount...
+	sudo mount -a
+fi
+
+if [ -d /mnt/farm/images/ ] ; then
+	mkdir /mnt/farm/images/${time}/
+	cp -v ${DIR}/deploy/*.tar /mnt/farm/images/${time}/
+	cp -v ${DIR}/deploy/gift_wrap_final_images.sh /mnt/farm/images/${time}/gift_wrap_final_images.sh
+	chmod +x /mnt/farm/images/${time}/gift_wrap_final_images.sh
+fi
