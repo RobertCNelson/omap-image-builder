d667cdf260ad95103032631219e8c93175ba4a55
Robert Nelson <robertcnelson@gmail.com>
2015-09-23 13:06:05 -0500

    cleanup: scripts/install_dependencies.sh: http://www.shellcheck.net/#
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -32,27 +32,27 @@ debootstrap_is_installed () {
 	if [ "x${host_arch}" != "xarmv7l" ] ; then
 		if [ "x${host_arch}" != "xaarch64" ] ; then
 			dpkg -l | grep qemu-user-static >/dev/null || deb_pkgs="${deb_pkgs}qemu-user-static "
-			dpkg -l | grep `dpkg --print-architecture` | grep -v "qemu-" | grep qemu >/dev/null || deb_pkgs="${deb_pkgs}qemu "
+			dpkg -l | grep $(dpkg --print-architecture) | grep -v "qemu-" | grep qemu >/dev/null || deb_pkgs="${deb_pkgs}qemu "
 		fi
 	fi
 
 	if [ "${deb_pkgs}" ] ; then
 		echo "Installing: ${deb_pkgs}"
 		sudo apt-get update
-		sudo apt-get -y install ${deb_pkgs}
+		sudo apt-get -y install "${deb_pkgs}"
 	fi
 }
 
 debootstrap_what_version () {
 	test_debootstrap=$(/usr/sbin/debootstrap --version | awk '{print $2}' | awk -F"." '{print $3}')
-	echo "Log: debootstrap version: 1.0."$test_debootstrap""
+	echo "Log: debootstrap version: 1.0.$test_debootstrap"
 }
 
 debootstrap_is_installed
 debootstrap_what_version
 
 if [[ "$test_debootstrap" < "$minimal_debootstrap" ]] ; then
-	echo "Log: Installing minimal debootstrap version: 1.0."${minimal_debootstrap}"..."
+	echo "Log: Installing minimal debootstrap version: 1.0.${minimal_debootstrap}..."
 	wget https://rcn-ee.com/mirror/debootstrap/debootstrap_1.0.${minimal_debootstrap}_all.deb
 	sudo dpkg -i debootstrap_1.0.${minimal_debootstrap}_all.deb
 	rm -rf debootstrap_1.0.${minimal_debootstrap}_all.deb || true
