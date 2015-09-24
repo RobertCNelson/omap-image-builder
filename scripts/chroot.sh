da23a01b3553a9d8585fcf17f11cc3fe2868a2da
Robert Nelson <robertcnelson@gmail.com>
2015-09-24 08:25:33 -0500

    fix: another tempdir messup
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -1057,7 +1057,7 @@ if [ -n "${chroot_script}" -a -r "${DIR}/target/chroot/${chroot_script}" ] ; the
 	echo "Calling chroot_script script: ${chroot_script}"
 	sudo cp -v "${DIR}/.project" "${tempdir}/etc/oib.project"
 	sudo cp -v "${DIR}/target/chroot/${chroot_script}" "${tempdir}/final.sh"
-	sudo chroot "${tempdir} /bin/sh" -e final.sh
+	sudo chroot "${tempdir}" /bin/sh -e final.sh
 	sudo rm -f "${tempdir}/final.sh" || true
 	sudo rm -f "${tempdir}/etc/oib.project" || true
 	chroot_script=""
