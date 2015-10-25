b0389941b029aa083ed92570edffbd6cb5c4283f
Robert Nelson <robertcnelson@gmail.com>
2015-10-25 15:25:02 -0500

    target: disable PyBBIO till serbus is fixed
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- target/chroot/beagleboard.org-console-jessie.sh
+++ target/chroot/beagleboard.org-console-jessie.sh
@@ -437,12 +437,12 @@ install_git_repos () {
 	git_target_dir="/opt/source/PyBBIO"
 	git_clone
 	if [ -f ${git_target_dir}/.git/config ] ; then
-		cd ${git_target_dir}/
-		if [ -f /usr/bin/dtc ] ; then
-			sed -i "s/PLATFORM = ''/PLATFORM = 'BeagleBone >=3.8'/g" setup.py
-			python setup.py install
-		fi
-		cd /
+		#cd ${git_target_dir}/
+		#if [ -f /usr/bin/dtc ] ; then
+		#	sed -i "s/PLATFORM = ''/PLATFORM = 'BeagleBone >=3.8'/g" setup.py
+		#	python setup.py install
+		#fi
+		#cd /
 	fi
 
 	git_repo="https://github.com/RobertCNelson/dtb-rebuilder.git"
--- target/chroot/machinekit-wheezy.sh
+++ target/chroot/machinekit-wheezy.sh
@@ -458,12 +458,12 @@ install_git_repos () {
 	git_target_dir="/opt/source/PyBBIO"
 	git_clone
 	if [ -f ${git_target_dir}/.git/config ] ; then
-		cd ${git_target_dir}/
-		if [ -f /usr/bin/dtc ] ; then
-			sed -i "s/PLATFORM = ''/PLATFORM = 'BeagleBone >=3.8'/g" setup.py
-			python setup.py install
-		fi
-		cd /
+		#cd ${git_target_dir}/
+		#if [ -f /usr/bin/dtc ] ; then
+		#	sed -i "s/PLATFORM = ''/PLATFORM = 'BeagleBone >=3.8'/g" setup.py
+		#	python setup.py install
+		#fi
+		#cd /
 	fi
 
 	git_repo="git://git.ti.com/pru-software-support-package/pru-software-support-package.git"
