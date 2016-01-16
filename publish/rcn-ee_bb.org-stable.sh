3a2b282ddf9cd93ea88a5c026e4ef3257900702e
Robert Nelson <robertcnelson@gmail.com>
2016-01-16 00:02:53 -0600

    bb.org: jessie: disable nodejs 4.2.x test
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- publish/rcn-ee_bb.org-stable.sh
+++ publish/rcn-ee_bb.org-stable.sh
@@ -24,7 +24,7 @@ debian_jessie_lxqt_2gb="debian-8.2-lxqt-2gb-armhf-${time}"
 debian_jessie_lxqt_4gb="debian-8.2-lxqt-4gb-armhf-${time}"
 debian_jessie_console="debian-8.2-console-armhf-${time}"
 debian_jessie_usbflasher="debian-8.2-usbflasher-armhf-${time}"
-debian_jessie_lxqt_4gb_nodejs="debian-8.2-lxqt-4gb-nodejs-4.2.x-armhf-${time}"
+#debian_jessie_lxqt_4gb_nodejs="debian-8.2-lxqt-4gb-nodejs-4.2.x-armhf-${time}"
 
 archive="xz -z -8"
 
