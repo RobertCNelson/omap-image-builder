99626c07f5ee03e560a6c7122417a015a2e47cb7
Robert Nelson <robertcnelson@gmail.com>
2016-03-04 15:48:02 -0600

    chroot: install rtl8723bu-modules if available
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -617,6 +617,7 @@ cat > "${DIR}/chroot_script.sh" <<-__EOF__
 		if [ ! "x${repo_rcnee_pkg_version}" = "x" ] ; then
 			echo "Log: (chroot) Installing modules for: ${repo_rcnee_pkg_version}"
 			apt-get -y --force-yes install mt7601u-modules-${repo_rcnee_pkg_version} || true
+			apt-get -y --force-yes install rtl8723bu-modules-${repo_rcnee_pkg_version} || true
 			depmod -a ${repo_rcnee_pkg_version}
 			update-initramfs -u -k ${repo_rcnee_pkg_version}
 		fi
