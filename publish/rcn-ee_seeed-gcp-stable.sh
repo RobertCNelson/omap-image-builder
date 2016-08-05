56fbdfec53736dba8dd066e7044f9d02263ab034
Robert Nelson <robertcnelson@gmail.com>
2016-08-05 11:04:34 -0500

    seeed: add gcp iot image
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ publish/rcn-ee_seeed-gcp-stable.sh
@@ -0,0 +1,152 @@
+#!/bin/bash -e
+
+time=$(date +%Y-%m-%d)
+mirror_dir="/var/www/html/rcn-ee.us/rootfs/bb.org/testing"
+DIR="$PWD"
+
+git pull --no-edit https://github.com/beagleboard/image-builder master
+
+export apt_proxy=apt-proxy:3142/
+
+if [ -d ./deploy ] ; then
+	sudo rm -rf ./deploy || true
+fi
+
+./RootStock-NG.sh -c seeed-debian-jessie-gcp-iot-v4.4
+
+debian_jessie_seeed_gcp_iot="debian-8.5-seeed-gcp-iot-armhf-${time}"
+
+archive="xz -z -8"
+
+beaglebone="--dtb beaglebone --bbb-old-bootloader-in-emmc \
+--rootfs_label rootfs --hostname beaglebone --enable-cape-universal"
+
+arduino_tre="--dtb am335x-arduino-tre --boot_label ARDUINO-TRE \
+--rootfs_label rootfs --hostname arduino-tre"
+
+omap5_uevm="--dtb omap5-uevm --rootfs_label rootfs --hostname omap5-uevm"
+beagle_x15="--dtb am57xx-beagle-x15 --rootfs_label rootfs \
+--hostname BeagleBoard-X15"
+
+cat > ${DIR}/deploy/gift_wrap_final_images.sh <<-__EOF__
+#!/bin/bash
+
+copy_base_rootfs_to_mirror () {
+        if [ -d ${mirror_dir}/ ] ; then
+                if [ ! -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        mkdir -p ${mirror_dir}/${time}/\${blend}/ || true
+                fi
+                if [ -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        if [ ! -f ${mirror_dir}/${time}/\${blend}/\${base_rootfs}.tar.xz ] ; then
+                                cp -v \${base_rootfs}.tar ${mirror_dir}/${time}/\${blend}/
+                                cd ${mirror_dir}/${time}/\${blend}/
+                                ${archive} \${base_rootfs}.tar && sha256sum \${base_rootfs}.tar.xz > \${base_rootfs}.tar.xz.sha256sum &
+                                cd -
+                        fi
+                fi
+        fi
+}
+
+archive_base_rootfs () {
+        if [ -d ./\${base_rootfs} ] ; then
+                rm -rf \${base_rootfs} || true
+        fi
+        if [ -f \${base_rootfs}.tar ] ; then
+                copy_base_rootfs_to_mirror
+        fi
+}
+
+extract_base_rootfs () {
+        if [ -d ./\${base_rootfs} ] ; then
+                rm -rf \${base_rootfs} || true
+        fi
+
+        if [ -f \${base_rootfs}.tar.xz ] ; then
+                tar xf \${base_rootfs}.tar.xz
+        fi
+
+        if [ -f \${base_rootfs}.tar ] ; then
+                tar xf \${base_rootfs}.tar
+        fi
+}
+
+copy_img_to_mirror () {
+        if [ -d ${mirror_dir} ] ; then
+                if [ ! -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        mkdir -p ${mirror_dir}/${time}/\${blend}/ || true
+                fi
+                if [ -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        if [ -f \${wfile}.bmap ] ; then
+                                mv -v \${wfile}.bmap ${mirror_dir}/${time}/\${blend}/
+                                sync
+                        fi
+                        if [ ! -f ${mirror_dir}/${time}/\${blend}/\${wfile}.img.zx ] ; then
+                                mv -v \${wfile}.img ${mirror_dir}/${time}/\${blend}/
+                                sync
+                                if [ -f \${wfile}.img.xz.job.txt ] ; then
+                                        mv -v \${wfile}.img.xz.job.txt ${mirror_dir}/${time}/\${blend}/
+                                        sync
+                                fi
+                                cd ${mirror_dir}/${time}/\${blend}/
+                                ${archive} \${wfile}.img && sha256sum \${wfile}.img.xz > \${wfile}.img.xz.sha256sum &
+                                cd -
+                        fi
+                fi
+        fi
+}
+
+archive_img () {
+        if [ -f \${wfile}.img ] ; then
+                if [ ! -f \${wfile}.bmap ] ; then
+                        if [ -f /usr/bin/bmaptool ] ; then
+                                bmaptool create -o \${wfile}.bmap \${wfile}.img
+                        fi
+                fi
+                copy_img_to_mirror
+        fi
+}
+
+generate_img () {
+        if [ -d \${base_rootfs}/ ] ; then
+                cd \${base_rootfs}/
+                sudo ./setup_sdcard.sh \${options}
+                sudo chown \${SUDO_USER}:\${SUDO_USER} *.img || true
+                sudo chown \${SUDO_USER}:\${SUDO_USER} *.job.txt || true
+                mv *.img ../ || true
+                mv *.job.txt ../ || true
+                cd ..
+        fi
+}
+
+###seeed gcp iot image
+base_rootfs="${debian_jessie_seeed_gcp_iot}" ; blend="seeed-gcp-iot" ; extract_base_rootfs
+
+options="--img-4gb bone-\${base_rootfs}       ${beaglebone}"                ; generate_img
+options="--img-4gb BBGW-blank-\${base_rootfs} ${beaglebone} --bbgw-flasher" ; generate_img
+
+###archive *.tar
+base_rootfs="${debian_jessie_seeed_gcp_iot}"      ; blend="seeed-gcp-iot"      ; archive_base_rootfs
+
+###archive *.img
+base_rootfs="${debian_jessie_seeed_gcp_iot}" ; blend="seeed-gcp-iot"
+
+wfile="bone-\${base_rootfs}-4gb"       ; archive_img
+wfile="BBGW-blank-\${base_rootfs}-4gb" ; archive_img
+
+__EOF__
+
+chmod +x ${DIR}/deploy/gift_wrap_final_images.sh
+
+if [ ! -d /mnt/farm/images/ ] ; then
+	#nfs mount...
+	sudo mount -a
+fi
+
+if [ -d /mnt/farm/images/ ] ; then
+	mkdir /mnt/farm/images/${time}/
+	echo "Copying: *.tar to server: images/${time}/"
+	cp -v ${DIR}/deploy/*.tar /mnt/farm/images/${time}/
+	cp -v ${DIR}/deploy/gift_wrap_final_images.sh /mnt/farm/images/${time}/gift_wrap_final_images.sh
+	chmod +x /mnt/farm/images/${time}/gift_wrap_final_images.sh
+fi
+
