e452e8e165c066efb901f2e5d4bdbe7508de40d7
Robert Nelson <robertcnelson@gmail.com>
2016-09-13 09:27:00 -0500

    bbb: u-boot patch bump (v2016.09)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- target/chroot/beagleboard.org-wheezy.sh
+++ target/chroot/beagleboard.org-wheezy.sh
@@ -23,7 +23,7 @@
 export LC_ALL=C
 
 chromium_release="chromium-33.0.1750.117"
-u_boot_release="v2016.09-rc2"
+u_boot_release="v2016.09"
 u_boot_release_x15="ti-2016.05"
 
 #contains: rfs_username, release_date
