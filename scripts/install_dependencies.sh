fd428d253c45174a98715859c08688d1f19ef87f
Robert Nelson <robertcnelson@gmail.com>
2016-09-22 10:45:05 -0500

    debootstrap: 1.0.83
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -22,7 +22,7 @@
 
 #http://ftp.us.debian.org/debian/pool/main/d/debootstrap/
 #1.0.${minimal_debootstrap}
-minimal_debootstrap="81"
+minimal_debootstrap="83"
 host_arch="$(uname -m)"
 
 debootstrap_is_installed () {
