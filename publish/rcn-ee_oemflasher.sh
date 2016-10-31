57bae42729dd6476fcaca3f8d78d13ead359d52b
Robert Nelson <robertcnelson@gmail.com>
2016-10-31 10:47:30 -0500

    add oemflasher
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ publish/rcn-ee_oemflasher.sh
@@ -0,0 +1,150 @@
+#!/bin/bash -e
+
+time=$(date +%Y-%m-%d)
+mirror_dir="/var/www/html/rcn-ee.us/rootfs/bb.org/testing"
+DIR="$PWD"
+
+git pull --no-edit https://github.com/beagleboard/image-builder master
+
+export apt_proxy=apt-proxy:3142/
+
+if [ -d ./deploy ] ; then
+	sudo rm -rf ./deploy || true
+fi
+
+./RootStock-NG.sh -c bb.org-debian-jessie-oemflasher
+
+debian_jessie_oemflasher="debian-8.6-oemflasher-armhf-${time}"
+
+archive="xz -z -8"
+
+
+cat > ${DIR}/deploy/gift_wrap_final_images.sh <<-__EOF__
+#!/bin/bash
+
+copy_base_rootfs_to_mirror () {
+        if [ -d ${mirror_dir}/ ] ; then
+                if [ ! -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        mkdir -p ${mirror_dir}/${time}/\${blend}/ || true
+                fi
+                if [ -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        if [ ! -f ${mirror_dir}/${time}/\${blend}/\${base_rootfs}.tar.xz ] ; then
+                                cp -v \${base_rootfs}.tar ${mirror_dir}/${time}/\${blend}/
+                                cd ${mirror_dir}/${time}/\${blend}/
+                                ${archive} \${base_rootfs}.tar && sha256sum \${base_rootfs}.tar.xz > \${base_rootfs}.tar.xz.sha256sum &
+                                cd -
+                        fi
+                fi
+        fi
+}
+
+archive_base_rootfs () {
+        if [ -d ./\${base_rootfs} ] ; then
+                rm -rf \${base_rootfs} || true
+        fi
+        if [ -f \${base_rootfs}.tar ] ; then
+                copy_base_rootfs_to_mirror
+        fi
+}
+
+extract_base_rootfs () {
+        if [ -d ./\${base_rootfs} ] ; then
+                rm -rf \${base_rootfs} || true
+        fi
+
+        if [ -f \${base_rootfs}.tar.xz ] ; then
+                tar xf \${base_rootfs}.tar.xz
+        fi
+
+        if [ -f \${base_rootfs}.tar ] ; then
+                tar xf \${base_rootfs}.tar
+        fi
+}
+
+copy_img_to_mirror () {
+        if [ -d ${mirror_dir} ] ; then
+                if [ ! -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        mkdir -p ${mirror_dir}/${time}/\${blend}/ || true
+                fi
+                if [ -d ${mirror_dir}/${time}/\${blend}/ ] ; then
+                        if [ -f \${wfile}.bmap ] ; then
+                                mv -v \${wfile}.bmap ${mirror_dir}/${time}/\${blend}/
+                                sync
+                        fi
+                        if [ ! -f ${mirror_dir}/${time}/\${blend}/\${wfile}.img.zx ] ; then
+                                mv -v \${wfile}.img ${mirror_dir}/${time}/\${blend}/
+                                sync
+                                if [ -f \${wfile}.img.xz.job.txt ] ; then
+                                        mv -v \${wfile}.img.xz.job.txt ${mirror_dir}/${time}/\${blend}/
+                                        sync
+                                fi
+                                cd ${mirror_dir}/${time}/\${blend}/
+                                ${archive} \${wfile}.img && sha256sum \${wfile}.img.xz > \${wfile}.img.xz.sha256sum &
+                                cd -
+                        fi
+                fi
+        fi
+}
+
+archive_img () {
+        if [ -f \${wfile}.img ] ; then
+                if [ ! -f \${wfile}.bmap ] ; then
+                        if [ -f /usr/bin/bmaptool ] ; then
+                                bmaptool create -o \${wfile}.bmap \${wfile}.img
+                        fi
+                fi
+                copy_img_to_mirror
+        fi
+}
+
+generate_img () {
+        if [ -d \${base_rootfs}/ ] ; then
+                cd \${base_rootfs}/
+                sudo ./setup_sdcard.sh \${options}
+                sudo chown 1000:1000 *.img || true
+                sudo chown 1000:1000 *.job.txt || true
+                mv *.img ../ || true
+                mv *.job.txt ../ || true
+                cd ..
+        fi
+}
+
+###oemflasher images: (also single partition)
+base_rootfs="${debian_jessie_oemflasher}" ; blend="oemflasher" ; extract_base_rootfs
+
+###archive *.tar
+base_rootfs="${debian_jessie_oemflasher}" ; blend="oemflasher" ; archive_base_rootfs
+
+#
+base_rootfs="${debian_jessie_oemflasher}" ; blend="oemflasher"
+
+__EOF__
+
+chmod +x ${DIR}/deploy/gift_wrap_final_images.sh
+
+image_prefix="oemflasher"
+#node:
+if [ ! -d /var/www/html/farm/images/ ] ; then
+	if [ ! -d /mnt/farm/images/ ] ; then
+		#nfs mount...
+		sudo mount -a
+	fi
+
+	if [ -d /mnt/farm/images/ ] ; then
+		mkdir -p /mnt/farm/images/${image_prefix}-${time}/ || true
+		echo "Copying: *.tar to server: images/${image_prefix}-${time}/"
+		cp -v ${DIR}/deploy/*.tar /mnt/farm/images/${image_prefix}-${time}/ || true
+		cp -v ${DIR}/deploy/gift_wrap_final_images.sh /mnt/farm/images/${image_prefix}-${time}/gift_wrap_final_images.sh || true
+		chmod +x /mnt/farm/images/${image_prefix}-${time}/gift_wrap_final_images.sh || true
+	fi
+fi
+
+#x86:
+if [ -d /var/www/html/farm/images/ ] ; then
+	mkdir -p /var/www/html/farm/images/${image_prefix}-${time}/ || true
+	echo "Copying: *.tar to server: images/${image_prefix}-${time}/"
+	cp -v ${DIR}/deploy/gift_wrap_final_images.sh /var/www/html/farm/images/${image_prefix}-${time}/gift_wrap_final_images.sh || true
+	chmod +x /var/www/html/farm/images/${image_prefix}-${time}/gift_wrap_final_images.sh || true
+	sudo chown -R apt-cacher-ng:apt-cacher-ng /var/www/html/farm/images/${image_prefix}-${time}/ || true
+fi
+
