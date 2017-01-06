353a86e7722e5ffbfa7a621f32c63be08c599c17
Robert Nelson <robertcnelson@gmail.com>
2017-01-05 20:24:08 -0600

    weekly: renable 2gb
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- publish/rcn-ee_bb.org-stable.sh
+++ publish/rcn-ee_bb.org-stable.sh
@@ -17,7 +17,7 @@ if [ ! -f jenkins.build ] ; then
 ./RootStock-NG.sh -c machinekit-debian-jessie
 ./RootStock-NG.sh -c bb.org-debian-jessie-console-v4.4
 ./RootStock-NG.sh -c bb.org-debian-jessie-iot-v4.4
-#./RootStock-NG.sh -c bb.org-debian-jessie-lxqt-2gb-v4.4
+./RootStock-NG.sh -c bb.org-debian-jessie-lxqt-2gb-v4.4
 ./RootStock-NG.sh -c bb.org-debian-jessie-lxqt-4gb-v4.4
 ./RootStock-NG.sh -c bb.org-debian-jessie-lxqt-4gb-xm
 
