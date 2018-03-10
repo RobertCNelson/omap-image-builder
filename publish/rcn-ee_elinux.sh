ed162ef21eff39f317a74f0fd76d8a971b9d371f
Robert Nelson <robertcnelson@gmail.com>
2018-03-10 07:54:29 -0600

    debian: 9.4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- publish/rcn-ee_elinux.sh
+++ publish/rcn-ee_elinux.sh
@@ -19,7 +19,7 @@ else
 	mkdir -p ${DIR}/deploy/ || true
 fi
 
- debian_stable="debian-9.3-console-armhf-${time}"
+ debian_stable="debian-9.4-console-armhf-${time}"
 debian_testing="debian-buster-console-armhf-${time}"
  ubuntu_stable="ubuntu-16.04.4-console-armhf-${time}"
 ubuntu_testing="ubuntu-bionic-console-armhf-${time}"
