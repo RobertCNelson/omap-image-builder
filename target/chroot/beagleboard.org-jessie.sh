b0fc6564c676ae29208318791d1bd5d81cd54b95
Robert Nelson <robertcnelson@gmail.com>
2018-04-12 12:00:06 -0500

    bb.org: jessie: add rcpy/pyctrl from stretch
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- target/chroot/beagleboard.org-jessie.sh
+++ target/chroot/beagleboard.org-jessie.sh
@@ -306,6 +306,26 @@ install_git_repos () {
 	git_target_dir="/opt/source/Robotics_Cape_Installer"
 	git_clone
 
+	git_repo="https://github.com/mcdeoliveira/rcpy"
+	git_target_dir="/opt/source/rcpy"
+	git_clone
+	if [ -f ${git_target_dir}/.git/config ] ; then
+		cd ${git_target_dir}/
+		if [ -f /usr/bin/python3 ] && [ -f /usr/bin/easy_install ] ; then
+			/usr/bin/python3 setup.py install
+		fi
+	fi
+
+	git_repo="https://github.com/mcdeoliveira/pyctrl"
+	git_target_dir="/opt/source/pyctrl"
+	git_clone
+	if [ -f ${git_target_dir}/.git/config ] ; then
+		cd ${git_target_dir}/
+		if [ -f /usr/bin/python3 ] && [ -f /usr/bin/easy_install ] ; then
+			/usr/bin/python3 setup.py install
+		fi
+	fi
+
 	git_repo="https://github.com/mvduin/py-uio"
 	git_target_dir="/opt/source/py-uio"
 	git_clone
