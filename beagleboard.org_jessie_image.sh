ff497e1e73967d0959552110f668e2b0c984c0ed
Robert Nelson <robertcnelson@gmail.com>
2019-04-03 11:08:40 -0500

    cleanup: beagleboard.org_jessie_image.sh
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- beagleboard.org_jessie_image.sh
+++ beagleboard.org_jessie_image.sh
@@ -3,27 +3,23 @@
 time=$(date +%Y-%m-%d)
 DIR="$PWD"
 
+./RootStock-NG.sh -c bb.org-debian-jessie-console-v4.4
 ./RootStock-NG.sh -c bb.org-debian-jessie-lxqt-2gb-v4.4
 ./RootStock-NG.sh -c bb.org-debian-jessie-lxqt-4gb-v4.4
-./RootStock-NG.sh -c bb.org-debian-jessie-console-v4.4
 
-debian_jessie_lxqt_2gb="debian-8.11-lxqt-2gb-armhf-${time}"
-debian_jessie_lxqt_4gb="debian-8.11-lxqt-4gb-armhf-${time}"
- debian_jessie_console="debian-8.11-console-armhf-${time}"
+       debian_jessie_console="debian-8.11-console-armhf-${time}"
+      debian_jessie_lxqt_2gb="debian-8.11-lxqt-2gb-armhf-${time}"
+      debian_jessie_lxqt_4gb="debian-8.11-lxqt-4gb-armhf-${time}"
 
 archive="xz -z -8 -v"
 
-beaglebone="--dtb beaglebone --hostname beaglebone"
+beaglebone="--dtb beaglebone --rootfs_label rootfs --hostname beaglebone --enable-cape-universal"
+pru_rproc_v44ti="--enable-uboot-pru-rproc-44ti"
 
-bb_blank_flasher="--dtb bbb-blank-eeprom --hostname beaglebone"
+beagle_xm="--dtb omap3-beagle-xm --rootfs_label rootfs --hostname beagleboard"
 
-beaglebone_console="--dtb beaglebone --hostname beaglebone"
-
-bb_blank_flasher_console="--dtb bbb-blank-eeprom --hostname beaglebone"
-
-omap3_beagle_xm="--dtb omap3-beagle-xm --hostname BeagleBoard"
-omap5_uevm="--dtb omap5-uevm --hostname omap5-uevm"
-am57xx_beagle_x15="--dtb am57xx-beagle-x15 --hostname BeagleBoard-X15"
+beagle_x15="--dtb am57xx-beagle-x15 --rootfs_label rootfs \
+--hostname BeagleBoard-X15"
 
 cat > ${DIR}/deploy/gift_wrap_final_images.sh <<-__EOF__
 #!/bin/bash
@@ -76,46 +72,49 @@ generate_img () {
         fi
 }
 
-###lxqt-4gb image
-base_rootfs="${debian_jessie_lxqt_4gb}" ; blend="lxqt-4gb" ; extract_base_rootfs
 
-options="--img-4gb BBB-eMMC-flasher-\${base_rootfs} ${beaglebone} --emmc-flasher" ; generate_img
-options="--img-4gb bone-\${base_rootfs} ${beaglebone}" ; generate_img
-options="--img-4gb am57xx-eMMC-flasher-\${base_rootfs} ${am57xx_beagle_x15} --emmc-flasher" ; generate_img
-options="--img-4gb am57xx-\${base_rootfs} ${am57xx_beagle_x15}" ; generate_img
-
-###lxqt-2gb image
-base_rootfs="${debian_jessie_lxqt_2gb}" ; blend="lxqt-2gb" ; extract_base_rootfs
-
-options="--img-2gb BBB-eMMC-flasher-\${base_rootfs} ${beaglebone} --bbb-flasher" ; generate_img
-
-###console images: (also single partition)
+###console images (jessie):
 base_rootfs="${debian_jessie_console}" ; blend="console" ; extract_base_rootfs
 
-options="--img-2gb BBB-eMMC-flasher-\${base_rootfs} ${beaglebone_console} --emmc-flasher" ; generate_img
-options="--img-2gb bone-\${base_rootfs} ${beaglebone_console}" ; generate_img
-options="--img-2gb am57xx-eMMC-flasher-\${base_rootfs} ${am57xx_beagle_x15} --emmc-flasher" ; generate_img
-options="--img-2gb am57xx-\${base_rootfs} ${am57xx_beagle_x15}" ; generate_img
+options="--img-1gb am57xx-\${base_rootfs}       ${beagle_x15}"                ; generate_img
+options="--img-1gb bone-\${base_rootfs}        ${beaglebone} ${pru_rproc_v44ti}"   ; generate_img
+
+###lxqt-2gb image (jessie):
+base_rootfs="${debian_jessie_lxqt_2gb}" ; blend="lxqt-2gb" ; extract_base_rootfs
+
+options="--img-2gb BBB-blank-\${base_rootfs} ${beaglebone}  ${pru_rproc_v44ti} --emmc-flasher" ; generate_img
+
+###lxqt-4gb image (jessie):
+base_rootfs="${debian_jessie_lxqt_4gb}" ; blend="lxqt-4gb" ; extract_base_rootfs
+
+options="--img-4gb am57xx-\${base_rootfs}       ${beagle_x15}"                             ; generate_img
+options="--img-4gb am57xx-blank-\${base_rootfs} ${beagle_x15} --emmc-flasher --am57xx-x15-revc-flasher" ; generate_img
+options="--img-4gb bone-\${base_rootfs}        ${beaglebone} ${pru_rproc_v44ti}"                ; generate_img
+options="--img-4gb BBB-blank-\${base_rootfs}   ${beaglebone} ${pru_rproc_v44ti} --emmc-flasher" ; generate_img
 
 ###archive *.tar
-base_rootfs="${debian_jessie_lxqt_4gb}" ; blend="lxqt-4gb" ; archive_base_rootfs
-base_rootfs="${debian_jessie_lxqt_2gb}" ; blend="lxqt-2gb" ; archive_base_rootfs
-base_rootfs="${debian_jessie_console}" ; blend="console" ; archive_base_rootfs
+base_rootfs="${debian_jessie_console}"        ; blend="console"         ; archive_base_rootfs
+base_rootfs="${debian_jessie_lxqt_2gb}"       ; blend="lxqt-2gb"        ; archive_base_rootfs
+base_rootfs="${debian_jessie_lxqt_4gb}"       ; blend="lxqt-4gb"        ; archive_base_rootfs
 
-blend="lxqt-4gb"
-wfile="BBB-eMMC-flasher-${debian_jessie_lxqt_4gb}-4gb" ; archive_img
-wfile="bone-${debian_jessie_lxqt_4gb}-4gb" ; archive_img
-wfile="am57xx-eMMC-flasher-${debian_jessie_lxqt_4gb}-4gb" ; archive_img
-wfile="am57xx-${debian_jessie_lxqt_4gb}-4gb" ; archive_img
+###console images (jessie):
+base_rootfs="${debian_jessie_console}" ; blend="console"
 
-blend="lxqt-2gb"
-wfile="BBB-eMMC-flasher-${debian_jessie_lxqt_2gb}-2gb" ; archive_img
+wfile="am57xx-\${base_rootfs}-1gb"       ; archive_img
+wfile="bone-\${base_rootfs}-1gb"        ; archive_img
 
-blend="console"
-wfile="BBB-eMMC-flasher-${debian_jessie_console}-2gb" ; archive_img
-wfile="bone-${debian_jessie_console}-2gb" ; archive_img
-wfile="am57xx-eMMC-flasher-${debian_jessie_console}-2gb" ; archive_img
-wfile="am57xx-${debian_jessie_console}-2gb" ; archive_img
+###lxqt-2gb image (jessie):
+base_rootfs="${debian_jessie_lxqt_2gb}" ; blend="lxqt-2gb"
+
+wfile="BBB-blank-\${base_rootfs}-2gb"      ; archive_img
+
+###lxqt-4gb image (jessie):
+base_rootfs="${debian_jessie_lxqt_4gb}" ; blend="lxqt-4gb"
+
+wfile="am57xx-\${base_rootfs}-4gb"       ; archive_img
+wfile="am57xx-blank-\${base_rootfs}-4gb" ; archive_img
+wfile="bone-\${base_rootfs}-4gb"        ; archive_img
+wfile="BBB-blank-\${base_rootfs}-4gb"   ; archive_img
 
 __EOF__
 
