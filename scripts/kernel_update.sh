5b183eef047784d8f4c04ed0855e9446c6f1496b
Robert Nelson <robertcnelson@gmail.com>
2019-04-11 09:07:58 -0500

    kernel bump: 5.0.x-xM: (5.0.7-armv7-x9)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/kernel_update.sh
+++ scripts/kernel_update.sh
@@ -29,10 +29,10 @@ current_kernel () {
 }
 
 if [ -f configs/kernel.data ] ; then
-	git_msg="4.19.x-xM"
+	git_msg="5.0.x-xM"
 #	var="armv7"      ; ver="LTS414"       ; current_kernel
-	var="armv7"      ; ver="LTS419"       ; current_kernel
-#	var="armv7"      ; ver="STABLE"       ; current_kernel
+#	var="armv7"      ; ver="LTS419"       ; current_kernel
+	var="armv7"      ; ver="STABLE"       ; current_kernel
 #	var="armv7"      ; ver="TESTING"      ; current_kernel
 #	var="armv7"      ; ver="EXPERIMENTAL" ; current_kernel
 
