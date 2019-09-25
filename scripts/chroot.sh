cd8a8f6ca7ce1e0ef8532617f8967762eaf3fa46
Robert Nelson <robertcnelson@gmail.com>
2019-09-25 08:51:27 -0500

    bb.org: buster/bionic use cmem 4.16.00.00
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -740,7 +740,13 @@ cat > "${DIR}/chroot_script.sh" <<-__EOF__
 			apt-get -y install libpruio-modules-${repo_rcnee_pkg_version} || true
 			apt-get -y install rtl8723bu-modules-${repo_rcnee_pkg_version} || true
 			apt-get -y install rtl8821cu-modules-${repo_rcnee_pkg_version} || true
-			apt-get -y install ti-cmem-modules-${repo_rcnee_pkg_version} || true
+
+			if [ ! "x${repo_rcnee_cmem_version}" = "x" ] ; then
+				apt-get -y install ti-cmem-${repo_rcnee_cmem_version}-modules-${repo_rcnee_pkg_version} || true
+			else
+				apt-get -y install ti-cmem-modules-${repo_rcnee_pkg_version} || true
+			fi
+
 			depmod -a ${repo_rcnee_pkg_version}
 			update-initramfs -u -k ${repo_rcnee_pkg_version}
 		fi
