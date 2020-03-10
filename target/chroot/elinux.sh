fa9f5121d0aa9bdbca123645d147d295105af543
Robert Nelson <robertcnelson@gmail.com>
2020-03-10 09:45:41 -0500

    cleanup elinux target
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- target/chroot/elinux.sh
+++ target/chroot/elinux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 #
-# Copyright (c) 2014-2019 Robert Nelson <robertcnelson@gmail.com>
+# Copyright (c) 2014-2020 Robert Nelson <robertcnelson@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -95,13 +95,12 @@ install_git_repos () {
 		git_clone
 		if [ -f ${git_target_dir}/.git/config ] ; then
 			cd ${git_target_dir}/
-			sed -i -e 's:4.1.0:3.4.0:g' setup.py
-			if [ -f /usr/bin/python2 ] ; then
-				python2 setup.py install || true
-			fi
+			sed -i -e 's:4.1.0:3.4.0:g' setup.py || true
+			sed -i -e "s/strict-aliasing/strict-aliasing', '-Wno-cast-function-type', '-Wno-format-truncation', '-Wno-sizeof-pointer-memaccess', '-Wno-stringop-overflow/g" setup.py || true
 			if [ -f /usr/bin/python3 ] ; then
 				python3 setup.py install || true
 			fi
+			git reset HEAD --hard || true
 		fi
 	fi
 
@@ -133,13 +132,31 @@ install_git_repos () {
 	git_target_dir="/opt/source/bb.org-overlays"
 	git_clone
 
-	git_repo="https://github.com/mcdeoliveira/rcpy"
-	git_target_dir="/opt/source/rcpy"
-	git_clone
+	if [ -f /usr/lib/librobotcontrol.so ] ; then
+		git_repo="https://github.com/StrawsonDesign/librobotcontrol"
+		git_target_dir="/opt/source/librobotcontrol"
+		git_clone
 
-	git_repo="https://github.com/mcdeoliveira/pyctrl"
-	git_target_dir="/opt/source/pyctrl"
-	git_clone
+		git_repo="https://github.com/mcdeoliveira/rcpy"
+		git_target_dir="/opt/source/rcpy"
+		git_clone
+		if [ -f ${git_target_dir}/.git/config ] ; then
+			cd ${git_target_dir}/
+			if [ -f /usr/bin/python3 ] ; then
+				/usr/bin/python3 setup.py install
+			fi
+		fi
+
+		git_repo="https://github.com/mcdeoliveira/pyctrl"
+		git_target_dir="/opt/source/pyctrl"
+		git_clone
+		if [ -f ${git_target_dir}/.git/config ] ; then
+			cd ${git_target_dir}/
+			if [ -f /usr/bin/python3 ] ; then
+				/usr/bin/python3 setup.py install
+			fi
+		fi
+	fi
 
 	git_repo="https://github.com/mvduin/py-uio"
 	git_target_dir="/opt/source/py-uio"
@@ -159,7 +176,4 @@ if [ -f /usr/bin/git ] ; then
 	git config --global --unset-all user.name
 	chown ${rfs_username}:${rfs_username} /home/${rfs_username}/.gitconfig
 fi
-#install_build_pkgs
-#other_source_links
-#unsecure_root
-#todo
+#
