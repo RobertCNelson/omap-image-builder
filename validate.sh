3ed175f6a64ba4fc2668c0d110d892745069d95e
Robert Nelson <robertcnelson@gmail.com>
2021-04-22 09:32:26 -0500

    ci: enable bb.org-debian-bullseye-console-v5.4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- validate.sh
+++ validate.sh
@@ -7,5 +7,5 @@ if [ -d ./deploy ] ; then
 fi
 
 ./RootStock-NG.sh -c bb.org-debian-buster-console-v4.19
-#./RootStock-NG.sh -c bb.org-debian-bullseye-console-v5.4
+./RootStock-NG.sh -c bb.org-debian-bullseye-console-v5.4
 #./RootStock-NG.sh -c bb.org-debian-bullseye-console-arm64
