ef009ecfcff1eab0e89ab7afabe38fe589993fb4
Robert Nelson <robertcnelson@gmail.com>
2021-05-14 16:16:52 -0500

    beaglev: use label
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -1561,7 +1561,11 @@ populate_rootfs () {
 			if [ "${BTRFS_FSTAB}" ] ; then
 				echo "${rootfs_drive}  /  btrfs  defaults,noatime  0  1" >> ${wfile}
 			else
-				echo "${rootfs_drive}  /  ${ROOTFS_TYPE}  noatime,errors=remount-ro  0  1" >> ${wfile}
+				if [ "x${fstab_use_label}" = "xenable" ] ; then
+					echo "LABEL=rootfs  /  ${ROOTFS_TYPE}  noatime,errors=remount-ro  0  1" >> ${wfile}
+				else
+					echo "${rootfs_drive}  /  ${ROOTFS_TYPE}  noatime,errors=remount-ro  0  1" >> ${wfile}
+				fi
 			fi
 		fi
 
