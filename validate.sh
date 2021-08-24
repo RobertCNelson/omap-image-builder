37b2a620e15df53f847090c4bc4ebfcbc4ef507c
Robert Nelson <robertcnelson@gmail.com>
2021-08-24 17:54:05 -0500

    ci: debian-sid-console-riscv64 is too unstable for ci
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- validate.sh
+++ validate.sh
@@ -10,7 +10,7 @@ fi
 ./RootStock-NG.sh -c octavo-debian-buster-console-v4.19
 ./RootStock-NG.sh -c bb.org-debian-bullseye-console-v5.10-ti-armhf
 ./RootStock-NG.sh -c bb.org-debian-bullseye-console-arm64
-./RootStock-NG.sh -c bb.org-debian-sid-console-riscv64
+#./RootStock-NG.sh -c bb.org-debian-sid-console-riscv64
 
 if [ -d ./ignore ] ; then
 	sudo rm -rf ./ignore || true
