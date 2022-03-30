abf526183ae515ff63aad0b43fdc3145cd199466
Robert Nelson <robertcnelson@gmail.com>
2022-03-30 14:12:39 -0500

    Debian 11: 11.3
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- publish/rcn-ee_bb.org-stable.sh
+++ publish/rcn-ee_bb.org-stable.sh
@@ -56,9 +56,9 @@ debian_buster_iot_grove_kit="debian-10.12-iot-grove-kit-armhf-${time}"
       ubuntu_bionic_ros_iot="ubuntu-18.04.6-ros-iot-armhf-${time}"
 
 
-debian_bullseye_minimal="debian-11.2-minimal-armhf-${time}"
-    debian_bullseye_iot="debian-11.2-iot-armhf-${time}"
-   debian_bullseye_xfce="debian-11.2-xfce-armhf-${time}"
+debian_bullseye_minimal="debian-11.3-minimal-armhf-${time}"
+    debian_bullseye_iot="debian-11.3-iot-armhf-${time}"
+   debian_bullseye_xfce="debian-11.3-xfce-armhf-${time}"
 
 xz_img="xz -T4 -z -8"
 xz_tar="xz -T4 -z -8"
