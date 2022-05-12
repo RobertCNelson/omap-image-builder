9128ee7425c96aa71000ec154148f74a20b8803c
Robert Nelson <robertcnelson@gmail.com>
2022-05-12 12:38:48 -0500

    chroot: cleanup text
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -723,46 +723,46 @@ cat > "${DIR}/chroot_script.sh" <<-__EOF__
 	}
 
 	install_pkgs () {
-        if [ ! "x${deb_additional_pkgs}" = "x" ] ; then
-            #Install the user choosen list.
-            echo "Log: (chroot) Installing: ${deb_additional_pkgs}"
-            apt-get update
-            apt-get -y install ${deb_additional_pkgs}
-        fi
-  
-        if [ ! "x${deb_console_application_pkgs}" = "x" ] ; then
-            #Install the user choosen list.
-            echo "Log: (chroot) Installing: ${deb_console_application_pkgs}"
-            apt-get update
-            apt-get -y install ${deb_console_application_pkgs}
-        fi
+		if [ ! "x${deb_additional_pkgs}" = "x" ] ; then
+			#Install the user choosen list.
+			echo "Log: (chroot) Installing: ${deb_additional_pkgs}"
+			apt-get update
+			apt-get -y install ${deb_additional_pkgs}
+		fi
 
-        if [ ! "x${deb_desktop_prerequisite_pkgs}" = "x" ] ; then
-            #Install the user choosen list.
-            echo "Log: (chroot) Installing: ${deb_desktop_common_pkgs}"
-            apt-get update
-            apt-get -y install ${deb_desktop_common_pkgs}
-        fi
+		if [ ! "x${deb_console_application_pkgs}" = "x" ] ; then
+			#Install the user choosen list.
+			echo "Log: (chroot) Installing: ${deb_console_application_pkgs}"
+			apt-get update
+			apt-get -y install ${deb_console_application_pkgs}
+		fi
 
-        if [ ! "x${deb_desktop_pkgs}" = "x" ] ; then
-            #Install the user choosen list.
-            echo "Log: (chroot) Installing: ${deb_desktop_pkgs}"
-            apt-get update
-            apt-get -y install ${deb_desktop_pkgs}
-        fi
+		if [ ! "x${deb_desktop_prerequisite_pkgs}" = "x" ] ; then
+			#Install the user choosen list.
+			echo "Log: (chroot) Installing: ${deb_desktop_common_pkgs}"
+			apt-get update
+			apt-get -y install ${deb_desktop_common_pkgs}
+		fi
 
-        if [ ! "x${deb_desktop_application_pkgs}" = "x" ] ; then
-            #Install the user choosen list.
-            echo "Log: (chroot) Installing: ${deb_desktop_application_pkgs}"
-            apt-get update
-            apt-get -y install ${deb_desktop_application_pkgs}
-        fi
-  
-        if [ ! "x${deb_purge_pkgs}" = "x" ] ; then
-            #Install the user choosen list.
-            echo "Log: (chroot) Removing: ${deb_purge_pkgs}"
-            apt-get purge -y ${deb_purge_pkgs}
-        fi
+		if [ ! "x${deb_desktop_pkgs}" = "x" ] ; then
+			#Install the user choosen list.
+			echo "Log: (chroot) Installing: ${deb_desktop_pkgs}"
+			apt-get update
+			apt-get -y install ${deb_desktop_pkgs}
+		fi
+
+		if [ ! "x${deb_desktop_application_pkgs}" = "x" ] ; then
+			#Install the user choosen list.
+			echo "Log: (chroot) Installing: ${deb_desktop_application_pkgs}"
+			apt-get update
+			apt-get -y install ${deb_desktop_application_pkgs}
+		fi
+
+		if [ ! "x${deb_purge_pkgs}" = "x" ] ; then
+			#Install the user choosen list.
+			echo "Log: (chroot) Removing: ${deb_purge_pkgs}"
+			apt-get purge -y ${deb_purge_pkgs}
+		fi
 
 		if [ "x${chroot_enable_debian_backports}" = "xenable" ] ; then
 			if [ ! "x${chroot_debian_backports_pkg_list}" = "x" ] ; then
