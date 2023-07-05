ba6c4d2831f35e15d3a7895ccca38a0a27aac4b5
Robert Nelson <robertcnelson@gmail.com>
2023-07-05 08:54:19 -0500

    add: daily-debian-arm64-12-bookworm-xfce-v5.10-ti-j721e.sh
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ci/daily-debian-arm64-12-bookworm-xfce-v5.10-ti-j721e.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+export apt_proxy=192.168.1.12:3142/
+
+config=bb.org-debian-bookworm-xfce-v5.10-ti-arm64-k3-j721e
+filesize=10gb
+rootfs="debian-arm64-12-bookworm-xfce-v5.10-ti"
+
+if [ -d ./deploy ] ; then
+	sudo rm -rf ./deploy || true
+fi
+
+touch .notar
+echo "./RootStock-NG.sh -c ${config}"
+./RootStock-NG.sh -c ${config}
+
+source .project
+
+if [ -d ./deploy/${export_filename}/ ] ; then
+	cd ./deploy/${export_filename}/
+
+	echo "sudo ./setup_sdcard.sh --img-${filesize} bbai64-${export_filename} --dtb bbai64 --hostname BeagleBone-AI64"
+	sudo ./setup_sdcard.sh --img-${filesize} bbai64-${export_filename} --dtb bbai64 --hostname BeagleBone-AI64
+	mv ./*.img ../
+
+	echo "sudo ./setup_sdcard.sh --img-${filesize} bbai64-emmc-flasher-${export_filename} --dtb bbai64 --enable-extlinux-flasher --hostname BeagleBone-AI64"
+	sudo ./setup_sdcard.sh --img-${filesize} bbai64-emmc-flasher-${export_filename} --dtb bbai64 --enable-extlinux-flasher --hostname BeagleBone-AI64
+	mv ./*.img ../
+
+	cd ..
+
+	device="bbai64"
+	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	echo "Compressing...${device}-${export_filename}-${filesize}.img"
+	xz -T4 -z ${device}-${export_filename}-${filesize}.img
+	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+
+	device="bbai64-emmc-flasher"
+	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	echo "Compressing...${device}-${export_filename}-${filesize}.img"
+	xz -T4 -z ${device}-${export_filename}-${filesize}.img
+	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+
+	#echo "Compressing...${export_filename}.tar"
+	#xz -T4 -z ${export_filename}.tar
+	#sha256sum ${export_filename}.tar.xz > ${export_filename}.tar.xz.sha256sum
+	#sudo -uvoodoo cp -v ./${export_filename}.tar.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	#sudo -uvoodoo cp -v ./${export_filename}.tar.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+else
+	exit 2
+fi
+
