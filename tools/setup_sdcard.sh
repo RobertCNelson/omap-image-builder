29d7dc55f0da7255814c0562334e9311381156b4
Robert Nelson <robertcnelson@gmail.com>
2023-07-07 14:00:58 -0500

    sdcard: cleanup: sfdisk_partition_one_endmb -> partition_one_end_mb
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -456,25 +456,25 @@ unmount_all_drive_partitions () {
 sfdisk_partition_layout () {
 	sfdisk_options="--force --in-order --Linux --unit M"
 	sfdisk_partition_one="${conf_boot_startmb}"
-	sfdisk_partition_one_endmb="${conf_boot_endmb}"
-	sfdisk_rootfs_startmb=$(($sfdisk_partition_one + $sfdisk_partition_one_endmb))
+	partition_one_end_mb="${conf_boot_endmb}"
+	sfdisk_rootfs_startmb=$(($sfdisk_partition_one + $partition_one_end_mb))
 
 	test_sfdisk=$(LC_ALL=C sfdisk --help | grep -m 1 -e "--in-order" || true)
 	if [ "x${test_sfdisk}" = "x" ] ; then
 		echo "log: sfdisk: 2.26.x or greater detected"
 		sfdisk_options="--force ${sfdisk_gpt}"
 		sfdisk_partition_one="${sfdisk_partition_one}M"
-		sfdisk_partition_one_endmb="${sfdisk_partition_one_endmb}M"
+		partition_one_end_mb="${partition_one_end_mb}M"
 		sfdisk_rootfs_startmb="${sfdisk_rootfs_startmb}M"
 	fi
 
 	echo "sfdisk: [$(LC_ALL=C sfdisk --version)]"
 	echo "sfdisk: [${sfdisk_options} ${media}]"
-	echo "sfdisk: [${sfdisk_partition_one},${sfdisk_partition_one_endmb},${sfdisk_partition_one_fstype},*]"
+	echo "sfdisk: [${sfdisk_partition_one},${partition_one_end_mb},${sfdisk_partition_one_fstype},*]"
 	echo "sfdisk: [${sfdisk_rootfs_startmb},,,-]"
 
 	LC_ALL=C sfdisk ${sfdisk_options} "${media}" <<-__EOF__
-		${sfdisk_partition_one},${sfdisk_partition_one_endmb},${sfdisk_partition_one_fstype},*
+		${sfdisk_partition_one},${partition_one_end_mb},${sfdisk_partition_one_fstype},*
 		${sfdisk_rootfs_startmb},,,-
 	__EOF__
 
