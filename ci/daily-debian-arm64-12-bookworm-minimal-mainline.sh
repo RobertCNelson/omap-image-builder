8416bb9d7f22f9d48c346de153e7963658266508
Robert Nelson <robertcnelson@gmail.com>
2023-08-07 18:47:00 -0500

    ci: cleanup generate json
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ci/daily-debian-arm64-12-bookworm-minimal-mainline.sh
+++ ci/daily-debian-arm64-12-bookworm-minimal-mainline.sh
@@ -6,6 +6,37 @@ config=bb.org-debian-bookworm-minimal-mainline-arm64
 filesize=6gb
 rootfs="debian-arm64-12-bookworm-minimal-mainline"
 
+compress_snapshot_image () {
+	json_file="${device}-${export_filename}-${filesize}.img.xz.json"
+	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sync
+	echo "        {" >> ${json_file}
+	echo "            \"name\": \"Debian 12 ${image_type} (${deb_arch})\"," >> ${json_file}
+	echo "            \"description\": \"A port of Debian Bookworm with the ${image_type} package set\"," >> ${json_file}
+	echo "            \"icon\": \"https://rcn-ee.net/rootfs/release/BorisImageWriter.png\"," >> ${json_file}
+	echo "            \"url\": \"https://rcn-ee.net/rootfs/${rootfs}/${time}/${device}-${export_filename}-${filesize}.img.xz\"," >> ${json_file}
+	extract_size=$(du -b ./${device}-${export_filename}-${filesize}.img | awk '{print $1}')
+	echo "            \"extract_size\": ${extract_size}," >> ${json_file}
+	extract_sha256=$(sha256sum ./${device}-${export_filename}-${filesize}.img | awk '{print $1}')
+	echo "            \"extract_sha256\": \"${extract_sha256}\"," >> ${json_file}
+
+	echo "Compressing...${device}-${export_filename}-${filesize}.img"
+	xz -T4 -z ${device}-${export_filename}-${filesize}.img
+	sync
+
+	image_download_size=$(du -b ./${device}-${export_filename}-${filesize}.img.xz | awk '{print $1}')
+	echo "            \"image_download_size\": ${image_download_size}," >> ${json_file}
+	echo "            \"release_date\": \"${time}\"," >> ${json_file}
+	echo "            \"init_format\": \"systemd\"" >> ${json_file}
+	echo "        }," >> ${json_file}
+	sync
+
+	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.json /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+}
+
 if [ -d ./deploy ] ; then
 	sudo rm -rf ./deploy || true
 fi
@@ -37,37 +68,10 @@ if [ -d ./deploy/${export_filename}/ ] ; then
 
 	cd ../
 
-	device="bbai64"
-	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	echo "Compressing...${device}-${export_filename}-${filesize}.img"
-	xz -T4 -z ${device}-${export_filename}-${filesize}.img
-	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
-	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-
-	device="bbai64-ti-2023.04"
-	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	echo "Compressing...${device}-${export_filename}-${filesize}.img"
-	xz -T4 -z ${device}-${export_filename}-${filesize}.img
-	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
-	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-
-	device="beagleplay"
-	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	echo "Compressing...${device}-${export_filename}-${filesize}.img"
-	xz -T4 -z ${device}-${export_filename}-${filesize}.img
-	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
-	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-
-	device="beagleplay-ti-2023.04"
-	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	echo "Compressing...${device}-${export_filename}-${filesize}.img"
-	xz -T4 -z ${device}-${export_filename}-${filesize}.img
-	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
-	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	device="bbai64" ; compress_snapshot_image
+	device="bbai64-ti-2023.04" ; compress_snapshot_image
+	device="beagleplay" ; compress_snapshot_image
+	device="beagleplay-ti-2023.04" ; compress_snapshot_image
 
 	#echo "Compressing...${export_filename}.tar"
 	#xz -T4 -z ${export_filename}.tar
