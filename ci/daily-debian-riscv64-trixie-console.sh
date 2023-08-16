d5102fca33d56a4bb8f5fdbc3e5f7e8d4f4d6124
Robert Nelson <robertcnelson@gmail.com>
2023-08-16 10:39:57 -0500

    try trixie
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ci/daily-debian-riscv64-trixie-console.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+export apt_proxy=192.168.1.12:3142/
+
+config=bb.org-debian-trixie-console-riscv64
+filesize=4gb
+rootfs="debian-riscv64-trixie-minimal"
+
+if [ -d ./deploy ] ; then
+	sudo rm -rf ./deploy || true
+fi
+
+echo "./RootStock-NG.sh -c ${config}"
+./RootStock-NG.sh -c ${config}
+
+source .project
+
+if [ -f ./deploy/${export_filename}.tar ] ; then
+	cd ./deploy/${export_filename}/
+
+	#setup stuff...
+
+	cd ..
+
+	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+
+	echo "Compressing...${export_filename}.tar"
+	xz -T4 -z ${export_filename}.tar
+	sha256sum ${export_filename}.tar.xz > ${export_filename}.tar.xz.sha256sum
+	sudo -uvoodoo cp -v ./${export_filename}.tar.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sudo -uvoodoo cp -v ./${export_filename}.tar.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	echo "${export_filename}.tar.xz" > latest
+	sudo -uvoodoo cp -v ./latest /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/
+
+	rm -rf ${tempdir} || true
+else
+	echo "failure"
+	exit 2
+fi
+#
