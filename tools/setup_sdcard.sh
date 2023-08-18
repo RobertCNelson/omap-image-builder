59ca3b7d08b2120cdce347b6e38b3b2007ab3db4
Robert Nelson <robertcnelson@gmail.com>
2023-08-18 14:58:29 -0500

    sdcard: stick vendor dtb under their own directory now too
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/setup_sdcard.sh
+++ tools/setup_sdcard.sh
@@ -1717,7 +1717,9 @@ populate_rootfs () {
 		fi
 		if [ ! "x${extlinux_dtb_vendor}" = "x" ] ; then
 			if [ ! "x${extlinux_dtb_fam}" = "x" ] ; then
-				cp -v ${TEMPDIR}/disk/boot/dtbs/${select_kernel}/${extlinux_dtb_vendor}/${extlinux_dtb_fam}*dtb ${TEMPDIR}/disk/boot/firmware/
+				mkdir -p ${TEMPDIR}/disk/boot/firmware/${extlinux_dtb_vendor}/ || true
+				cp ${TEMPDIR}/disk/boot/dtbs/${select_kernel}/${extlinux_dtb_vendor}/${extlinux_dtb_fam}*dtb ${TEMPDIR}/disk/boot/firmware/
+				cp -v ${TEMPDIR}/disk/boot/dtbs/${select_kernel}/${extlinux_dtb_vendor}/${extlinux_dtb_fam}*dtb ${TEMPDIR}/disk/boot/firmware/${extlinux_dtb_vendor}/
 				if [ -d ${TEMPDIR}/disk/boot/dtbs/${select_kernel}/${extlinux_dtb_vendor}/overlays/ ] ; then
 					mkdir -p ${TEMPDIR}/disk/boot/firmware/overlays/
 					cp -v ${TEMPDIR}/disk/boot/dtbs/${select_kernel}/${extlinux_dtb_vendor}/overlays/*.dtbo ${TEMPDIR}/disk/boot/firmware/overlays/
