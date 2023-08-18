1ade52b34697e1c20765d9013409fd243e8e54d8
Robert Nelson <robertcnelson@gmail.com>
2023-08-18 12:28:29 -0500

    daily-debian-arm64-12-bookworm-minimal-v5.10-ti-am62 disble debian rootfs
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ci/daily-debian-arm64-12-bookworm-minimal-v5.10-ti-am62.sh
+++ ci/daily-debian-arm64-12-bookworm-minimal-v5.10-ti-am62.sh
@@ -63,11 +63,11 @@ if [ -d ./deploy/${export_filename}/ ] ; then
 	device="beagleplay" ; compress_snapshot_image
 	device="beagleplay-emmc-flasher" ; compress_snapshot_image
 
-	echo "Compressing...${export_filename}.tar"
-	xz -T4 -z ${export_filename}.tar
-	sha256sum ${export_filename}.tar.xz > ${export_filename}.tar.xz.sha256sum
-	sudo -uvoodoo cp -v ./${export_filename}.tar.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	sudo -uvoodoo cp -v ./${export_filename}.tar.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	#echo "Compressing...${export_filename}.tar"
+	#xz -T4 -z ${export_filename}.tar
+	#sha256sum ${export_filename}.tar.xz > ${export_filename}.tar.xz.sha256sum
+	#sudo -uvoodoo cp -v ./${export_filename}.tar.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	#sudo -uvoodoo cp -v ./${export_filename}.tar.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
 
 	rm -rf ${tempdir} || true
 else
