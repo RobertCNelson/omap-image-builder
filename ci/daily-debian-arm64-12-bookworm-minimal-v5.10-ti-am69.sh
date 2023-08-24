0b42cdb75f1cc43c5139c3efa9772938ebf41ced
Robert Nelson <robertcnelson@gmail.com>
2023-08-24 10:04:53 -0500

    fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ci/daily-debian-arm64-12-bookworm-minimal-v5.10-ti-am69.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+export apt_proxy=192.168.1.12:3142/
+
+config=bb.org-debian-bookworm-minimal-v5.10-ti-arm64-k3-am69
+filesize=6gb
+rootfs="debian-arm64-12-bookworm-minimal-v5.10-ti"
+
+compress_snapshot_image () {
+	json_file="${device}-${export_filename}-${filesize}.img.xz.json"
+	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sync
+
+	echo "        {" >> ${json_file}
+	echo "            \"name\": \"Debian 12 ${image_type} (${deb_arch})\"," >> ${json_file}
+	echo "            \"description\": \"A port of Debian Bookworm with the ${image_type} package set\"," >> ${json_file}
+	echo "            \"icon\": \"https://rcn-ee.net/rootfs/release/BorisImageWriter.png\"," >> ${json_file}
+	echo "            \"url\": \"https://rcn-ee.net/rootfs/${rootfs}/${time}/${device}-${export_filename}-${filesize}.img.xz\"," >> ${json_file}
+	extract_size=$(du -b ./${device}-${export_filename}-${filesize}.img | awk '{print $1}')
+	echo "            \"extract_size\": ${extract_size}," >> ${json_file}
+	extract_sha256=$(sha256sum ./${device}-${export_filename}-${filesize}.img | awk '{print $1}')
+	echo "            \"extract_sha256\": \"${extract_sha256}\"," >> ${json_file}
+
+	echo "Compressing...${device}-${export_filename}-${filesize}.img"
+	xz -T4 -z ${device}-${export_filename}-${filesize}.img
+	sync
+
+	image_download_size=$(du -b ./${device}-${export_filename}-${filesize}.img.xz | awk '{print $1}')
+	echo "            \"image_download_size\": ${image_download_size}," >> ${json_file}
+	echo "            \"release_date\": \"${time}\"," >> ${json_file}
+	echo "            \"init_format\": \"systemd\"" >> ${json_file}
+	echo "        }," >> ${json_file}
+	sync
+
+	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.json /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+}
+
+if [ -d ./deploy ] ; then
+	sudo rm -rf ./deploy || true
+fi
+
+touch .notar
+echo "./RootStock-NG.sh -c ${config}"
+./RootStock-NG.sh -c ${config}
+
+source .project
+
+if [ -d ./deploy/${export_filename}/ ] ; then
+	cd ./deploy/${export_filename}/
+
+	echo "sudo ./setup_sdcard.sh --img-${filesize} am69-${export_filename} --dtb sk-am69"
+	sudo ./setup_sdcard.sh --img-${filesize} am69-${export_filename} --dtb sk-am69
+	mv ./*.img ../
+
+	cd ../
+
+	device="am69" ; compress_snapshot_image
+
+	#echo "Compressing...${export_filename}.tar"
+	#xz -T4 -z ${export_filename}.tar
+	#sha256sum ${export_filename}.tar.xz > ${export_filename}.tar.xz.sha256sum
+	#sudo -uvoodoo cp -v ./${export_filename}.tar.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	#sudo -uvoodoo cp -v ./${export_filename}.tar.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+
+	rm -rf ${tempdir} || true
+else
+	echo "failure"
+	exit 2
+fi
+#
