3f1860b064dac44c2cb6cb0f1ff14c9a54bac02c
Robert Nelson <robertcnelson@gmail.com>
2023-12-01 16:28:22 -0600

    RootStock-NG: update local gitea mirror
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- RootStock-NG.sh
+++ RootStock-NG.sh
@@ -181,7 +181,7 @@ git_trees () {
 
 	git_project_name="linux-firmware"
 	if [ -f ./.gitea.mirror ] ; then
-		git_clone_address="https://gitea.gfnd.rcn-ee.org/kernel.org/linux-firmware.git"
+		git_clone_address="git@gitea.gfnd.rcn-ee.org:kernel.org/linux-firmware.git"
 		generic_git_mirror
 	else
 		git_clone_address="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git"
