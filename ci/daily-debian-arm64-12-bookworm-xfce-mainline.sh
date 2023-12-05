069ed106a06637efa6806cbbeabd31346a7c1a9e
Robert Nelson <robertcnelson@gmail.com>
2023-12-05 09:40:22 -0600

    daily-debian-arm64-12-bookworm-xfce-mainline: would like flasher
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ci/daily-debian-arm64-12-bookworm-xfce-mainline.sh
+++ ci/daily-debian-arm64-12-bookworm-xfce-mainline.sh
@@ -10,9 +10,8 @@ compress_snapshot_image () {
 	json_file="${device}-${export_filename}-${filesize}.img.xz.json"
 	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
 	sync
+
 	echo "        {" >> ${json_file}
-	echo "            \"name\": \"Debian 12 ${image_type} (${deb_arch})\"," >> ${json_file}
-	echo "            \"description\": \"A port of Debian Bookworm with the ${image_type} package set\"," >> ${json_file}
 	echo "            \"icon\": \"https://rcn-ee.net/rootfs/release/BorisImageWriter.png\"," >> ${json_file}
 	echo "            \"url\": \"https://rcn-ee.net/rootfs/${rootfs}/${time}/${device}-${export_filename}-${filesize}.img.xz\"," >> ${json_file}
 	extract_size=$(du -b ./${device}-${export_filename}-${filesize}.img | awk '{print $1}')
@@ -20,18 +19,21 @@ compress_snapshot_image () {
 	extract_sha256=$(sha256sum ./${device}-${export_filename}-${filesize}.img | awk '{print $1}')
 	echo "            \"extract_sha256\": \"${extract_sha256}\"," >> ${json_file}
 
-	echo "Compressing...${device}-${export_filename}-${filesize}.img"
+	echo "Creating... ${device}-${export_filename}-${filesize}.bmap"
+	bmaptool -d create -o ./${device}-${export_filename}-${filesize}.bmap ./${device}-${export_filename}-${filesize}.img
+
+	echo "Compressing... ${device}-${export_filename}-${filesize}.img"
 	xz -T4 -z ${device}-${export_filename}-${filesize}.img
 	sync
 
 	image_download_size=$(du -b ./${device}-${export_filename}-${filesize}.img.xz | awk '{print $1}')
 	echo "            \"image_download_size\": ${image_download_size}," >> ${json_file}
 	echo "            \"release_date\": \"${time}\"," >> ${json_file}
-	echo "            \"init_format\": \"systemd\"" >> ${json_file}
 	echo "        }," >> ${json_file}
 	sync
 
 	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.bmap /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
 	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
 	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
 	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.json /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
@@ -51,9 +53,9 @@ source .project
 if [ -d ./deploy/${export_filename}/ ] ; then
 	cd ./deploy/${export_filename}/
 
-	echo "sudo ./setup_sdcard.sh --img-${filesize} bbai64-${export_filename} --dtb bbai64 --hostname BeagleBone-AI64"
-	sudo ./setup_sdcard.sh --img-${filesize} bbai64-${export_filename} --dtb bbai64 --hostname BeagleBone-AI64
-	mv ./*.img ../
+	#echo "sudo ./setup_sdcard.sh --img-${filesize} bbai64-${export_filename} --dtb bbai64 --hostname BeagleBone-AI64"
+	#sudo ./setup_sdcard.sh --img-${filesize} bbai64-${export_filename} --dtb bbai64 --hostname BeagleBone-AI64
+	#mv ./*.img ../
 
 	#echo "sudo ./setup_sdcard.sh --img-${filesize} bbai64-ti-2023.04-${export_filename} --dtb bbai64-ti-2023.04 --hostname BeagleBone-AI64"
 	#sudo ./setup_sdcard.sh --img-${filesize} bbai64-ti-2023.04-${export_filename} --dtb bbai64-ti-2023.04 --hostname BeagleBone-AI64
@@ -61,6 +63,10 @@ if [ -d ./deploy/${export_filename}/ ] ; then
 
 	echo "sudo ./setup_sdcard.sh --img-${filesize} beagleplay-${export_filename} --dtb beagleplay --hostname BeaglePlay"
 	sudo ./setup_sdcard.sh --img-${filesize} beagleplay-${export_filename} --dtb beagleplay --hostname BeaglePlay
+	mv ./*.img ../
+
+	echo "sudo ./setup_sdcard.sh --img-${filesize} beagleplay-emmc-flasher-${export_filename} --dtb beagleplay --enable-extlinux-flasher --hostname BeaglePlay"
+	sudo ./setup_sdcard.sh --img-${filesize} beagleplay-emmc-flasher-${export_filename} --dtb beagleplay --enable-extlinux-flasher --hostname BeaglePlay
 	mv ./*.img ../
 
 	#echo "sudo ./setup_sdcard.sh --img-${filesize} beagleplay-ti-2023.04-${export_filename} --dtb beagleplay-swap-ti-2023.04 --hostname BeaglePlay"
@@ -73,9 +79,10 @@ if [ -d ./deploy/${export_filename}/ ] ; then
 
 	cd ../
 
-	device="bbai64" ; compress_snapshot_image
+	#device="bbai64" ; compress_snapshot_image
 	#device="bbai64-ti-2023.04" ; compress_snapshot_image
 	device="beagleplay" ; compress_snapshot_image
+	device="beagleplay-emmc-flasher" ; compress_snapshot_image
 	#device="beagleplay-ti-2023.04" ; compress_snapshot_image
 	#device="beagleplay-mainline" ; compress_snapshot_image
 
