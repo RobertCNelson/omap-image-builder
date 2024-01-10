17d1e5e767f69e2c3128ff6f1922330418e28648
Robert Nelson <robertcnelson@gmail.com>
2024-01-10 16:14:37 -0600

    chroot: etc/hosts dont be creative, just keep debian defaults
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -657,12 +657,9 @@ if [ "${apt_proxy}" ] ; then
 fi
 
 echo "127.0.0.1	localhost" > /tmp/hosts
-echo "127.0.1.1	${rfs_hostname}.localdomain	${rfs_hostname}" >> /tmp/hosts
-echo "" >> /tmp/hosts
-echo "# The following lines are desirable for IPv6 capable hosts" >> /tmp/hosts
-echo "::1     localhost ip6-localhost ip6-loopback" >> /tmp/hosts
-echo "ff02::1 ip6-allnodes" >> /tmp/hosts
-echo "ff02::2 ip6-allrouters" >> /tmp/hosts
+echo "::1		localhost ip6-localhost ip6-loopback" >> /tmp/hosts
+echo "ff02::1		ip6-allnodes" >> /tmp/hosts
+echo "ff02::2		ip6-allrouters" >> /tmp/hosts
 sudo mv /tmp/hosts "${tempdir}/etc/hosts"
 sudo chown root:root "${tempdir}/etc/hosts"
 
