9689e48882055c91c9a2f3986ace87185324d59d
Robert Nelson <robertcnelson@gmail.com>
2024-03-29 11:11:55 -0500

    ci: cut back on the number of arm64-11-bullseye flasher specific images
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ci/daily-debian-arm64-11-bullseye-xfce-v5.10-ti-j721e.sh
+++ ci/daily-debian-arm64-11-bullseye-xfce-v5.10-ti-j721e.sh
@@ -10,9 +10,8 @@ compress_snapshot_image () {
 	json_file="${device}-${export_filename}-${filesize}.img.xz.json"
 	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
 	sync
+
 	echo "        {" >> ${json_file}
-	echo "            \"name\": \"Debian 11 ${image_type} (${deb_arch})\"," >> ${json_file}
-	echo "            \"description\": \"A port of Debian Bullseye with the ${image_type} package set\"," >> ${json_file}
 	echo "            \"icon\": \"https://rcn-ee.net/rootfs/release/BorisImageWriter.png\"," >> ${json_file}
 	echo "            \"url\": \"https://rcn-ee.net/rootfs/${rootfs}/${time}/${device}-${export_filename}-${filesize}.img.xz\"," >> ${json_file}
 	extract_size=$(du -b ./${device}-${export_filename}-${filesize}.img | awk '{print $1}')
@@ -20,18 +19,21 @@ compress_snapshot_image () {
 	extract_sha256=$(sha256sum ./${device}-${export_filename}-${filesize}.img | awk '{print $1}')
 	echo "            \"extract_sha256\": \"${extract_sha256}\"," >> ${json_file}
 
-	echo "Compressing...${device}-${export_filename}-${filesize}.img"
+	echo "Creating... ${device}-${export_filename}-${filesize}.bmap"
+	bmaptool -d create -o ./${device}-${export_filename}-${filesize}.bmap ./${device}-${export_filename}-${filesize}.img
+
+	echo "Compressing... ${device}-${export_filename}-${filesize}.img"
 	xz -T4 -z ${device}-${export_filename}-${filesize}.img
 	sync
 
 	image_download_size=$(du -b ./${device}-${export_filename}-${filesize}.img.xz | awk '{print $1}')
 	echo "            \"image_download_size\": ${image_download_size}," >> ${json_file}
 	echo "            \"release_date\": \"${time}\"," >> ${json_file}
-	echo "            \"init_format\": \"systemd\"" >> ${json_file}
 	echo "        }," >> ${json_file}
 	sync
 
 	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.bmap /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
 	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
 	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
 	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.json /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
@@ -55,14 +57,9 @@ if [ -d ./deploy/${export_filename}/ ] ; then
 	sudo ./setup_sdcard.sh --img-${filesize} bbai64-${export_filename} --dtb bbai64 --hostname BeagleBone-AI64
 	mv ./*.img ../
 
-	echo "sudo ./setup_sdcard.sh --img-${filesize} bbai64-emmc-flasher-${export_filename} --dtb bbai64 --enable-extlinux-flasher --hostname BeagleBone-AI64"
-	sudo ./setup_sdcard.sh --img-${filesize} bbai64-emmc-flasher-${export_filename} --dtb bbai64 --enable-extlinux-flasher --hostname BeagleBone-AI64
-	mv ./*.img ../
-
 	cd ../
 
 	device="bbai64" ; compress_snapshot_image
-	device="bbai64-emmc-flasher" ; compress_snapshot_image
 
 	#echo "Compressing...${export_filename}.tar"
 	#xz -T4 -z ${export_filename}.tar
