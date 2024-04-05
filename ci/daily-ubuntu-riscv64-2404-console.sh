20168c2cfd5e2706ef3ec2883548f309608d4899
Robert Nelson <robertcnelson@gmail.com>
2024-04-05 09:31:23 -0500

    riscv64: start testing ubuntu-2404
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ci/daily-ubuntu-riscv64-2404-console.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+export apt_proxy=192.168.1.10:3142/
+
+config=bb.org-ubuntu-2404-console-riscv64
+filesize=4gb
+rootfs="ubuntu-riscv64-24.04-minimal"
+
+if [ -d ./deploy ] ; then
+	sudo rm -rf ./deploy || true
+fi
+
+touch .gitea.mirror
+echo "./RootStock-NG.sh -c ${config}"
+./RootStock-NG.sh -c ${config}
+
+source .project
+
+if [ -f ./deploy/${export_filename}.tar ] ; then
+	cd ./deploy/${export_filename}/
+
+	#setup stuff...
+
+	cd ..
+
+	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+
+	echo "Compressing...${export_filename}.tar"
+	bzip2 -9 -z ${export_filename}.tar
+	sha256sum ${export_filename}.tar.bz2 > ${export_filename}.tar.bz2.sha256sum
+	sudo -uvoodoo cp -v ./${export_filename}.tar.bz2 /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sudo -uvoodoo cp -v ./${export_filename}.tar.bz2.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	echo "${export_filename}.tar.bz2" > latest
+	sudo -uvoodoo cp -v ./latest /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/
+
+	rm -rf ${tempdir} || true
+else
+	echo "failure"
+	exit 2
+fi
+#
