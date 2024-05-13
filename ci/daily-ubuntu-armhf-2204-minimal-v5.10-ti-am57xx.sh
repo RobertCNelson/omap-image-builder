76b0eb5d450ab73dbd0a151aef3952633b34489f
Robert Nelson <robertcnelson@gmail.com>
2024-05-13 16:00:38 -0500

    split: rcn-ee.net-console-ubuntu-2204-v5.10-ti-armhf into rcn-ee.net-console-ubuntu-2204-v5.10-ti-armhf-am335x and rcn-ee.net-console-ubuntu-2204-v5.10-ti-armhf-am57xx
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ci/daily-ubuntu-armhf-2204-minimal-v5.10-ti-am57xx.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export apt_proxy=192.168.1.10:3142/
+
+config=rcn-ee.net-console-ubuntu-2204-v5.10-ti-armhf-am57xx
+filesize=4gb
+rootfs="ubuntu-armhf-22.04-console-v5.10-ti"
+
+if [ -d ./deploy ] ; then
+	sudo rm -rf ./deploy || true
+fi
+
+touch .notar
+touch .gitea.mirror
+echo "./RootStock-NG.sh -c ${config}"
+./RootStock-NG.sh -c ${config}
+
+source .project
+
+if [ -d ./deploy/${export_filename}/ ] ; then
+	cd ./deploy/${export_filename}/
+
+	echo "sudo ./setup_sdcard.sh --img-${filesize} am57xx-${export_filename} --dtb am57xx-beagle-x15 --distro-bootloader --enable-uboot-cape-overlays --enable-bypass-bootup-scripts"
+	sudo ./setup_sdcard.sh --img-${filesize} am57xx-${export_filename} --dtb am57xx-beagle-x15 --distro-bootloader --enable-uboot-cape-overlays --enable-bypass-bootup-scripts
+	mv ./*.img ../
+
+	cd ..
+
+	device="am57xx"
+	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	echo "Compressing...${device}-${export_filename}-${filesize}.img"
+	xz -T0 -z ${device}-${export_filename}-${filesize}.img
+	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+
+	#echo "Compressing...${export_filename}.tar"
+	#xz -T0 -z ${export_filename}.tar
+	#sha256sum ${export_filename}.tar.xz > ${export_filename}.tar.xz.sha256sum
+	#sudo -uvoodoo cp -v ./${export_filename}.tar.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	#sudo -uvoodoo cp -v ./${export_filename}.tar.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+
+	rm -rf ${tempdir} || true
+else
+	echo "failure"
+	exit 2
+fi
+#
