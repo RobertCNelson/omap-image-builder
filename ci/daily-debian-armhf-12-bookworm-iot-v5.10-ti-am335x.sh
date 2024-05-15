d251937e9f7717d401716217bb4c6cdc881424d4
Robert Nelson <robertcnelson@gmail.com>
2024-05-15 09:33:43 -0500

    ci: bring back daily-debian-armhf-12-bookworm-iot-v5.10-ti-am335x.sh
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ci/daily-debian-armhf-12-bookworm-iot-v5.10-ti-am335x.sh
+++ ci/daily-debian-armhf-12-bookworm-iot-v5.10-ti-am335x.sh
@@ -2,7 +2,7 @@
 
 export apt_proxy=192.168.1.10:3142/
 
-config=bb.org-debian-bookworm-iot-v5.10-ti-armhf
+config=bb.org-debian-bookworm-iot-v5.10-ti-armhf-am335x
 filesize=4gb
 rootfs="debian-armhf-12-bookworm-iot-v5.10-ti"
 
@@ -20,25 +20,13 @@ source .project
 if [ -d ./deploy/${export_filename}/ ] ; then
 	cd ./deploy/${export_filename}/
 
-	echo "sudo ./setup_sdcard.sh --img-${filesize} am335x-${export_filename} --dtb beaglebone --distro-bootloader --enable-cape-universal --optional-uboot-uio-pru --enable-bypass-bootup-scripts"
-	sudo ./setup_sdcard.sh --img-${filesize} am335x-${export_filename} --dtb beaglebone --distro-bootloader --enable-cape-universal --optional-uboot-uio-pru --enable-bypass-bootup-scripts
+	echo "sudo ./setup_sdcard.sh --img-${filesize} am335x-${export_filename} --dtb beaglebone --distro-bootloader --enable-bypass-bootup-scripts"
+	sudo ./setup_sdcard.sh --img-${filesize} am335x-${export_filename} --dtb beaglebone --distro-bootloader --enable-bypass-bootup-scripts
 	mv ./*.img ../
 
-	echo "sudo ./setup_sdcard.sh --img-${filesize} am57xx-${export_filename} --dtb am57xx-beagle-x15 --distro-bootloader --enable-uboot-cape-overlays --enable-bypass-bootup-scripts"
-	sudo ./setup_sdcard.sh --img-${filesize} am57xx-${export_filename} --dtb am57xx-beagle-x15 --distro-bootloader --enable-uboot-cape-overlays --enable-bypass-bootup-scripts
-	mv ./*.img ../
-
-	cd ..
+	cd ../
 
 	device="am335x"
-	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	echo "Compressing...${device}-${export_filename}-${filesize}.img"
-	xz -T0 -z ${device}-${export_filename}-${filesize}.img
-	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
-	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
-
-	device="am57xx"
 	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
 	echo "Compressing...${device}-${export_filename}-${filesize}.img"
 	xz -T0 -z ${device}-${export_filename}-${filesize}.img
