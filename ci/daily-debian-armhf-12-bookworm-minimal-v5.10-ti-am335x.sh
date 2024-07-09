e8115b067e160c6353cbc7106e882e5cd8d071ec
Robert Nelson <robertcnelson@gmail.com>
2024-07-09 14:45:35 -0500

    add back minimal v5.10.x
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ci/daily-debian-armhf-12-bookworm-minimal-v5.10-ti-am335x.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export apt_proxy=192.168.1.10:3142/
+
+config=bb.org-debian-bookworm-minimal-v5.10-ti-armhf-am335x
+filesize=2gb
+rootfs="debian-armhf-12-bookworm-minimal-v5.10-ti"
+
+if [ -d ./deploy ] ; then
+	sudo rm -rf ./deploy || true
+fi
+
+touch .notar
+touch .gitea.mirror
+echo "./RootStock-NG.sh -c ${config}"
+./RootStock-NG.sh -c ${config}
+
+source .project
+
+if [ -d ./deploy/${export_filename}/ ] ; then
+	cd ./deploy/${export_filename}/
+
+	echo "sudo ./setup_sdcard.sh --img-${filesize} am335x-${export_filename} --dtb beaglebone --distro-bootloader --enable-bypass-bootup-scripts"
+	sudo ./setup_sdcard.sh --img-${filesize} am335x-${export_filename} --dtb beaglebone --distro-bootloader --enable-bypass-bootup-scripts
+	mv ./*.img ../
+
+	cd ../
+
+	device="am335x"
+	sudo -uvoodoo mkdir -p /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	echo "Compressing...${device}-${export_filename}-${filesize}.img"
+	xz -T0 -z ${device}-${export_filename}-${filesize}.img
+	sha256sum ${device}-${export_filename}-${filesize}.img.xz > ${device}-${export_filename}-${filesize}.img.xz.sha256sum
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	sudo -uvoodoo cp -v ./${device}-${export_filename}-${filesize}.img.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+
+	#echo "Compressing...${export_filename}.tar"
+	#xz -T0 -z ${export_filename}.tar
+	#sha256sum ${export_filename}.tar.xz > ${export_filename}.tar.xz.sha256sum
+	#sudo -uvoodoo cp -v ./${export_filename}.tar.xz /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+	#sudo -uvoodoo cp -v ./${export_filename}.tar.xz.sha256sum /mnt/mirror/rcn-ee.us/rootfs/${rootfs}/${time}/
+
+	rm -rf ${tempdir} || true
+else
+	echo "failure"
+	exit 2
+fi
+#
