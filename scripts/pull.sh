470398a70be8e578b08750c025747c6a27d6b47f
Robert Nelson <robertcnelson@gmail.com>
2025-04-14 15:12:55 -0500

    ci: add old repo
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git pull --rebase --no-edit git@gitlab.gfnd.rcn-ee.org:RobertCNelson/omap-image-builder.git
+git pull --rebase --no-edit git@gitlab.gfnd.rcn-ee.org:RobertCNelson/omap-image-builder.git --tags
 git pull --rebase --no-edit git@github.com:beagleboard/image-builder.git master
 git pull --rebase --no-edit git@github.com:beagleboard/image-builder.git master --tags
 git pull --rebase --no-edit git@github.com:RobertCNelson/omap-image-builder.git master
