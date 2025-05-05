2e6b453c5bf22d80cb8a32db807c99852e857575
Robert Nelson <robertcnelson@gmail.com>
2025-05-05 16:21:50 -0500

    debootstrap: update to 1.0.141
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -23,8 +23,8 @@
 #http://ftp.us.debian.org/debian/pool/main/d/debootstrap/
 #1.0.${minimal_debootstrap}
 #Debian Trixie/Sid usr merge....
-actual_debootstrap="140"
-minimal_debootstrap="140"
+actual_debootstrap="141"
+minimal_debootstrap="141"
 host_arch="$(uname -m)"
 
 debootstrap_is_installed () {
