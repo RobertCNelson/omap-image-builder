ff1746fb540d126a0b3ceff9c2cabe0c983248ea
Robert Nelson <robertcnelson@gmail.com>
2025-05-13 15:18:39 -0500

    openbeagle back online
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -4,5 +4,5 @@ git pull --rebase --no-edit git@github.com:beagleboard/image-builder.git master
 git pull --rebase --no-edit git@github.com:beagleboard/image-builder.git master --tags
 git pull --rebase --no-edit git@github.com:RobertCNelson/omap-image-builder.git master
 git pull --rebase --no-edit git@github.com:RobertCNelson/omap-image-builder.git master --tags
-#git pull --rebase --no-edit git@openbeagle.org:beagleboard/image-builder.git master
-#git pull --rebase --no-edit git@openbeagle.org:beagleboard/image-builder.git master --tags
+git pull --rebase --no-edit git@openbeagle.org:beagleboard/image-builder.git master
+git pull --rebase --no-edit git@openbeagle.org:beagleboard/image-builder.git master --tags
