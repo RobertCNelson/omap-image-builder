fff724dd6e317652d81d5d9a6aa9776f9532680d
Robert Nelson <robertcnelson@gmail.com>
2026-09-18 18:00:01 -0500

    chroot: trixie: also include dpkg -l here too
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- target/chroot/beagleboard.org-trixie-arm64.sh
+++ target/chroot/beagleboard.org-trixie-arm64.sh
@@ -161,6 +161,7 @@ if [ -f /usr/bin/pipx ] ; then
 		/home/${rfs_username}/.local/bin/debsbom generate -t cdx -o /opt/source/sbom/debsbom.cdx.json
 		echo "Log: (chroot): [debsbom generate -t spdx -o /opt/source/sbom/debsbom.spdx.json]"
 		/home/${rfs_username}/.local/bin/debsbom generate -t spdx -o /opt/source/sbom/debsbom.spdx.json
+		dpkg -l > /opt/source/sbom/dpkg-list.txt
 		tar -czvf /opt/source/sbom.tar.gz -C /opt/source/sbom .
 	fi
 	echo "Log: (chroot): [pipx list]"
